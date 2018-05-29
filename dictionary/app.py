e8cbac9be904de54f24d31a2780690cb4a1dedac
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-05-29 10:56:33 +0200

    Add dictionary module

---
+++ dictionary/app.py
@@ -0,0 +1,13 @@
+import falcon
+
+class DictionaryReource:
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        entry = {
+            'lemma': ['abā\'u']
+        }
+
+        resp.media = entry
+
+api = application = falcon.API()
+api.add_route('/', DictionaryReource())
