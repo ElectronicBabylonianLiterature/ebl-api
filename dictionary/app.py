348204de697699ccc282ba84a6f077aa1d724146
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-05-29 13:36:47 +0200

    Inject dictionary to WordsResource

--- dictionary/app.py
+++ dictionary/app.py
@@ -2,5 +2,12 @@ import falcon
 
 from .words import WordsResource 
 
+words = WordsResource([
+    {
+        'lemma': ['part1', 'part2'],
+        'homonym': 'I'
+    }
+])
+
 api = application = falcon.API()
-api.add_route('/words/{lemma}/{homonym}', WordsResource())
+api.add_route('/words/{lemma}/{homonym}', words)
--- dictionary/words.py
+++ dictionary/words.py
@@ -1,17 +1,14 @@
 import falcon
 
 class WordsResource:
-    dictionary = [
-        {
-            'lemma': ['part1', 'part2'],
-            'homonym': 'I'
-        }
-    ]
+    
+    def __init__(self, dictionary):
+        self.dictionary = dictionary
 
     def on_get(self, req, resp, lemma, homonym):
-        
+
         compoundLemma = lemma.split(' ')
-        words = [word for word in WordsResource.dictionary if word['lemma'] == compoundLemma and word['homonym'] == homonym]
+        words = [word for word in self.dictionary if word['lemma'] == compoundLemma and word['homonym'] == homonym]
 
         if(len(words) > 0):
             resp.media = words[0]
