6919c39cedc385ee9235f6e8a5dbb103d33faf4c
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-05-29 12:51:29 +0200

    Add lemma as route parameter

--- dictionary/app.py
+++ dictionary/app.py
@@ -1,12 +1,12 @@
 import falcon
 
 class DictionaryReource:
-    def on_get(self, req, resp):
+    def on_get(self, req, resp, lemma):
         entry = {
-            'lemma': ['abā\'u']
+            'lemma': lemma.split(' ')
         }
 
         resp.media = entry
 
 api = application = falcon.API()
-api.add_route('/', DictionaryReource())
+api.add_route('/{lemma}', DictionaryReource())
--- tests/test_app.py
+++ tests/test_app.py
@@ -11,12 +11,14 @@ def client():
     return testing.TestClient(api)
 
 def test_list_images(client):
+    part1 = 'part1'
+    part2 = 'part2'
     entry = {
-        'lemma': ['abā\'u']
+        'lemma': [part1, part2]
     }
 
-    response = client.simulate_get('/')
-
+    response = client.simulate_get(f'/{part1} {part2}')
+ 
     assert json.loads(response.content) == entry
     assert response.status == falcon.HTTP_OK
     
