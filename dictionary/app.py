b68ccef47f8e03edccf8782470e8cac14cc3b947
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-05-29 13:26:27 +0200

    Search lemma from dictionary

--- dictionary/app.py
+++ dictionary/app.py
@@ -1,13 +1,21 @@
 import falcon
 
 class WordsResource:
-    def on_get(self, req, resp, lemma, homonym):
-        entry = {
-            'lemma': lemma.split(' '),
-            'homonym': homonym
+    dictionary = [
+        {
+            'lemma': ['part1', 'part2'],
+            'homonym': 'homonym'
         }
+    ]
 
-        resp.media = entry
+    def on_get(self, req, resp, lemma, homonym):
+        compoundLemma = lemma.split(' ')
+        words = [word for word in WordsResource.dictionary if word['lemma'] == compoundLemma]
+
+        if(len(words) > 0):
+            resp.media = words[0]
+        else:
+            resp.status = falcon.HTTP_NOT_FOUND
 
 api = application = falcon.API()
 api.add_route('/words/{lemma}/{homonym}', WordsResource())
--- tests/test_app.py
+++ tests/test_app.py
@@ -10,17 +10,23 @@ from dictionary.app import api
 def client():
     return testing.TestClient(api)
 
-def test_list_images(client):
+def test_get_word(client):
     part1 = 'part1'
     part2 = 'part2'
-    homonym = 'homonym'
-    entry = {
+    homonym = 'I'
+
+    expectedWord = {
         'lemma': [part1, part2],
         'homonym': homonym
     }
 
     response = client.simulate_get(f'/words/{part1} {part2}/{homonym}')
  
-    assert json.loads(response.content) == entry
+    assert json.loads(response.content) == expectedWord
     assert response.status == falcon.HTTP_OK
+
+def test_lemma_not_found(client):
+    response = client.simulate_get(f'/words/unknown/I')
+ 
+    assert response.status == falcon.HTTP_NOT_FOUND
     
