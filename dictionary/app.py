6ba3eca2cdfeab52e6e92907b81f96ce5ba1576e
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-06-04 16:16:19 +0200

    Add dummy basic authentication

--- dictionary/app.py
+++ dictionary/app.py
@@ -1,5 +1,6 @@
 import json
 import falcon
+from falcon_auth import FalconAuthMiddleware, BasicAuthBackend
 
 from .cors_component import CORSComponent
 from .dictionary import Dictionary
@@ -9,7 +10,11 @@ def create_app(dictionary_path):
     with open(dictionary_path, encoding='utf8') as dictionary_json:
         dictionary = Dictionary(json.load(dictionary_json))
 
-    api = falcon.API(middleware=CORSComponent())
+    user_loader = lambda username, password: { 'username': username }
+    auth_backend = BasicAuthBackend(user_loader)
+    auth_middleware = FalconAuthMiddleware(auth_backend)
+
+    api = falcon.API(middleware=[CORSComponent(), auth_middleware])
 
     words = WordsResource(dictionary)
     api.add_route('/words/{lemma}/{homonym}', words)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
 import json
+from base64 import b64encode
 import falcon
 from falcon import testing
 import pytest
@@ -6,12 +7,18 @@ import pytest
 import dictionary.app
 
 
+
 @pytest.fixture
 def client():
     api = dictionary.app.create_app('./tests/dictionary.json')
     return testing.TestClient(api)
 
-def test_get_word(client):
+@pytest.fixture 
+def headers():
+    userAndPass = b64encode(b"username:password").decode("ascii")
+    return { 'Authorization' : f'Basic {userAndPass}'}
+
+def test_get_word(client, headers):
     part1 = 'part1'
     part2 = 'part2'
     homonym = 'I'
@@ -21,27 +28,32 @@ def test_get_word(client):
         'homonym': homonym
     }
 
-    result = client.simulate_get(f'/words/{part1} {part2}/{homonym}')
+    result = client.simulate_get(f'/words/{part1} {part2}/{homonym}', headers=headers)
 
     assert json.loads(result.content) == expected_word
     assert result.status == falcon.HTTP_OK
     assert result.headers['Access-Control-Allow-Origin'] == '*'
 
-def test_lemma_not_found(client):
-    result = client.simulate_get(f'/words/unknown/I')
+def test_lemma_not_found(client, headers):
+    result = client.simulate_get(f'/words/unknown/I', headers=headers)
 
     assert result.status == falcon.HTTP_NOT_FOUND
 
-def test_homonym_not_found(client):
-    result = client.simulate_get(f'/words/part1 part2/II')
+def test_homonym_not_found(client, headers):
+    result = client.simulate_get(f'/words/part1 part2/II', headers=headers)
 
     assert result.status == falcon.HTTP_NOT_FOUND
 
-def test_cors(client):
+def test_cors(client, headers):
     headers = {'Access-Control-Request-Method': 'GET'}
     result = client.simulate_options(f'/words/part1 part2/I', headers=headers)
 
     assert result.headers['Access-Control-Allow-Methods'] == 'GET'
     assert result.headers['Access-Control-Allow-Origin'] == '*'
     assert result.headers['Access-Control-Max-Age'] == '86400'
+
+def test_unauthorized(client):
+    result = client.simulate_get(f'/words/part1 part2/I')
+
+    assert result.status == falcon.HTTP_UNAUTHORIZED
     
