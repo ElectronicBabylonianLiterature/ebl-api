4824d94fc84163aaa02b9e9515a4b112839185ee
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-07-06 14:58:54 +0200

    Add method for updating transliteration

--- ebl/fragmentarium/fragmentarium.py
+++ ebl/fragmentarium/fragmentarium.py
@@ -5,3 +5,9 @@ class MongoFragmentarium(MongoRepository):
 
     def __init__(self, database):
         super().__init__(database, 'fragments')
+
+    def update_transliteration(self, number, transliteration):
+        self.get_collection().update_one(
+            {'_id': number},
+            {'$set': {'transliteration': transliteration}}
+        )
--- tests/test_fragmentarium.py
+++ tests/test_fragmentarium.py
@@ -1,3 +1,4 @@
+import pydash
 import pytest
 
 
@@ -10,3 +11,19 @@ def test_create_and_find(fragmentarium, fragment):
 def test_fragment_not_found(fragmentarium):
     with pytest.raises(KeyError):
         fragmentarium.find('unknown id')
+
+
+def test_update_transliteration(fragmentarium, fragment):
+    fragmentarium.create(fragment)
+    transliteration = 'the transliteration'
+
+    fragmentarium.update_transliteration(fragment['_id'], transliteration)
+    updated_fragment = fragmentarium.find(fragment['_id'])
+
+    expected_fragment = pydash.assign(
+        {},
+        fragment,
+        {'transliteration': transliteration}
+    )
+
+    assert updated_fragment == expected_fragment
