dd3f33eb47d9bc91db2af44a580f29a22b1d5eba
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-09-19 10:54:23 +0200

    Refactor mapping transliteration

--- ebl/sign_list/sign_list.py
+++ ebl/sign_list/sign_list.py
@@ -20,34 +20,38 @@ class SignList:
         return self._repository.search(reading, sub_index)
 
     def map_transliteration(self, cleaned_transliteration):
-        return [self._parse_row(row) for row in cleaned_transliteration]
-
-    def _parse_row(self, row):
-        return [self._parse_value(value) for value in row.split(' ')]
+        return [
+            [self._parse_value(value) for value in row.split(' ')]  
+            for row in cleaned_transliteration
+        ]
 
     def _parse_value(self, value):
-        match = re.fullmatch(r'\|?(\d*[.x×%&+@]?[A-ZṢŠṬ₀-₉]+)+\|?|'
+        grapheme_match = re.fullmatch(
+            r'\|?(\d*[.x×%&+@]?[A-ZṢŠṬ₀-₉]+)+\|?|'
                              r'\d+|'
-                             r'[^\(]+\((.+)\)', value)
-        if match:
-            return match.group(2) or value
-        else:
-            return self._parse_reading(value)
-
-    def _parse_reading(self, reading):
-        reading_match = re.fullmatch(r'([^₀-₉ₓ/]+)([₀-₉ₓ]+)?', reading)
-        if reading_match:
-            return self._parse_match(reading_match)
-        elif '/' in reading:
-            return self._parse_variant(reading)
+            r'[^\(]+\((.+)\)', value
+        )
+        reading_match = re.fullmatch(r'([^₀-₉ₓ/]+)([₀-₉ₓ]+)?', value)
+        if grapheme_match:
+            return self._parse_grapheme(grapheme_match)
+        elif reading_match:
+            return self._parse_reading(reading_match)
+        elif '/' in value:
+            return self._parse_variant(value)
         else:
             return UNKNOWN_SIGN
 
-    def _parse_match(self, match):
+    def _parse_grapheme(self, match):
+        return match.group(2) or match.group(0)
+
+    def _parse_reading(self, match):
         value = match.group(1)
-        sub_index = match.group(2) or '1'
-        normalized_sub_index = unicodedata.normalize('NFKC', sub_index)
-        sign = self.search(value, int(normalized_sub_index))
+        sub_index = (
+            int(unicodedata.normalize('NFKC', match.group(2)))
+            if match.group(2)
+            else 1
+        )
+        sign = self.search(value, sub_index)
         return sign['_id'] if sign else UNKNOWN_SIGN
 
     def _parse_variant(self, reading):
