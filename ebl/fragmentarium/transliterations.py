6f2bbba95e1ecbc6e8fe6da51936c58a72d6ab6e
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-09-20 16:08:20 +0200

    Refactor transliterations.py

--- ebl/fragmentarium/transliterations.py
+++ ebl/fragmentarium/transliterations.py
@@ -2,52 +2,94 @@ import re
 import pydash
 
 
+STRIP_PATTERN = (
+    r'^[^\.]+\.([^\.]+\.)?\s+|'
+    r'<<?\(?[^>]+\)?>?>|'
+    r'\[|'
+    r'\]|'
+    r'\.\.\.|'
+    r'\(\$_+\$\)|'
+    r'\?|'
+    r'\*|'
+    r'#|'
+    r'!|'
+    r'\$|'
+    r'%\w+\s+'
+)
+WHITE_SPACE_PATTERN = (
+    r'\s*{+\+?|'
+    r'}+({+\+?)?\s*|'
+    r'-|'
+    r'(^|\s+)(\||&\d*)($|\s+)'
+)
+BRACES_PATTERN = (
+    r'(?<![^\s])'
+    r'\(([^\(\)]*)\)'
+    r'(?!=[^\s])'
+)
+
+
 def clean(transliteration):
-    return (pydash
-            .chain(transliteration)
-            .thru(filter_lines)
-            .map(lambda line: re.sub(r'^[^\.]+\.([^\.]+\.)?\s+|'
-                                     r'<<?\(?[^>]+\)?>?>|'
-                                     r'\[|'
-                                     r'\]|'
-                                     r'\.\.\.', '', line))
-            .map(lambda line: re.sub(r'\s*{+\+?|'
-                                     r'}+({+\+?)?\s*|'
-                                     r'-|'
-                                     r'(^|\s+)(\||&\d*)($|\s+)', ' ', line))
-            .map(lambda line:
-                 re.sub(r'\(\$_+\$\)|\?|\*|#|!|\$|%\w+\s+', '', line))
-            .map(lambda line:
-                 re.sub(r'(?<![^\s])\(([^\(\)]*)\)(?!=[^\s])', r'\1', line))
-            .map(pydash.clean)
-            .map(lambda line: line.split(' '))
-            .map(lambda line: [
-                _clean_value(value)
-                for value in line
-            ])
-            .map(' '.join)
-            .map(pydash.clean)
-            .value())
+    return (
+        pydash
+        .chain(transliteration)
+        .thru(filter_lines)
+        .map(_clean_line)
+        .map(_clean_values)
+        .value()
+    )
+
+
+def _clean_line(line):
+    return (
+        pydash
+        .chain(line)
+        .reg_exp_replace(WHITE_SPACE_PATTERN, ' ')
+        .reg_exp_replace(STRIP_PATTERN, '')
+        .reg_exp_replace(BRACES_PATTERN, r'\1')
+        .clean()
+        .value()
+    )
+
+
+def _clean_values(line):
+    return (
+        pydash
+        .chain(line)
+        .split(' ')
+        .map(_clean_value)
+        .join(' ')
+        .clean()
+        .value()
+    )
 
 
 def _clean_value(value):
     grapheme = re.fullmatch(r'\|[^|]+\|', value)
     reading_with_sign = re.fullmatch(r'[^\(]+\(([^\)]+)\)', value)
     if '/' in value:
-        return '/'.join([
-            _clean_value(part)
-            for part in value.split('/')
-        ])
+        return _clean_variant(value)
     elif grapheme or reading_with_sign:
         return value
     else:
-        return (
-            pydash.chain(value)
-            .reg_exp_replace(r'[.+]', ' ')
-            .reg_exp_replace(r'@[^\s]+', '')
-            .value()
-            .lower()
-        )
+        return _clean_reading(value)
+
+
+def _clean_variant(value):
+    return '/'.join([
+        _clean_value(part)
+        for part in value.split('/')
+    ])
+
+
+def _clean_reading(value):
+    return (
+        pydash.chain(value)
+        .reg_exp_replace(r'[.+]', ' ')
+        .reg_exp_replace(r'@[^\s]+', '')
+        .value()
+        .lower()
+    )
 
 
 def filter_lines(transliteration):
