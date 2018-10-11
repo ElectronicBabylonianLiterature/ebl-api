ac25328072839df461ecf67e5580d1bda3f1362e
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-10-11 16:03:29 +0200

    Refactor fragment serach

--- ebl/fragmentarium/fragment_search.py
+++ ebl/fragmentarium/fragment_search.py
@@ -1,6 +1,7 @@
 import falcon
 
 from ebl.require_scope import require_scope
+from ebl.fragmentarium.fragment import Fragment
 from ebl.fragmentarium.transliterations import Transliteration
 
 
@@ -8,58 +9,48 @@ class FragmentSearch:
     # pylint: disable=R0903
     def __init__(self, fragmentarium):
         self._fragmentarium = fragmentarium
-
-    @falcon.before(require_scope, 'read:fragments')
-    def on_get(self, req, resp):
-        param_names = list(req.params)
-        if len(param_names) == 1:
-            self._select_method(param_names)(req, resp)
-        else:
-            self._unprocessable_entity(req, resp)
-
-    def _select_method(self, param_names):
-        methods = {
+        self._commands = {
             'number': self._search,
             'random': self._find_random,
             'interesting': self._find_interesting,
             'transliteration': self._search_transliteration
         }
-        return methods.get(param_names[0], self._unprocessable_entity)
 
-    def _search(self, req, resp):
-        resp.media = self._map_fragments(
-            self._fragmentarium.search(req.params['number']),
-            req.context['user']
-        )
+    @falcon.before(require_scope, 'read:fragments')
+    def on_get(self, req, resp):
+        param_names = list(req.params)
+        user = req.context['user']
+        if len(param_names) == 1:
+            param = param_names[0]
+            value = req.params[param]
+            fragments = self._get_command(param)(value)
+            resp.media = [fragment.to_dict_for(user) for fragment in fragments]
+        else:
+            raise falcon.HTTPUnprocessableEntity()
 
-    def _find_random(self, req, resp):
-        resp.media = self._map_fragments(
-            self._fragmentarium.find_random(),
-            req.context['user']
-        )
+    def _get_command(self, param):
+        if param in self._commands:
+            return self._commands[param]
+        else:
+            raise falcon.HTTPUnprocessableEntity()
 
-    def _find_interesting(self, req, resp):
-        resp.media = self._map_fragments(
-            self._fragmentarium.find_interesting(),
-            req.context['user']
-        )
+    def _search(self, number):
+        return self._fragmentarium.search(number)
 
-    def _search_transliteration(self, req, resp):
+    def _find_random(self, _):
+        return self._fragmentarium.find_random()
+
+    def _find_interesting(self, _):
+        return self._fragmentarium.find_interesting()
+
+    def _search_transliteration(self, query):
         transliteration =\
-            Transliteration.without_notes(req.params['transliteration'])
-        resp.media = [
-            {
-                **(fragment_and_lines[0].to_dict_for(req.context['user'])),
+            Transliteration.without_notes(query)
+        return [
+            Fragment({
+                **fragment_and_lines[0].to_dict(),
                 'matching_lines': fragment_and_lines[1]
-            }
+            })
             for fragment_and_lines
             in self._fragmentarium.search_signs(transliteration)
         ]
-
-    @staticmethod
-    def _unprocessable_entity(_, resp):
-        resp.status = falcon.HTTP_UNPROCESSABLE_ENTITY
-
-    @staticmethod
-    def _map_fragments(fragments, user):
-        return [fragment.to_dict_for(user) for fragment in fragments]
