1d50c008e4cb607cefa6c8b763ee3d85175fd9f0
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-11-20 14:07:26 +0100

    Simplify resources

--- ebl/dictionary/word_search.py
+++ ebl/dictionary/word_search.py
@@ -6,18 +6,11 @@ from ebl.require_scope import require_scope
 class WordSearch:
     # pylint: disable=R0903
     def __init__(self, dictionary):
-        self._dictionary = dictionary
         self._dispatch = create_dispatcher({
-            'query': self._search,
-            'lemma': self._search_lemma,
+            'query': dictionary.search,
+            'lemma': dictionary.search_lemma,
         })
 
     @falcon.before(require_scope, 'read:words')
     def on_get(self, req, resp):
         resp.media = self._dispatch(req)
-
-    def _search(self, query):
-        return self._dictionary.search(query)
-
-    def _search_lemma(self, lemma):
-        return self._dictionary.search_lemma(lemma)
--- ebl/fragmentarium/fragment_search.py
+++ ebl/fragmentarium/fragment_search.py
@@ -1,4 +1,5 @@
 import falcon
+import pydash
 
 from ebl.dispatcher import create_dispatcher
 from ebl.require_scope import require_scope
@@ -8,29 +9,15 @@ from ebl.fragmentarium.transliteration import Transliteration
 class FragmentSearch:
     # pylint: disable=R0903
     def __init__(self, fragmentarium):
-        self._fragmentarium = fragmentarium
         self._dispatch = create_dispatcher({
-            'number': self._search,
-            'random': self._find_random,
-            'interesting': self._find_interesting,
-            'transliteration': self._search_transliteration
+            'number': fragmentarium.search,
+            'random': lambda _: fragmentarium.find_random(),
+            'interesting': lambda _: fragmentarium.find_interesting(),
+            'transliteration': pydash.flow(Transliteration, fragmentarium.search_signs)
         })
 
     @falcon.before(require_scope, 'read:fragments')
     def on_get(self, req, resp):
         user = req.context['user']
         fragments = self._dispatch(req)
-        resp.media = [fragment.to_dict_for(user) for fragment in fragments]
-
-    def _search(self, number):
-        return self._fragmentarium.search(number)
-
-    def _find_random(self, _):
-        return self._fragmentarium.find_random()
-
-    def _find_interesting(self, _):
-        return self._fragmentarium.find_interesting()
-
-    def _search_transliteration(self, query):
-        transliteration = Transliteration(query)
-        return self._fragmentarium.search_signs(transliteration)
+        resp.media = [fragment.to_dict_for(user) for fragment in fragments]     
