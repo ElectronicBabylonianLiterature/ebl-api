24d33ed75f20d1b9c66e6e0eca9a1f89f7084ddb
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-12-07 10:03:44 +0100

    Refactor Merger

--- ebl/fragmentarium/merger.py
+++ ebl/fragmentarium/merger.py
@@ -45,59 +45,60 @@ class Merge:
         return self._result
 
     @property
-    def old_line(self):
+    def previous_old(self):
         return self._old_line
 
-    def advance_new(self):
+    def _advance_new(self):
         self._new = self._new[1:]
         self._old_line = None
 
-    def advance_old(self, save_old_line=False):
+    def _advance_old(self, save_old_line):
         if save_old_line:
             self._old_line = self.current_old
         self._old = self._old[1:]
 
-    def append(self, entry):
+    def _append(self, entry):
         self._result.append(entry)
+
+    def delete(self):
+        self._advance_old(True)
+        return self
+
+    def keep(self):
+        self._append(self.current_old)
+        self._advance_old(False)
+        self._advance_new()
+        return self
+
+    def add(self, entry):
+        self._append(entry)
+        self._advance_new()
+        return self
 
 
 class Merger:
     # pylint: disable=R0903
     def __init__(self, map_, dimensions):
         self._operations = {
-            '- ': self._delete_entry,
+            '- ': lambda state: state.delete(),
             '+ ': self._add_entry,
-            '  ': self._keep_entry,
+            '  ': lambda state: state.keep(),
             '? ': pydash.identity
         }
         self._map = map_
         self._dimensions = dimensions
 
-    @staticmethod
-    def _delete_entry(state):
-        state.advance_old(True)
-        return state
-
     def _inner_merge(self, state):
         return Merger(self._map, self._dimensions - 1).merge(
-            state.old_line,
+            state.previous_old,
             state.current_new
         )
 
     def _add_entry(self, state):
         new_entry = (self._inner_merge(state)
-                     if self._dimensions > 1 and state.old_line is not None
+                     if self._dimensions > 1 and state.previous_old is not None
                      else state.current_new)
-        state.append(new_entry)
-        state.advance_new()
-        return state
-
-    @staticmethod
-    def _keep_entry(state):
-        state.append(state.current_old)
-        state.advance_new()
-        state.advance_old()
-        return state
+        return state.add(new_entry)
 
     def merge(self, old, new):
         diff = Differ(self._map, self._dimensions).diff(old, new)
