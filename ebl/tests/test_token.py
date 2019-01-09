52f0e9e25cc294740487908c7f33ca7950d221c0
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-01-09 08:45:50 +0100

    Extract Word tests to separate file

--- ebl/tests/test_token.py
+++ ebl/tests/test_token.py
@@ -1,9 +1,15 @@
 import pytest
-from ebl.text.language import Language, DEFAULT_LANGUAGE
+from ebl.text.language import Language
 from ebl.text.lemmatization import LemmatizationToken, LemmatizationError
 from ebl.text.token import (
-    UniqueLemma, Token, Word, LanguageShift, DEFAULT_NORMALIZED
+    Token, LanguageShift, DEFAULT_NORMALIZED
 )
+
+
+TOKENS = [
+    Token('...'),
+    LanguageShift('%sux')
+]
 
 
 def test_token():
@@ -24,74 +30,6 @@ def test_token():
 
     assert token != other
     assert hash(token) != hash(other)
-
-
-def test_word_defaults():
-    value = 'value'
-    word = Word(value)
-
-    assert word.value == value
-    assert word.lemmatizable is True
-    assert word.language == DEFAULT_LANGUAGE
-    assert word.normalized is DEFAULT_NORMALIZED
-    assert word.unique_lemma == tuple()
-
-
-@pytest.mark.parametrize("language,normalized,unique_lemma", [
-    (Language.SUMERIAN, False, (UniqueLemma('ku II'), UniqueLemma('aklu I'))),
-    (Language.SUMERIAN, True, tuple()),
-    (Language.EMESAL, False, tuple()),
-    (Language.EMESAL, True, tuple()),
-    (Language.AKKADIAN, False, (UniqueLemma('aklu I'), )),
-    (Language.AKKADIAN, True, tuple())
-])
-def test_word(language, normalized, unique_lemma):
-    value = 'value'
-    word = Word(value, language, normalized, unique_lemma)
-
-    equal = Word(value, language, normalized, unique_lemma)
-    other_language = Word(value, Language.UNKNOWN, normalized, unique_lemma)
-    other_value = Word('other value', language, normalized, unique_lemma)
-    other_unique_lemma =\
-        Word(value, language, normalized, tuple(UniqueLemma('waklu I')))
-    other_normalized =\
-        Word('other value', language, not normalized, unique_lemma)
-
-    assert word.value == value
-    assert word.lemmatizable is (language.lemmatizable and not normalized)
-    assert word.language == language
-    assert word.normalized is normalized
-    assert word.unique_lemma == unique_lemma
-    assert word.to_dict() == {
-        'type': 'Word',
-        'value': word.value,
-        'uniqueLemma': [*unique_lemma],
-        'normalized': normalized,
-        'language': word.language.name,
-        'lemmatizable': word.lemmatizable
-    }
-
-    assert word == equal
-    assert hash(word) == hash(equal)
-
-    for not_equal in [
-            other_language, other_value, other_unique_lemma, other_normalized
-    ]:
-        assert word != not_equal
-        assert hash(word) != hash(not_equal)
-
-    assert word != Token(value)
-
-
-def test_word_set_language():
-    value = 'value'
-    unique_lemma = (UniqueLemma('aklu I'), )
-    language = Language.SUMERIAN
-    normalized = False
-    word = Word(value, Language.AKKADIAN, not normalized, unique_lemma)
-    expected_word = Word(value, language, normalized, unique_lemma)
-
-    assert word.set_language(language, normalized) == expected_word
 
 
 @pytest.mark.parametrize("value,expected_language,normalized", [
@@ -126,46 +64,21 @@ def test_language_shift(value, expected_language, normalized):
     assert shift != Token(value)
 
 
-def test_default_normalized():
-    assert DEFAULT_NORMALIZED is False
+@pytest.mark.parametrize("token", TOKENS)
+def test_set_unique_lemma_incompatible(token):
+    lemma = LemmatizationToken('other-value')
+    with pytest.raises(LemmatizationError):
+        token.set_unique_lemma(lemma)
 
 
-@pytest.mark.parametrize("token", [
-    Token('...'),
-    LanguageShift('%sux')
-])
-def test_update_lemmatization_invalid(token):
+@pytest.mark.parametrize("token", TOKENS)
+def test_set_unique_lemma_with_lemma(token):
     lemma = LemmatizationToken(token.value, tuple())
     with pytest.raises(LemmatizationError):
         token.set_unique_lemma(lemma)
 
 
-@pytest.mark.parametrize("token", [
-    Token('...'),
-    LanguageShift('%sux')
-])
-def test_update_lemmatization_lemma(token):
+@pytest.mark.parametrize("token", TOKENS)
+def test_set_unique_lemma_no_lemma(token):
     lemma = LemmatizationToken(token.value)
     assert token.set_unique_lemma(lemma) == token
-
-
-def test_set_unique_lemma_word():
-    word = Word('bu')
-    lemma = LemmatizationToken('bu', ('nu I', ))
-    expected = Word('bu', unique_lemma=('nu I', ))
-
-    assert word.set_unique_lemma(lemma) == expected
-
-
-def test_set_unique_lemma_incompatible():
-    word = Word('mu')
-    lemma = LemmatizationToken('bu', ('nu I', ))
-    with pytest.raises(LemmatizationError):
-        word.set_unique_lemma(lemma)
-
-
-def test_set_unique_lemma_not_lemmatizable():
-    word = Word('bu', language=Language.SUMERIAN)
-    lemma = LemmatizationToken('bu', ('nu I', ))
-    with pytest.raises(LemmatizationError):
-        word.set_unique_lemma(lemma)
---
+++ ebl/tests/test_word.py
@@ -0,0 +1,96 @@
+import pytest
+from ebl.text.language import Language, DEFAULT_LANGUAGE
+from ebl.text.lemmatization import LemmatizationToken, LemmatizationError
+from ebl.text.token import (
+    UniqueLemma, Token, Word, DEFAULT_NORMALIZED
+)
+
+
+def test_default_normalized():
+    assert DEFAULT_NORMALIZED is False
+
+
+def test_defaults():
+    value = 'value'
+    word = Word(value)
+
+    assert word.value == value
+    assert word.lemmatizable is True
+    assert word.language == DEFAULT_LANGUAGE
+    assert word.normalized is DEFAULT_NORMALIZED
+    assert word.unique_lemma == tuple()
+
+
+@pytest.mark.parametrize("language,normalized,unique_lemma", [
+    (Language.SUMERIAN, False, (UniqueLemma('ku II'), UniqueLemma('aklu I'))),
+    (Language.SUMERIAN, True, tuple()),
+    (Language.EMESAL, False, tuple()),
+    (Language.EMESAL, True, tuple()),
+    (Language.AKKADIAN, False, (UniqueLemma('aklu I'), )),
+    (Language.AKKADIAN, True, tuple())
+])
+def test_word(language, normalized, unique_lemma):
+    value = 'value'
+    word = Word(value, language, normalized, unique_lemma)
+
+    equal = Word(value, language, normalized, unique_lemma)
+    other_language = Word(value, Language.UNKNOWN, normalized, unique_lemma)
+    other_value = Word('other value', language, normalized, unique_lemma)
+    other_unique_lemma =\
+        Word(value, language, normalized, tuple(UniqueLemma('waklu I')))
+    other_normalized =\
+        Word('other value', language, not normalized, unique_lemma)
+
+    assert word.value == value
+    assert word.lemmatizable is (language.lemmatizable and not normalized)
+    assert word.language == language
+    assert word.normalized is normalized
+    assert word.unique_lemma == unique_lemma
+    assert word.to_dict() == {
+        'type': 'Word',
+        'value': word.value,
+        'uniqueLemma': [*unique_lemma],
+        'normalized': normalized,
+        'language': word.language.name,
+        'lemmatizable': word.lemmatizable
+    }
+
+    assert word == equal
+    assert hash(word) == hash(equal)
+
+    for not_equal in [
+            other_language, other_value, other_unique_lemma, other_normalized
+    ]:
+        assert word != not_equal
+        assert hash(word) != hash(not_equal)
+
+    assert word != Token(value)
+
+
+def test_set_language():
+    value = 'value'
+    unique_lemma = (UniqueLemma('aklu I'), )
+    language = Language.SUMERIAN
+    normalized = False
+    word = Word(value, Language.AKKADIAN, not normalized, unique_lemma)
+    expected_word = Word(value, language, normalized, unique_lemma)
+
+    assert word.set_language(language, normalized) == expected_word
+
+
+def test_set_unique_lemma():
+    word = Word('bu')
+    lemma = LemmatizationToken('bu', ('nu I', ))
+    expected = Word('bu', unique_lemma=('nu I', ))
+
+    assert word.set_unique_lemma(lemma) == expected
+
+
+@pytest.mark.parametrize("word", [
+    Word('mu'),
+    Word('bu', language=Language.SUMERIAN)
+])
+def test_set_unique_lemma_invalid(word):
+    lemma = LemmatizationToken('bu', ('nu I', ))
+    with pytest.raises(LemmatizationError):
+        word.set_unique_lemma(lemma)
