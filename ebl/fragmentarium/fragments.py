627fd08fd65c599493fe33ce52404074d499c66d
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-01-09 11:33:49 +0100

    Add ATF to fragment response

--- ebl/fragmentarium/fragments.py
+++ ebl/fragmentarium/fragments.py
@@ -9,10 +9,11 @@ class FragmentsResource:
     @falcon.before(require_scope, 'read:fragments')
     def on_get(self, req, resp, number):
         user = req.context['user']
-        resp.media = (self
-                      ._fragmentarium
-                      .find(number)
-                      .to_dict_for(user))
+        fragment = self._fragmentarium.find(number)
+        resp.media = {
+            **fragment.to_dict_for(user),
+            'atf': fragment.text.atf
+        }
 
     def on_post(self, _req, _resp, number):
         # pylint: disable=R0201
--- ebl/tests/test_fragments_route.py
+++ ebl/tests/test_fragments_route.py
@@ -2,11 +2,14 @@ import json
 import falcon
 
 
-def test_get_fragment(client, fragmentarium, fragment, user):
-    fragment_number = fragmentarium.create(fragment)
+def test_get_fragment(client, fragmentarium, transliterated_fragment, user):
+    fragment_number = fragmentarium.create(transliterated_fragment)
     result = client.simulate_get(f'/fragments/{fragment_number}')
 
-    assert result.json == fragment.to_dict_for(user)
+    assert result.json == {
+        **transliterated_fragment.to_dict_for(user),
+        'atf': transliterated_fragment.text.atf
+    }
     assert result.status == falcon.HTTP_OK
     assert result.headers['Access-Control-Allow-Origin'] == '*'
 
