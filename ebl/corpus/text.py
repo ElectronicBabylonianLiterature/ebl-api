0503bba511aeec129e2aeb428b46224ece5f167c
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-04-12 10:12:11 +0200

    Remove duplication from period and stage configuration

--- ebl/corpus/text.py
+++ ebl/corpus/text.py
@@ -76,37 +76,27 @@ class PeriodModifier(Enum):
     LATE = 'Late'
 
 
-class Period(Enum):
-    UR_III = 'Ur III'
-    OLD_ASSYRIAN = 'Old Assyrian'
-    OLD_BABYLONIAN = 'Old Babylonian'
-    MIDDLE_BABYLONIAN = 'Middle Babylonian'
-    MIDDLE_ASSYRIAN = 'Middle Assyrian'
-    HITTITE = 'Hittite'
-    NEO_ASSYRIAN = 'Neo-Assyrian'
-    NEO_BABYLONIAN = 'Neo-Babylonian'
-    LATE_BABYLONIAN = 'Late Babylonian'
-    PERSIAN = 'Persian'
-    HELLENISTIC = 'Hellenistic'
-    PARTHIAN = 'Parthian'
-    UNCERTAIN = 'Uncertain'
+PERIOD_NAMES = {
+    'UR_III': 'Ur III',
+    'OLD_ASSYRIAN': 'Old Assyrian',
+    'OLD_BABYLONIAN': 'Old Babylonian',
+    'MIDDLE_BABYLONIAN': 'Middle Babylonian',
+    'MIDDLE_ASSYRIAN': 'Middle Assyrian',
+    'HITTITE': 'Hittite',
+    'NEO_ASSYRIAN': 'Neo-Assyrian',
+    'NEO_BABYLONIAN': 'Neo-Babylonian',
+    'LATE_BABYLONIAN': 'Late Babylonian',
+    'PERSIAN': 'Persian',
+    'HELLENISTIC': 'Hellenistic',
+    'PARTHIAN': 'Parthian',
+    'UNCERTAIN': 'Uncertain'
+}
 
 
-class Stage(Enum):
-    UR_III = 'Ur III'
-    OLD_ASSYRIAN = 'Old Assyrian'
-    OLD_BABYLONIAN = 'Old Babylonian'
-    MIDDLE_BABYLONIAN = 'Middle Babylonian'
-    MIDDLE_ASSYRIAN = 'Middle Assyrian'
-    HITTITE = 'Hittite'
-    NEO_ASSYRIAN = 'Neo-Assyrian'
-    NEO_BABYLONIAN = 'Neo-Babylonian'
-    LATE_BABYLONIAN = 'Late Babylonian'
-    PERSIAN = 'Persian'
-    HELLENISTIC = 'Hellenistic'
-    PARTHIAN = 'Parthian'
-    UNCERTAIN = 'Uncertain'
-    STANDARD_BABYLONIAN = 'Standard Babylonian'
+Period = Enum('Period', PERIOD_NAMES)
+
+
+Stage = Enum('Stage', {**PERIOD_NAMES, 'STANDARD_BABYLONIAN': 'Standard Babylonian'})
 
 
 ManuscriptDict = Dict[str, Union[int, str, List[dict]]]
