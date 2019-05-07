4d60e069426c7887ce4c38ed12ccf17d37a4fdb9
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-05-07 10:32:06 +0200

    Implement ApiDeserializer

--- ebl/corpus/api_serializer.py
+++ ebl/corpus/api_serializer.py
@@ -1,15 +1,13 @@
-import pydash
 from parsy import ParseError
 
 from ebl.corpus.text import Text, ManuscriptLine
 from ebl.corpus.text_serializer import TextSerializer, TextDeserializer
 from ebl.errors import DataError
-from ebl.text.labels import LineNumberLabel
+from ebl.text.labels import LineNumberLabel, Label
 from ebl.text.text_parser import TEXT_LINE
 
 
 class ApiSerializer(TextSerializer):
-    # pylint: disable=R0903
 
     def __init__(self, include_documents=True):
         super().__init__(include_documents)
@@ -24,43 +22,26 @@ class ApiSerializer(TextSerializer):
         })
 
 
+class ApiDeserializer(TextDeserializer):
+
+    def deserialize_manuscript_line(self,
+                                    manuscript_line: dict) -> ManuscriptLine:
+        line_number = LineNumberLabel(manuscript_line['number']).to_atf()
+        atf = manuscript_line['atf']
+        line = TEXT_LINE.parse(f'{line_number} {atf}')
+        return ManuscriptLine(
+            manuscript_line['manuscriptId'],
+            tuple(Label.parse(label) for label in manuscript_line['labels']),
+            line
+        )
+
+
 def serialize(text: Text) -> dict:
     return ApiSerializer.serialize(text, True)
 
 
 def deserialize(dto: dict) -> Text:
-    def parse_manuscript(manuscript_dto: dict):
-        try:
-            atf_line_number =\
-                LineNumberLabel(manuscript_dto['number']).to_atf()
-            line = \
-                TEXT_LINE.parse(f'{atf_line_number} {manuscript_dto["atf"]}')
-        except (ParseError, ValueError) as error:
-            raise DataError(error)
-
-        return pydash.omit({
-            **manuscript_dto,
-            'line': line.to_dict()
-        }, 'atf')
-
-    parsed_media = {
-        **dto,
-        'chapters': [
-            {
-                **chapter,
-                'lines': [
-                    {
-                        **line,
-                        'manuscripts': [
-                            parse_manuscript(manuscript)
-                            for manuscript in line['manuscripts']
-                        ]
-                    } for line in chapter['lines']
-                ]
-            } for chapter in dto['chapters']
-        ]
-    }
     try:
-        return TextDeserializer.deserialize(parsed_media)
-    except ValueError as error:
+        return ApiDeserializer.deserialize(dto)
+    except (ValueError, ParseError) as error:
         raise DataError(error)
