ff6f2644f1d0d68173458350fdf62c94e17f1de3
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-05-14 14:05:01 +0200

    Use enum for word modifiers

--- ebl/tests/text/test_reconstructed_text_parser.py
+++ ebl/tests/text/test_reconstructed_text_parser.py
@@ -1,7 +1,8 @@
 import pytest
 from parsy import ParseError
 
-from ebl.text.reconstructed_text_parser import AKKADIAN_WORD, LACUNA, CAESURA
+from ebl.text.reconstructed_text_parser import AKKADIAN_WORD, LACUNA, \
+    CAESURA, FEET_SEPARATOR, Modifier
 
 
 def assert_parse(parser, expected, text):
@@ -16,26 +17,26 @@ def assert_parse_error(parser, text):
 @pytest.mark.parametrize('text,expected', [
     ('abcdefghiklmnopqrstuwyzâêîûāēīšūṣṭʾ',
      ['abcdefghiklmnopqrstuwyzâêîûāēīšūṣṭʾ']),
-    ('ibnû?', ['ibnû', '?']),
-    ('ibnû#', ['ibnû', '#']),
-    ('ibnû#?', ['ibnû', '#?']),
-    ('ibnû?#', ['ibnû', '?#']),
+    ('ibnû?', ['ibnû', [Modifier.UNCERTAIN]]),
+    ('ibnû#', ['ibnû', [Modifier.BROKEN]]),
+    ('ibnû#?', ['ibnû', [Modifier.BROKEN, Modifier.UNCERTAIN]]),
+    ('ibnû?#', ['ibnû', [Modifier.UNCERTAIN, Modifier.BROKEN]]),
     ('[ibnû]', ['[', 'ibnû', ']']),
     ('ib[nû', ['ib[nû']),
     ('ib]nû', ['ib]nû']),
     ('i[b]nû', ['i[b]nû']),
-    ('ibnû?]', ['ibnû', '?', ']']),
+    ('ibnû?]', ['ibnû', [Modifier.UNCERTAIN], ']']),
     ('(ibnû)', ['(', 'ibnû', ')']),
     ('ib(nû', ['ib(nû']),
     ('ib)nû', ['ib)nû']),
     ('i(b)nû', ['i(b)nû']),
-    ('ibnû#)', ['ibnû', '#', ')']),
+    ('ibnû#)', ['ibnû', [Modifier.BROKEN], ')']),
     ('[(ibnû)]', ['[(', 'ibnû', ')]']),
     ('ib[(nû', ['ib[(nû']),
     ('ib)]nû', ['ib)]nû']),
     ('i[(b)]nû', ['i[(b)]nû']),
     ('[i(b)n]û', ['[', 'i(b)n]û']),
-    ('ibnû?)]', ['ibnû', '?', ')]'])
+    ('ibnû?)]', ['ibnû', [Modifier.UNCERTAIN], ')]'])
 ])
 def test_word(text, expected):
     assert_parse(AKKADIAN_WORD, expected, text)
@@ -99,3 +100,19 @@ def test_caesura(text, expected):
 ])
 def test_invalid_caesura(text):
     assert_parse_error(CAESURA, text)
+
+
+@pytest.mark.parametrize('text,expected', [
+    ('|', '|'),
+    ('(|)', '(|)')
+])
+def test_feet_separator(text, expected):
+    assert FEET_SEPARATOR.parse(text) == expected
+
+
+@pytest.mark.parametrize('text', [
+    '||', '|||',
+    '[|]', '[(|)]'
+])
+def test_invalid_feet_separator(text):
+    assert_parse_error(FEET_SEPARATOR, text)
--- ebl/text/reconstructed_text_parser.py
+++ ebl/text/reconstructed_text_parser.py
@@ -1,16 +1,24 @@
-from parsy import char_from, seq, string, string_from
+from enum import Enum
+
+from parsy import char_from, seq, string, string_from, from_enum
+
+
+class Modifier(Enum):
+    BROKEN = '#'
+    UNCERTAIN = '?'
+
 
 BROKEN_LEFT = string_from('[(', '[', '(')
 BROKEN_RIGHT = string_from(')]', ']', ')')
 BROKEN = BROKEN_LEFT | BROKEN_RIGHT
 
 AKKADIAN_ALPHABET = char_from('abcdefghiklmnopqrstuwyzâêîûāēīšūṣṭʾ')
-MODIFIER = char_from('#?')
+MODIFIER = from_enum(Modifier)
 AKKADIAN_WORD = seq(
     BROKEN_LEFT.optional(),
     AKKADIAN_ALPHABET + ((BROKEN + AKKADIAN_ALPHABET) |
                          AKKADIAN_ALPHABET).many().concat(),
-    MODIFIER.at_most(2).concat(),
+    MODIFIER.at_most(2),
     BROKEN_RIGHT.optional()
 )
 
@@ -20,3 +28,5 @@ LACUNA = seq(BROKEN_LEFT.optional(),
 
 
 CAESURA = string_from('(||)', '||')
+
+FEET_SEPARATOR = string_from('(|)', '|')
