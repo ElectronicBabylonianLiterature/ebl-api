26fc82b7dd4604b87eac4c70ba13364dcd3c0a22
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-05-14 11:02:00 +0200

    Tokenize reconstructed words

---
+++ ebl/tests/text/test_reconstructed_text_parser.py
@@ -0,0 +1,47 @@
+import pytest
+from parsy import ParseError
+
+from text.reconstructed_text_parser import AKKADIAN_WORD
+
+
+@pytest.mark.parametrize('text,expected', [
+    ('abcdefghiklmnopqrstuwyzâêîûāēīšūṣṭʾ',
+     ['abcdefghiklmnopqrstuwyzâêîûāēīšūṣṭʾ']),
+    ('ibnû?', ['ibnû', '?']),
+    ('ibnû#', ['ibnû', '#']),
+    ('ibnû#?', ['ibnû', '#?']),
+    ('ibnû?#', ['ibnû', '?#']),
+    ('[ibnû]', ['[', 'ibnû', ']']),
+    ('ib[nû', ['ib[nû']),
+    ('ib]nû', ['ib]nû']),
+    ('i[b]nû', ['i[b]nû']),
+    ('ibnû?]', ['ibnû', '?', ']']),
+    ('(ibnû)', ['(', 'ibnû', ')']),
+    ('ib(nû', ['ib(nû']),
+    ('ib)nû', ['ib)nû']),
+    ('i(b)nû', ['i(b)nû']),
+    ('ibnû#)', ['ibnû', '#', ')']),
+    ('[(ibnû)]', ['[(', 'ibnû', ')]']),
+    ('ib[(nû', ['ib[(nû']),
+    ('ib)]nû', ['ib)]nû']),
+    ('i[(b)]nû', ['i[(b)]nû']),
+    ('[i(b)n]û', ['[', 'i(b)n]û']),
+    ('ibnû?)]', ['ibnû', '?', ')]']),
+])
+def test_word(text, expected):
+    assert [token for token in AKKADIAN_WORD.parse(text) if token] == expected
+
+
+@pytest.mark.parametrize('text', [
+    'x', 'KUR',
+    'ibnû!', 'ibnû?#?',
+    ']ibnû', 'ibnû[', '[[ibnû',
+    ')ibnû', 'ibnû(', '((ibnû',
+    'i([bnû', 'i])bnû', 'i[)]bnû',
+    'ibnû?[#', 'ibnû#)?',
+    'ibnû]?', 'ibnû)#', 'ibnû)]?'
+
+])
+def test_invalid_word(text):
+    with pytest.raises(ParseError):
+        AKKADIAN_WORD.parse(text)
---
+++ ebl/text/reconstructed_text_parser.py
@@ -0,0 +1,17 @@
+
+from parsy import char_from, seq
+
+from ebl.text.text_parser import INLINE_BROKEN_LEFT, INLINE_BROKEN, \
+    INLINE_BROKEN_RIGHT
+
+AKKADIAN_ALPHABET = char_from('abcdefghiklmnopqrstuwyzâêîûāēīšūṣṭʾ')
+MODIFIER = char_from('#?')
+
+AKKADIAN_WORD = seq(
+    INLINE_BROKEN_LEFT.optional(),
+    AKKADIAN_ALPHABET + (
+            (INLINE_BROKEN + AKKADIAN_ALPHABET) | AKKADIAN_ALPHABET
+    ).many().concat(),
+    MODIFIER.at_most(2).concat(),
+    INLINE_BROKEN_RIGHT.optional()
+)
