4e516f7c7c957f9a8e7991cccf0a3dda4bb6cb3b
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-08-21 13:10:59 +0200

    Upgrade Falcon to version 2

--- ebl/require_scope.py
+++ ebl/require_scope.py
@@ -1,6 +1,6 @@
 import falcon
 
 
-def require_scope(req, _resp, _resource, scope):
+def require_scope(req, _resp, _resource, _params, scope):
     if not req.context['user'].has_scope(scope):
         raise falcon.HTTPForbidden()
