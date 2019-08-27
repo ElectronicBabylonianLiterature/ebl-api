b4d980984c0fa5f31f01a4d026e0131f8d8705cf
Jussi Laasonen <jussi.laasonen@iki.fi>
2019-08-27 16:17:19 +0200

    Update word_cleaner.py

--- ebl/text/word_cleaner.py
+++ ebl/text/word_cleaner.py
@@ -3,16 +3,16 @@ import re
 from ebl.text.atf import ATF_EXTENSIONS, ATF_SPEC
 
 IGNORE = [
-        ATF_SPEC['lacuna']['begin'], # type: ignore
+        ATF_SPEC['lacuna']['begin'],  # type: ignore
         r'\(',
         r'\)',
-        ATF_SPEC['lacuna']['end'], # type: ignore
-        ATF_SPEC['flags']['uncertainty'], # type: ignore
-        ATF_SPEC['flags']['collation'], # type: ignore
-        ATF_SPEC['flags']['damage'], # type: ignore
-        ATF_SPEC['flags']['correction'], # type: ignore
-        ATF_EXTENSIONS['erasure_illegible'], # type: ignore
-        ATF_EXTENSIONS['erasure_boundary'] # type: ignore
+        ATF_SPEC['lacuna']['end'],  # type: ignore
+        ATF_SPEC['flags']['uncertainty'],  # type: ignore
+        ATF_SPEC['flags']['collation'],  # type: ignore
+        ATF_SPEC['flags']['damage'],  # type: ignore
+        ATF_SPEC['flags']['correction'],  # type: ignore
+        ATF_EXTENSIONS['erasure_illegible'],  # type: ignore
+        ATF_EXTENSIONS['erasure_boundary']  # type: ignore
     ]
 IGNORE_REGEX = f'({"|".join(IGNORE)})*'
 
