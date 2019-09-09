f3efba06ea0f83d16bcc8b8afa18ce981967992f
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-09-09 10:46:52 +0200

    Add type hints to dispatcher

--- ebl/dispatcher.py
+++ ebl/dispatcher.py
@@ -1,22 +1,30 @@
+from typing import Callable, Mapping, Tuple, TypeVar
+
+
 class DispatchError(Exception):
     pass
 
 
-def create_dispatcher(commands):
-    def get_parameter(parameters):
+T = TypeVar('T')
+Command = Callable[[str], T]
+Dispatcher = Callable[[dict], T]
+
+
+def create_dispatcher(commands: Mapping[str, Command]) -> Dispatcher:
+    def get_parameter(parameters: dict) -> Tuple[str, str]:
         if len(parameters) == 1:
             return next(iter(parameters.items()))
         else:
             raise DispatchError("Invalid number of parameters.")
 
-    def get_command(parameter):
-        if parameter in commands:
+    def get_command(parameter: str) -> Command:
+        try:
             return commands[parameter]
-        else:
+        except KeyError:
             raise DispatchError(f'Invalid parameter {parameter}.')
 
-    def execute_command(request):
-        parameter, value = get_parameter(request)
+    def execute_command(parameters: dict) -> T:
+        parameter, value = get_parameter(parameters)
         return get_command(parameter)(value)
 
     return execute_command
