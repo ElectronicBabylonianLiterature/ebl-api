bc1751b6039fc2743248870b47ea812088f7369a
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-09-10 10:09:23 +0200

    Extract factory methods

--- ebl/fragment/record.py
+++ ebl/fragment/record.py
@@ -4,6 +4,12 @@ from typing import Dict, List, Tuple
 
 import attr
 
+from ebl.auth0 import User
+
+
+def now() -> str:
+    return datetime.datetime.utcnow().isoformat()
+
 
 class RecordType(Enum):
     TRANSLITERATION = 'Transliteration'
@@ -16,7 +22,15 @@ class RecordType(Enum):
 class RecordEntry:
     user: str
     type: RecordType
-    date: str
+    date: str = attr.ib(factory=now)
+
+    @staticmethod
+    def transliteration(user: str) -> 'RecordEntry':
+        return RecordEntry(user, RecordType.TRANSLITERATION)
+
+    @staticmethod
+    def revision(user: str) -> 'RecordEntry':
+        return RecordEntry(user, RecordType.REVISION)
 
     def to_dict(self) -> Dict[str, str]:
         return {
@@ -33,11 +47,11 @@ class Record:
     def add_entry(self,
                   old_transliteration: str,
                   new_transliteration: str,
-                  user) -> 'Record':
+                  user: User) -> 'Record':
         if new_transliteration != old_transliteration:
             return Record((
                 *self.entries,
-                self._create_entry(old_transliteration, user)
+                self._create_entry(old_transliteration, user.ebl_name)
             ))
         else:
             return self
@@ -46,14 +60,7 @@ class Record:
         return [entry.to_dict() for entry in self.entries]
 
     @staticmethod
-    def _create_entry(old_transliteration: str, user) -> RecordEntry:
-        record_type = (
-            RecordType.REVISION
-            if old_transliteration
-            else RecordType.TRANSLITERATION
-        )
-        return RecordEntry(
-            user.ebl_name,
-            record_type,
-            datetime.datetime.utcnow().isoformat()
-        )
+    def _create_entry(old_transliteration: str, user: str) -> RecordEntry:
+        return (RecordEntry.revision(user)
+                if old_transliteration
+                else RecordEntry.transliteration(user))
