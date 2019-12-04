9d01f2b898251537bf19cacc86f7d660d2029d92
yCobanoglu <yunus.cobanoglu@campus.lmu.de>
2019-12-04 10:09:23 +0100

    assignments instead type annotations

--- ebl/fragmentarium/application/folio_pager_schema.py
+++ ebl/fragmentarium/application/folio_pager_schema.py
@@ -2,5 +2,5 @@ from marshmallow import Schema, fields
 
 
 class FolioPagerInfo(Schema):
-    previous: fields.String(required=True)
-    next: fields.String(required=True)
+    previous = fields.String(required=True)
+    next = fields.String(required=True)
