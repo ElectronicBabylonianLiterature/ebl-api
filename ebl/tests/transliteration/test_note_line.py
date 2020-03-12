fab2951cb1dab101afa9f97dc8d5d54440969b36
Jussi Laasonen <jussi.laasonen@lmu.de>
2020-03-12 13:33:42 +0100

    Introduce NoteLine

---
+++ ebl/tests/transliteration/test_note_line.py
@@ -0,0 +1,38 @@
+import pytest
+
+from ebl.transliteration.domain.language import Language
+from ebl.transliteration.domain.note_line import (
+    NoteLine,
+    StringPart,
+    EmphasisPart,
+    LanguagePart,
+)
+from ebl.transliteration.domain.tokens import ValueToken
+
+
+def test_note_line():
+    parts = [
+        StringPart("this is a note "),
+        EmphasisPart("italic text"),
+        LanguagePart("Akkadian language", Language.AKKADIAN),
+        LanguagePart("Sumerian language", Language.SUMERIAN),
+    ]
+    line = NoteLine(parts)
+
+    assert line.parts == parts
+    assert line.prefix == "#note: "
+    assert (
+        line.atf
+        == "#note: this is a note @it{italic text}@akk{Akkadian language}@sux{Sumerian language}"
+    )
+    assert line.content == [
+        ValueToken.of("this is a note "),
+        ValueToken.of("@it{italic text}"),
+        ValueToken.of("@akk{Akkadian language}"),
+        ValueToken.of("@sux{Sumerian language}"),
+    ]
+
+
+def test_invalid_language():
+    with (pytest.raises(ValueError)):
+        LanguagePart("bad language", Language.EMESAL)
