52e5eb20a036b63a42561f004427de8d2e4fb59a
Jussi Laasonen <jussi.laasonen@lmu.de>
2020-03-17 12:55:59 +0000

    Create value tokens with factory method

--- ebl/tests/transliteration/test_at_line.py
+++ ebl/tests/transliteration/test_at_line.py
@@ -17,28 +17,28 @@ def test_at_line_heading():
     at_line = HeadingAtLine(1)
 
     assert at_line.prefix == "@"
-    assert at_line.content == (ValueToken("h1"),)
+    assert at_line.content == (ValueToken.of("h1"),)
 
 
 def test_at_line_column():
     at_line = ColumnAtLine(ColumnLabel.from_int(1, (atf.Status.COLLATION,)))
 
     assert at_line.prefix == "@"
-    assert at_line.content == (ValueToken("column 1*"),)
+    assert at_line.content == (ValueToken.of("column 1*"),)
 
 
 def test_at_line_column_no_status():
     at_line = ColumnAtLine(ColumnLabel.from_int(1))
 
     assert at_line.prefix == "@"
-    assert at_line.content == (ValueToken("column 1"),)
+    assert at_line.content == (ValueToken.of("column 1"),)
 
 
 def test_at_line_discourse():
     at_line = DiscourseAtLine(atf.Discourse.SIGNATURES)
 
     assert at_line.prefix == "@"
-    assert at_line.content == (ValueToken("signatures"),)
+    assert at_line.content == (ValueToken.of("signatures"),)
     assert at_line.discourse_label == atf.Discourse.SIGNATURES
 
 
@@ -49,7 +49,7 @@ def test_at_line_surface():
     )
 
     assert at_line.prefix == "@"
-    assert at_line.content == (ValueToken("surface Stone wig!"),)
+    assert at_line.content == (ValueToken.of("surface Stone wig!"),)
     assert at_line.surface_label == SurfaceLabel(
         (atf.Status.CORRECTION,), atf.Surface.SURFACE, "Stone wig"
     )
@@ -60,7 +60,7 @@ def test_at_line_surface_no_status():
     at_line = SurfaceAtLine(SurfaceLabel([], atf.Surface.SURFACE, "Stone wig"))
 
     assert at_line.prefix == "@"
-    assert at_line.content == (ValueToken("surface Stone wig"),)
+    assert at_line.content == (ValueToken.of("surface Stone wig"),)
     assert at_line.surface_label == SurfaceLabel([], atf.Surface.SURFACE, "Stone wig")
 
 
@@ -71,7 +71,7 @@ def test_at_line_surface_instantiate_text_with_wrong_surface():
             SurfaceLabel((atf.Status.CORRECTION,), atf.Surface.OBVERSE, "Stone wig")
         )
         assert at_line.prefix == "@"
-        assert at_line.content == (ValueToken("obverse Stone wig!"),)
+        assert at_line.content == (ValueToken.of("obverse Stone wig!"),)
         assert at_line.surface_label == SurfaceLabel(
             (atf.Status.CORRECTION,), atf.Surface.OBVERSE, "Stone wig"
         )
@@ -81,7 +81,7 @@ def test_at_line_object_no_status():
     at_line = ObjectAtLine([], atf.Object.OBJECT, "Stone wig")
 
     assert at_line.prefix == "@"
-    assert at_line.content == (ValueToken("object Stone wig"),)
+    assert at_line.content == (ValueToken.of("object Stone wig"),)
     assert at_line.object_label == atf.Object.OBJECT
     assert at_line.status == []
     assert at_line.text == "Stone wig"
@@ -91,7 +91,7 @@ def test_at_line_object():
     at_line = ObjectAtLine([atf.Status.CORRECTION], atf.Object.OBJECT, "Stone wig")
 
     assert at_line.prefix == "@"
-    assert at_line.content == (ValueToken("object Stone wig!"),)
+    assert at_line.content == (ValueToken.of("object Stone wig!"),)
     assert at_line.object_label == atf.Object.OBJECT
     assert at_line.status == [atf.Status.CORRECTION]
     assert at_line.text == "Stone wig"
@@ -101,7 +101,7 @@ def test_at_line_composite():
     at_line = CompositeAtLine(atf.Composite.DIV, "paragraph", 1)
 
     assert at_line.prefix == "@"
-    assert at_line.content == (ValueToken("div paragraph 1"),)
+    assert at_line.content == (ValueToken.of("div paragraph 1"),)
     assert at_line.composite == atf.Composite.DIV
     assert at_line.text == "paragraph"
     assert at_line.number == 1
