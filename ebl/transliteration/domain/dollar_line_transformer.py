64d2719ced64f4ab0b44d0da8f68c1b817d13d0d
Jussi Laasonen <jussi.laasonen@iki.fi>
2020-03-20 11:51:59 +0000

    Move dollar-line transformer to own file

---
+++ ebl/transliteration/domain/dollar_line_transformer.py
@@ -0,0 +1,114 @@
+from lark.visitors import Transformer, v_args
+
+from ebl.transliteration.domain import atf
+from ebl.transliteration.domain.dollar_line import (
+    LooseDollarLine,
+    ImageDollarLine,
+    RulingDollarLine,
+    ScopeContainer,
+    StateDollarLine,
+    SealDollarLine,
+)
+
+
+class DollarLineTransfomer(Transformer):
+    def ebl_atf_dollar_line__free_text(self, content):
+        return "".join(content)
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__loose(self, content):
+        return LooseDollarLine(str(content))
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__ruling(self, number, status=None):
+        return RulingDollarLine(atf.Ruling(str(number)), status)
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__image(self, number, letter, text):
+        return ImageDollarLine(str(number), letter and str(letter), text)
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__seal(self, number):
+        return SealDollarLine(number)
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__DOLLAR_STATUS(self, status):
+        return atf.DollarStatus(str(status))
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__STATE(self, state):
+        return atf.State(str(state))
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__OBJECT(self, object):
+        return ScopeContainer(atf.Object(object))
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__generic_object(self, text):
+        return ScopeContainer(atf.Object.OBJECT, str(text))
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__fragment(self, text):
+        return ScopeContainer(atf.Object.FRAGMENT, str(text))
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__SURFACE(self, surface):
+        return ScopeContainer(atf.Surface.from_atf(str(surface)))
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__generic_surface(self, text):
+        return ScopeContainer(atf.Surface.SURFACE, str(text))
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__face(self, text):
+        return ScopeContainer(atf.Surface.FACE, str(text))
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__edge(self, text=""):
+        return ScopeContainer(atf.Surface.EDGE, str(text))
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__SCOPE(self, scope):
+        return ScopeContainer(atf.Scope(str(scope)))
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__EXTENT(self, extent):
+        return atf.Extent(str(extent))
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__INT(self, number):
+        return int(number)
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__range(self, number1, number2):
+        return (number1, number2)
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__QUALIFICATION(self, qualification):
+        return atf.Qualification(str(qualification))
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__state(
+        self, qualification, extent=None, scope_container=None, state=None, status=None
+    ):
+        return StateDollarLine(qualification, extent, scope_container, state, status)
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__state_extent(
+        self, extent, scope_container=None, state=None, status=None
+    ):
+        return StateDollarLine(None, extent, scope_container, state, status)
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__state_scope(
+        self, scope_container, state=None, status=None
+    ):
+        return StateDollarLine(None, None, scope_container, state, status)
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__state_state(self, state, status):
+        return StateDollarLine(None, None, None, state, status)
+
+    @v_args(inline=True)
+    def ebl_atf_dollar_line__state_status(self, status):
+        return StateDollarLine(None, None, None, None, status)
--- ebl/transliteration/domain/lark_parser.py
+++ ebl/transliteration/domain/lark_parser.py
@@ -13,14 +13,7 @@ from ebl.errors import DataError
 from ebl.transliteration.domain import atf
 from ebl.transliteration.domain.atf import sub_index_to_int
 from ebl.transliteration.domain.at_line_transformer import AtLineTransformer
-from ebl.transliteration.domain.dollar_line import (
-    LooseDollarLine,
-    ImageDollarLine,
-    RulingDollarLine,
-    ScopeContainer,
-    StateDollarLine,
-    SealDollarLine,
-)
+from ebl.transliteration.domain.dollar_line_transformer import DollarLineTransfomer
 from ebl.transliteration.domain.enclosure_error import EnclosureError
 from ebl.transliteration.domain.enclosure_tokens import (
     AccidentalOmission,
@@ -265,7 +258,7 @@ class TreeToWord(TreeToSign):
         )
 
 
-class TreeToLine(AtLineTransformer, NoteLineTransformer, TreeToWord):
+class TreeToLine(AtLineTransformer, DollarLineTransfomer, NoteLineTransformer, TreeToWord):
     def empty_line(self, _):
         return EmptyLine()
 
@@ -355,109 +348,6 @@ class TreeToLine(AtLineTransformer, NoteLineTransformer, TreeToWord):
         ]
 
 
-class TreeDollarSignToTokens(TreeToLine):
-    def ebl_atf_dollar_line__free_text(self, content):
-        return "".join(content)
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__loose(self, content):
-        return LooseDollarLine(str(content))
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__ruling(self, number, status=None):
-        return RulingDollarLine(atf.Ruling(str(number)), status)
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__image(self, number, letter, text):
-        return ImageDollarLine(str(number), letter and str(letter), text)
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__seal(self, number):
-        return SealDollarLine(number)
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__DOLLAR_STATUS(self, status):
-        return atf.DollarStatus(str(status))
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__STATE(self, state):
-        return atf.State(str(state))
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__OBJECT(self, object):
-        return ScopeContainer(atf.Object(object))
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__generic_object(self, text):
-        return ScopeContainer(atf.Object.OBJECT, str(text))
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__fragment(self, text):
-        return ScopeContainer(atf.Object.FRAGMENT, str(text))
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__SURFACE(self, surface):
-        return ScopeContainer(atf.Surface.from_atf(str(surface)))
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__generic_surface(self, text):
-        return ScopeContainer(atf.Surface.SURFACE, str(text))
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__face(self, text):
-        return ScopeContainer(atf.Surface.FACE, str(text))
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__edge(self, text=""):
-        return ScopeContainer(atf.Surface.EDGE, str(text))
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__SCOPE(self, scope):
-        return ScopeContainer(atf.Scope(str(scope)))
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__EXTENT(self, extent):
-        return atf.Extent(str(extent))
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__INT(self, number):
-        return int(number)
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__range(self, number1, number2):
-        return (number1, number2)
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__QUALIFICATION(self, qualification):
-        return atf.Qualification(str(qualification))
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__state(
-        self, qualification, extent=None, scope_container=None, state=None, status=None
-    ):
-        return StateDollarLine(qualification, extent, scope_container, state, status)
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__state_extent(
-        self, extent, scope_container=None, state=None, status=None
-    ):
-        return StateDollarLine(None, extent, scope_container, state, status)
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__state_scope(
-        self, scope_container, state=None, status=None
-    ):
-        return StateDollarLine(None, None, scope_container, state, status)
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__state_state(self, state, status):
-        return StateDollarLine(None, None, None, state, status)
-
-    @v_args(inline=True)
-    def ebl_atf_dollar_line__state_status(self, status):
-        return StateDollarLine(None, None, None, None, status)
-
-
 WORD_PARSER = Lark.open(
     "ebl_atf.lark", maybe_placeholders=True, rel_to=__file__, start="any_word"
 )
@@ -476,7 +366,7 @@ def parse_erasure(atf: str) -> Sequence[EblToken]:
 
 def parse_line(atf: str) -> Line:
     tree = LINE_PARSER.parse(atf)
-    return TreeDollarSignToTokens().transform(tree)
+    return TreeToLine().transform(tree)
 
 
 def validate_line(line: Line) -> None:
