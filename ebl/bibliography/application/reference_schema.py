f8bb0cebc35c1125d8cddd2293cb738a3f545876
Jussi Laasonen <jussi.laasonen@lmu.de>
2020-05-14 15:09:22 +0200

    Add serialization for BibliographyPart

---
+++ ebl/bibliography/application/reference_schema.py
@@ -0,0 +1,22 @@
+from marshmallow import Schema, fields, post_load  # pyre-ignore[21]
+from ebl.bibliography.domain.reference import (
+    BibliographyId,
+    Reference,
+    ReferenceType,
+)
+from ebl.schemas import NameEnum
+
+
+class ReferenceSchema(Schema):  # pyre-ignore[11]
+    id = fields.String(required=True)
+    type = NameEnum(ReferenceType, required=True)
+    pages = fields.String(required=True)
+    notes = fields.String(required=True)
+    lines_cited = fields.List(fields.String(), required=True, data_key="linesCited")
+    document = fields.Mapping(missing=None, load_only=True)
+
+    @post_load
+    def make_reference(self, data, **kwargs) -> Reference:
+        data["id"] = BibliographyId(data["id"])
+        data["lines_cited"] = tuple(data["lines_cited"])
+        return Reference(**data)
