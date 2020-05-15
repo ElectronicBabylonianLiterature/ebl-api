02c53ecd58242a667cd6cdaa4b8f80fbfca5bdd3
Jussi Laasonen <jussi.laasonen@lmu.de>
2020-05-15 12:51:23 +0000

    Use marshmallow schema for references

--- ebl/fragmentarium/web/references.py
+++ ebl/fragmentarium/web/references.py
@@ -1,32 +1,35 @@
+from typing import Sequence
+
 import falcon  # pyre-ignore
-from falcon.media.validators.jsonschema import validate
+from marshmallow import Schema, fields, post_load  # pyre-ignore[21]
 
 from ebl.bibliography.application.reference_schema import ReferenceSchema
-from ebl.bibliography.domain.reference import REFERENCE_DTO_SCHEMA
+from ebl.bibliography.domain.reference import Reference
 from ebl.fragmentarium.application.fragment_updater import FragmentUpdater
 from ebl.fragmentarium.web.dtos import create_response_dto
+from ebl.marshmallowschema import validate
 from ebl.users.web.require_scope import require_scope
 
-REFERENCES_DTO_SCHEMA = {
-    "type": "object",
-    "properties": {"references": {"type": "array", "items": REFERENCE_DTO_SCHEMA}},
-    "required": ["references"],
-}
+
+class ReferencesDtoSchema(Schema):  # pyre-ignore[11]
+    references = fields.Nested(ReferenceSchema, required=True, many=True)
+
+    @post_load
+    def get_references(self, data, **kwargs) -> Sequence[Reference]:
+        return tuple(data["references"])
 
 
 class ReferencesResource:
-    def __init__(self, updater: FragmentUpdater):
+    def __init__(self, updater: FragmentUpdater) -> None:
         self._updater = updater
 
     @falcon.before(require_scope, "transliterate:fragments")
-    @validate(REFERENCES_DTO_SCHEMA)
-    def on_post(self, req, resp, number):
+    @validate(ReferencesDtoSchema())
+    def on_post(self, req, resp, number) -> None:
         user = req.context.user
         updated_fragment, has_photo = self._updater.update_references(
             number,
-            tuple(
-                ReferenceSchema().load(req.media["references"], many=True)
-            ),
+            ReferencesDtoSchema().load(req.media),  # pyre-ignore[16]
             user,
         )
         resp.media = create_response_dto(updated_fragment, user, has_photo)
--- ebl/tests/transliteration/test_parse_note_line.py
+++ ebl/tests/transliteration/test_parse_note_line.py
@@ -2,10 +2,12 @@ import pytest  # pyre-ignore
 
 from ebl.transliteration.domain.language import Language
 from ebl.transliteration.domain.lark_parser import LINE_PARSER, parse_atf_lark
-from ebl.transliteration.domain.note_line import (EmphasisPart, LanguagePart,
+from ebl.transliteration.domain.note_line import (BibliographyPart,
+                                                  EmphasisPart, LanguagePart,
                                                   NoteLine, StringPart)
 from ebl.transliteration.domain.text import Text
-from ebl.transliteration.domain.text_line_transformer import TextLineTransformer
+from ebl.transliteration.domain.text_line_transformer import \
+    TextLineTransformer
 
 
 def parse_text(atf: str):
@@ -35,6 +37,26 @@ def expected_language_part(language: Language, transliteration: str) -> Language
             NoteLine([expected_language_part(Language.EMESAL, "kur"),]),
         ),
         (
+            "#note: @bib{RN123@x 2-3a}",
+            NoteLine([BibliographyPart.of("RN123", "x 2-3a")]),
+        ),
+        (
+            "#note: @bib{RN1}@2}",
+            NoteLine([BibliographyPart.of("RN1}", "2")]),
+        ),
+        (
+            "#note: @bib{RN1@1}2}",
+            NoteLine([BibliographyPart.of("RN1", "1}2")]),
+        ),
+        (
+            "#note: @bib{RN12@3@3}",
+            NoteLine([BibliographyPart.of("RN12@3", "3")]),
+        ),
+        (
+            "#note: @bib{RN@1}@2}",
+            NoteLine([BibliographyPart.of("RN", "1}@2")]),
+        ),
+        (
             (
                 "#note: this is a note "
                 "@i{italic text}@akk{kur}@sux{kur}"
@@ -50,5 +72,5 @@ def expected_language_part(language: Language, transliteration: str) -> Language
         ),
     ],
 )
-def test_parse_note_line(atf, expected_line):
+def test_parse_note_line(atf, expected_line) -> None:
     assert parse_atf_lark(atf).lines == Text.of_iterable([expected_line]).lines
