45ef54f5032592194a1f8f3377311540283482b3
Jussi Laasonen <404469+jlaasonen@users.noreply.github.com>
2020-09-02 09:56:10 +0000

    Refactor signs visitor test

--- ebl/tests/transliteration/test_signs_visitor.py
+++ ebl/tests/transliteration/test_signs_visitor.py
@@ -1,45 +1,22 @@
-from typing import cast, Sequence
+from typing import Sequence
 
 import pytest  # pyre-ignore[21]
 
 from ebl.transliteration.application.signs_visitor import SignsVisitor
 from ebl.transliteration.domain.lark_parser import parse_line
-from ebl.transliteration.domain.text_line import TextLine
-from ebl.transliteration.domain.tokens import Token, Variant
-from ebl.transliteration.domain.word_tokens import Word
-from ebl.transliteration.domain.sign_tokens import (
-    CompoundGrapheme, Divider, Grapheme, Logogram, Number, Reading
-)
-from ebl.transliteration.domain.unknown_sign_tokens import UnclearSign, UnidentifiedSign
 
 
-@pytest.mark.parametrize("tokens,expected",[
+@pytest.mark.parametrize("text,expected",[
     (
-        [
-            Word.of([Reading.of_name("ku")]),
-            Word.of([Reading.of_name("gid", 2)]),
-            Word.of([Reading.of_name("nu")]),
-            Word.of([Reading.of_name("ši")]),
-        ],
+        "ku gid₂ nu ši",
         ["KU", "BU", "ABZ075", "ABZ207a\\u002F207b\\u0020X"]
     ),
     (
-        [
-            Word.of([CompoundGrapheme.of(["(4×ZA)×KUR"])]),
-            Word.of([CompoundGrapheme.of(["(AŠ&AŠ@180)×U"])]),
-            Word.of([Logogram.of_name("NU")]),
-            Word.of([CompoundGrapheme.of(["GA₂#*+BAD!?"])]),
-            Word.of([CompoundGrapheme.of(["GA₂#*", "BAD!?"])]),
-        ],
+        "|(4×ZA)×KUR| |(AŠ&AŠ@180)×U| NU |GA₂#*+BAD!?| |GA₂#*.BAD!?|",
         ["ABZ531+588", "|(AŠ&AŠ@180)×U|", "ABZ075", "|GA₂+BAD|", "GA₂", "BAD"]
     ),
     (
-        [
-            Word.of([Reading.of_name("ummu", 3)]),
-            Word.of([CompoundGrapheme.of(["IGI", "KU"])]),
-            Word.of([Reading.of_name("mat", 3)]),
-            Word.of([Reading.of_name("kunga", 1)]),
-        ],
+        "ummu₃ |IGI.KU| mat₃ kunga",
         [
             "A",
             "ABZ168",
@@ -53,89 +30,52 @@ from ebl.transliteration.domain.unknown_sign_tokens import UnclearSign, Unidenti
         ],
     ),
     (
-        [
-            Word.of([Reading.of_name("unknown", 1)]),
-            Word.of([UnidentifiedSign.of()]),
-            Word.of([UnclearSign.of()]),
-        ],
+        "unknwn x X",
         ["?", "X", "X"],
     ),
     (
-        [
-            Word.of([Number.of_name("1", sign=Grapheme.of("AŠ"))]),
-            Word.of([Number.of_name("1")]),
-            Word.of([Number.of_name("2")]),
-            Word.of([Number.of_name("10")]),
-            Word.of([Number.of_name("20")]),
-            Word.of([Number.of_name("30")]),
-            Word.of([Number.of_name("256")]),
-        ],
+        "1(AŠ) 1 2 10 20 30 256",
         ["ABZ001", "DIŠ", "2", "ABZ411", "ABZ411", "ABZ411", "30", "256"],
     ),
     (
-        [
-            Divider.of(":"),
-            Divider.of("|"),
-        ],
+        "| :",
         ["ABZ377n1"],
     ),
     (
-        [
-            Variant.of(
-                Divider.of(":"), Reading.of_name("ku"),
-            ),
-            Word.of(
-                [
-                    Variant.of(
-                        Reading.of_name("šu"), CompoundGrapheme.of(["BI×IS"])
-                    ),
-                    Variant.of(
-                        Reading.of_name("ummu", 3),
-                        CompoundGrapheme.of(["IGI", "KU"]),
-                        Reading.of_name("mat", 3),
-                    ),
-                ],
-            ),
-        ],
+        ":/ku šu/|BI×IS|-ummu₃/|IGI.KU|/mat₃",
         ["ABZ377n1/KU", "ŠU/|BI×IS|", "|A.EDIN.LAL|/|IGI.KU|/ABZ081"],
     ),
     (
-        cast(TextLine, parse_line("1. ku-[nu ši]")).content,
+        "ku-[nu ši]",
         ["KU", "ABZ075", "ABZ207a\\u002F207b\\u0020X"],
     ),
     (
-        cast(TextLine, parse_line("1. < : ši>-ku")).content,
+        "< : ši>-ku",
         ["KU"],
     ),
     (
-        cast(TextLine, parse_line(
-            "1. ku-<<nu ši 1 |KU+KU| nu/ši nu(KU) x X ... : >>"
-        )).content,
+        "ku-<<nu ši 1 |KU+KU| nu/ši nu(KU) x X ... : >>",
         ["KU"],
     ),
     (
-        cast(TextLine, parse_line(
-            "1. ku-<nu ši 1 |KU+KU| nu/ši nu(KU) x X ... : >"
-        )).content,
+        "ku-<nu ši 1 |KU+KU| nu/ši nu(KU) x X ... : >",
         ["KU"],
     ),
     (
-        cast(TextLine, parse_line(
-            "1. ku-<(nu ši 1 |KU+KU| nu/ši nu(KU) x X ... : )>"
-        )).content,
+        "ku-<(nu ši 1 |KU+KU| nu/ši nu(KU) x X ... : )>",
         ["KU"],
     ),
     (
-        cast(TextLine, parse_line("1. °nu : ši\\ku°")).content,
+        "°nu : ši\\ku°",
         ["KU"],
     ),
     (
-        cast(TextLine, parse_line("1. ku-°|NU+NU|-1-nu-x-X-...-nu/ši\\ku°-ku")).content,
+        "ku-°|NU+NU|-1-nu-x-X-...-nu/ši\\ku°-ku",
         ["KU", "KU", "KU"],
     ),
 ])
 def test_signs_visitor(
-    tokens: Sequence[Token],
+    text: str,
     expected: Sequence[str],
     sign_repository, signs
 ):
@@ -143,7 +83,6 @@ def test_signs_visitor(
         sign_repository.create(sign)
 
     visitor = SignsVisitor(sign_repository)
-    for token in tokens:
-        token.accept(visitor)
+    parse_line(f"1. {text}").accept(visitor)
 
     assert visitor.result == expected
