fd4e4a40ddad9c78f24cea88073fb15215451be7
Jussi Laasonen <404469+jlaasonen@users.noreply.github.com>
2020-10-01 08:52:26 +0000

    Add enclosure types to normalized Akkadian

--- ebl/transliteration/domain/enclosure_visitor.py
+++ ebl/transliteration/domain/enclosure_visitor.py
@@ -179,6 +179,11 @@ class EnclosureUpdater(TokenVisitor):
         visited_parts: Sequence = self._visit_parts(named_sign.name_parts)
         self._append_token(attr.evolve(new_token, name_parts=visited_parts))
 
+    def visit_akkadian_word(self, word: AkkadianWord) -> None:
+        new_token = self._set_enclosure_type(word)
+        visited_parts: Sequence = self._visit_parts(word.parts)
+        self._append_token(attr.evolve(new_token, parts=visited_parts))
+
     def visit_gloss(self, gloss: Gloss) -> None:
         new_token = self._set_enclosure_type(gloss)
         visited_parts = self._visit_parts(gloss.parts)
@@ -217,6 +222,11 @@ class EnclosureUpdater(TokenVisitor):
     def visit_document_oriented_gloss(self, gloss: DocumentOrientedGloss) -> None:
         new_token = self._set_enclosure_type(gloss)
         self._update_enclosures(gloss, EnclosureType.DOCUMENT_ORIENTED_GLOSS)
+        self._append_token(new_token)
+
+    def visit_emendation(self, emendation: Emendation) -> None:
+        new_token = self._set_enclosure_type(emendation)
+        self._update_enclosures(emendation, EnclosureType.EMENDATION)
         self._append_token(new_token)
 
     def _visit_parts(self, tokens: Sequence[Token]) -> Sequence[Token]:
