02c9ad3a1cca50894ab145125cb2c1e73e183c4f
yCobanoglu <yunus.cobanoglu@campus.lmu.de>
2020-10-05 15:06:01 +0200

    mongosync script

--- pull-db.sh
+++ pull-db.sh
@@ -1,59 +1,42 @@
 #!/bin/bash
 
-defaultTempFolder="/tmp/pull-db"
-read -p "Download folder [$defaultTempFolder]: " tempFolder
-tempFolder=${tempFolder:-$defaultTempFolder}
+MONGO_HOST=<MONGO_HOST>
+MONGO_USER=<MONGO_USER>
+MONGO_PASSWORD=<MONGO_PASSWORD>
+MONGO_PRODUCTION_DB=<MONGO_PRODUCTION_DB>
+MONGO_DEVELOPEMENT_DB=<MONGO_DEVELOPEMENT_DB>
+DUMP_FOLDER=/tmp/pull-db/
+RESTORE_FOLDER=/tmp/pull-db/ebl
 
-defaultSourceHost=$PULL_DB_DEFAULT_SOURCE_HOST
-read -p "Source host [$defaultSourceHost]: " sourceHost
-sourceHost=${sourceHost:-$defaultSourceHost}
-
-defaultSourceDb="ebl"
-read -p "Database [$defaultSourceDb]: " sourceDb
-sourceDb=${sourceDb:-$defaultSourceDb}
-
-defaultSourceUser=$PULL_DB_DEFAULT_SOURCE_USER
-defaultSourcePassword=$PULL_DB_DEFAULT_SOURCE_PASSWORD
-read -p "Source user name [$defaultSourceUser]: " sourceUser
-read -sp "Source password [${defaultSourcePassword//?/*}]: " sourcePassword
-sourceUser=${sourceUser:-$defaultSourceUser}
-sourcePassword=${sourcePassword:-$defaultSourcePassword}
-echo
-
-
-defaultTargetHost="localhost:27017"
-read -p "Target host [$defaultTargetHost]: " targetHost
-targetHost=${targetHost:-$defaultTargetHost}
-
-read -p "Target user name (Leave blank if target has no authentication.): " targetUser
-if [ $targetUser ]
-then
-    read -sp "Target password:" targetPassword
-    echo
-fi
-
-mkdir -p $tempFolder
-
-mongodump -h $sourceHost \
-    -d $sourceDb \
+mongodump -h $MONGO_HOST \
+    -d $MONGO_PRODUCTION_DB \
     --forceTableScan \
+    --excludeCollectionsWithPrefix=fragments_backup \
+    --excludeCollectionsWithPrefix=fragments_bakcup \
+    --excludeCollectionsWithPrefix=bibliography_backup \
+    --excludeCollectionsWithPrefix=bibliography_backup \
+    --excludeCollectionsWithPrefix=orphan \
+    --excludeCollectionsWithPrefix=signs_backup \
+    --excludeCollectionsWithPrefix=signs_with_invalid \
+    --excludeCollectionsWithPrefix=texts_backup \
+    --excludeCollectionsWithPrefix=words_backup \
     --excludeCollection=changelog \
     --excludeCollection=folios.files \
     --excludeCollection=folios.chunks \
     --excludeCollection=photos.files \
     --excludeCollection=photos.chunks \
-    -u $sourceUser -p $sourcePassword \
+    -u $MONGO_USER -p $MONGO_PASSWORD \
     --ssl --sslAllowInvalidCertificates \
-    -o $tempFolder
+    -o $DUMP_FOLDER
 
 
-if [ $targetUser ]
-then
-    mongorestore -h $targetHost \
-        -u $targetUser -p $targetPassword \
-        --authenticationDatabase $sourceDb \
-        --drop \
-        $tempFolder
-else
-    mongorestore -h $targetHost --drop $tempFolder
-fi
+mongorestore -h rs-ebl1/lmkwitg-ebl01.srv.mwn.de:27017,lmkwitg-ebl02.srv.mwn.de:27018  \
+    -d $MONGO_DEVELOPEMENT_DB \
+     -u $MONGO_USER -p $MONGO_PASSWORD \
+    --ssl --sslAllowInvalidCertificates \
+    --authenticationDatabase ebl \
+    --drop \
+    $RESTORE_FOLDER
+
+
+rm -rf $RESTORE_FOLDER
