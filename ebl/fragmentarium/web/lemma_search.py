1778f28a90df086bb7f9f7ace2a229fe09990978
Jussi Laasonen <404469+jlaasonen@users.noreply.github.com>
2020-11-11 11:36:43 +0000

    Add normalized query parameter to lemma search route

--- ebl/fragmentarium/web/lemma_search.py
+++ ebl/fragmentarium/web/lemma_search.py
@@ -1,21 +1,33 @@
+from typing import Tuple
+
 import falcon  # pyre-ignore[21]
 
-from ebl.dispatcher import create_dispatcher
 from ebl.fragmentarium.application.fragment_finder import FragmentFinder
 from ebl.users.web.require_scope import require_scope
 
 
+def get_parameters(params: dict) -> Tuple[str, bool]:
+    try:
+        word = params["word"]
+        is_normalized = (
+            {"true": True, "false": False}[params["isNormalized"]]
+            if "isNormalized" in params
+            else False
+        )
+        return (word, is_normalized)
+    except KeyError:
+        raise falcon.HTTPUnprocessableEntity()
+
+
 class LemmaSearch:
     def __init__(self, finder: FragmentFinder):
-        self._dispatch = create_dispatcher(
-            {
-                frozenset(["word"]): lambda values: finder.find_lemmas(
-                    values["word"], False
-                )
-            }
-        )
+        self._finder = finder
 
     @falcon.before(require_scope, "read:fragments")
     @falcon.before(require_scope, "read:words")
     def on_get(self, req, resp):
-        resp.media = self._dispatch(req.params)
+        allowed_params = {"word", "isNormalized"}
+        if not set(req.params.keys()).issubset(allowed_params):
+            raise falcon.HTTPUnprocessableEntity()
+
+        resp.media = self._finder.find_lemmas(*get_parameters(req.params))
