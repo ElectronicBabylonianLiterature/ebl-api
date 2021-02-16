1c34ad67426aa31331f5e560f1c354b8c5fce440
Jussi Laasonen <404469+jlaasonen@users.noreply.github.com>
2021-02-16 14:44:51 +0000

    Format files

--- ebl/fragmentarium/application/fragment_repository.py
+++ ebl/fragmentarium/application/fragment_repository.py
@@ -41,9 +41,7 @@ class FragmentRepository(ABC):
         ...
 
     @abstractmethod
-    def query_path_of_the_pioneers(
-        self,
-    ) -> List[Fragment]:
+    def query_path_of_the_pioneers(self,) -> List[Fragment]:
         ...
 
     @abstractmethod
@@ -51,9 +49,7 @@ class FragmentRepository(ABC):
         ...
 
     @abstractmethod
-    def query_by_transliterated_not_revised_by_other(
-        self,
-    ) -> List[FragmentInfo]:
+    def query_by_transliterated_not_revised_by_other(self,) -> List[FragmentInfo]:
         ...
 
     @abstractmethod
