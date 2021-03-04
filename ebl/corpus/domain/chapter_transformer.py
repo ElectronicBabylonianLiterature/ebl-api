e62a9b3b48e916c84e755ecbb8cb44852c1a2201
Jussi Laasonen <404469+jlaasonen@users.noreply.github.com>
2021-03-04 08:43:07 +0000

    Add parser for manuscript line

--- ebl/corpus/domain/chapter_transformer.py
+++ ebl/corpus/domain/chapter_transformer.py
@@ -1,11 +1,13 @@
 from lark.visitors import v_args
 
 from ebl.corpus.domain.manuscript import Period, Provenance, ManuscriptType, Siglum
+from ebl.corpus.domain.chapter import ManuscriptLine
 from ebl.transliteration.domain.dollar_line_transformer import DollarLineTransfomer
 from ebl.transliteration.domain.note_line_transformer import NoteLineTransformer
 from ebl.transliteration.domain.parallel_line_transformer import ParallelLineTransformer
 from ebl.transliteration.domain.text_line_transformer import TextLineTransformer
 from ebl.transliteration.domain.labels import LabelTransformer
+from ebl.transliteration.domain.line import EmptyLine
 
 
 class ChapterTransformer(
@@ -26,3 +28,10 @@ class ChapterTransformer(
             ManuscriptType.from_abbreviation(type_ or ""),
             disambiquator or "",
         )
+
+    @v_args(inline=True)
+    def manuscript_line(self, siglum, labels, line, *paratext):
+        return ManuscriptLine(0, labels or tuple(), line, tuple(paratext))
+
+    def empty_line(self, _):
+        return EmptyLine()
--- ebl/tests/corpus/test_parse_chapter.py
+++ ebl/tests/corpus/test_parse_chapter.py
@@ -1,8 +1,15 @@
 import pytest
 
-from ebl.corpus.domain.manuscript import Period, Provenance, ManuscriptType, Siglum
+from ebl.corpus.domain.chapter import ManuscriptLine
 from ebl.corpus.domain.chapter_transformer import ChapterTransformer
-from ebl.transliteration.domain.lark_parser import CHAPTER_PARSER
+from ebl.corpus.domain.manuscript import ManuscriptType, Period, Provenance, Siglum
+from ebl.transliteration.domain.labels import parse_labels
+from ebl.transliteration.domain.lark_parser import (
+    CHAPTER_PARSER,
+    parse_paratext,
+    parse_text_line,
+)
+from ebl.transliteration.domain.line import EmptyLine
 
 
 def parse_siglum(siglum):
@@ -21,3 +28,38 @@ def test_parse_siglum(
     assert parse_siglum(
         f"{provenance.abbreviation}{period.abbreviation}{type_.abbreviation}{disambiquator}"
     ) == Siglum(provenance, period, type_, disambiquator)
+
+
+def parse_manuscript(atf):
+    tree = CHAPTER_PARSER.parse(atf, start="manuscript_line")
+    return ChapterTransformer().transform(tree)
+
+
+@pytest.mark.parametrize(
+    "lines,expected",
+    [
+        (
+            ["NinNA o iii 1. kur", "#note: a note", "$ single ruling"],
+            ManuscriptLine(
+                0,
+                parse_labels("o iii"),
+                parse_text_line("1. kur"),
+                (parse_paratext("#note: a note"), parse_paratext("$ single ruling")),
+            ),
+        ),
+        (["NinNA 1. kur"], ManuscriptLine(0, tuple(), parse_text_line("1. kur"))),
+        (
+            ["NinNA o iii", "#note: a note", "$ single ruling"],
+            ManuscriptLine(
+                0,
+                parse_labels("o iii"),
+                EmptyLine(),
+                (parse_paratext("#note: a note"), parse_paratext("$ single ruling")),
+            ),
+        ),
+        (["NinNA"], ManuscriptLine(0, tuple(), EmptyLine())),
+    ],
+)
+def test_parse_manuscript(lines, expected) -> None:
+    atf = "\n".join(lines)
+    assert parse_manuscript(atf) == expected
