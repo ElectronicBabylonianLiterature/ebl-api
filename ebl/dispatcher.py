33f8d441c7884b5d18dcfd280bb23f4096ba0665
Jussi Laasonen <404469+jlaasonen@users.noreply.github.com>
2021-04-15 10:31:45 +0000

    Update types in dispatcher

--- ebl/dispatcher.py
+++ ebl/dispatcher.py
@@ -1,4 +1,4 @@
-from typing import Callable, Mapping, TypeVar, Tuple, Dict, FrozenSet
+from typing import Callable, Mapping, TypeVar, Tuple, FrozenSet
 
 
 class DispatchError(Exception):
@@ -7,25 +7,25 @@ class DispatchError(Exception):
 
 T = TypeVar("T")
 Command = Callable[[Mapping[str, str]], T]
-Dispatcher = Callable[[dict], T]
+Dispatcher = Callable[[Mapping[str, str]], T]
 
 
 def get_parameter(
-    parameters: Dict[str, str]
+    parameters: Mapping[str, str]
 ) -> Tuple[FrozenSet[str], Mapping[str, str]]:
     parameter = frozenset(parameters.keys())
     values = parameters
     return parameter, values
 
 
-def create_dispatcher(commands: Mapping[FrozenSet[str], Command]) -> Dispatcher:
-    def get_command(parameter: FrozenSet[str]) -> Command:
+def create_dispatcher(commands: Mapping[FrozenSet[str], Command[T]]) -> Dispatcher[T]:
+    def get_command(parameter: FrozenSet[str]) -> Command[T]:
         try:
             return commands[parameter]
         except KeyError:
             raise DispatchError(f"Invalid parameter {parameter}.")
 
-    def dispatch(parameters: Dict[str, str]) -> T:
+    def dispatch(parameters: Mapping[str, str]) -> T:
         parameter, value = get_parameter(parameters)
         return get_command(parameter)(value)
 
