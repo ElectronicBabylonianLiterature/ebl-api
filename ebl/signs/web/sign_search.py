46da6609b2b16e975f95fb4048d537180a3f098c
yCobanoglu <yunus.cobanoglu@campus.lmu.de>
2021-06-15 14:20:56 +0200

    remove unsused import

--- ebl/signs/web/sign_search.py
+++ ebl/signs/web/sign_search.py
@@ -4,7 +4,6 @@ import falcon
 
 from ebl.dispatcher import create_dispatcher
 from ebl.errors import DataError
-from ebl.signs.infrastructure.menoizing_sign_repository import MemoizingSignRepository
 from ebl.signs.infrastructure.mongo_sign_repository import SignDtoSchema
 from ebl.transliteration.application.sign_repository import SignRepository
 from ebl.users.web.require_scope import require_scope
