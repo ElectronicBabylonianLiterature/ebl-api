82123704182351f82c287d9a472f6209a3e5ad4e
yCobanoglu <yunus.cobanoglu@campus.lmu.de>
2021-08-13 01:03:40 +0200

    types and code climate

--- ebl/fragmentarium/retrieve_annotations.py
+++ ebl/fragmentarium/retrieve_annotations.py
@@ -3,6 +3,7 @@ import os
 import shutil
 from io import BytesIO
 from os.path import join
+from typing import Sequence, Tuple, NewType
 from urllib.request import urlopen
 
 from PIL import Image
@@ -10,7 +11,10 @@ from PIL import Image
 from ebl.app import create_context
 from ebl.context import Context
 from ebl.files.application.file_repository import FileRepository
-from ebl.fragmentarium.application.fragment_repository import FragmentRepository
+from ebl.fragmentarium.domain.annotation import Annotations, Annotation
+from ebl.fragmentarium.domain.museum_number import MuseumNumber
+
+BBox = NewType("BBox", Tuple[float, float, float, float, float, float, float, float])
 
 
 def create_context_() -> Context:
@@ -22,11 +26,15 @@ def retrieve_annotations(context: Context):
     return annotation_repository.retrieve_all()
 
 
-def calculate_bbox_coordinates(x, y, width, height):
-    return [x, y, x + width, y, x + width, y + height, x, y + height]
+def calculate_bbox_coordinates(
+    x: float, y: float, width: float, height: float
+) -> Tuple[float, ...]:
+    return (x, y, x + width, y, x + width, y + height, x, y + height)
 
 
-def convert_bboxes(shape, annotations):
+def convert_bboxes(
+    shape: Tuple[int, ...], annotations: Sequence[Annotation]
+) -> Tuple[Tuple[float, ...], ...]:
     x_size = shape[0]
     y_size = shape[1]
     bboxes = []
@@ -36,28 +44,27 @@ def convert_bboxes(shape, annotations):
         new_width = int(round(bbox.geometry.width / 100 * x_size))
         new_height = int(round(bbox.geometry.height / 100 * y_size))
         bboxes.append(calculate_bbox_coordinates(new_x, new_y, new_width, new_height))
-    return bboxes
+    return tuple(bboxes)
 
 
 def create_annotations(
-    annotation_collection,
-    output_folder_annotations,
-    output_folder_images,
-    fragment_repository: FragmentRepository,
-    photo_repository: FileRepository,
-):
+    annotation_collection: Sequence[Annotations],
+    output_folder_annotations: str,
+    output_folder_images: str,
+    context: Context,
+) -> None:
     """
     creates icdar style annotations.
     Bounding boxes specified by x1,y1,x2,y2,x3,y3,x4,y4 clockwise vertices starting
     from top left.
-    Original format from react-annotation tool is bottom left vertice and height and
+    Original format from react-annotation tool is bottom left vertex and height and
     width values between 0 - 100 relative to image size.
-    We compare number of annotations to transliteration so we can exclude annotaions
+    We compare number of annotations to transliteration so we can exclude annotations
     which aren't complete.
     """
     for counter, single_annotation in enumerate(annotation_collection):
         fragment_number = single_annotation.fragment_number
-        fragment = fragment_repository.query_by_museum_number(
+        fragment = context.fragment_repository.query_by_museum_number(
             single_annotation.fragment_number
         )
         signs_count = len(
@@ -78,15 +85,17 @@ def create_annotations(
             fragment_number,
             fragment.cdli_number,
             output_folder_images,
-            photo_repository,
+            context.photo_repository,
         )
 
         bboxes = convert_bboxes(shape, single_annotation.annotations)
         write_annotations(output_folder_annotations, f"gt_{file_name_id}.txt", bboxes)
 
 
-def write_annotations(output_annotations, file_name, bboxes):
-    txt_file = join(output_annotations, file_name)
+def write_annotations(
+    output_folder_annotations: str, file_name: str, bboxes: Tuple[Tuple[float, ...]]
+) -> None:
+    txt_file = join(output_folder_annotations, file_name)
     with open(txt_file, "w+") as file:
         for bbox in bboxes:
             vertices = ",".join(map(str, bbox))
@@ -94,12 +103,13 @@ def write_annotations(output_annotations, file_name, bboxes):
 
 
 def save_image(
-    fragment_number, cdli_number, output_folder, photo_repository: FileRepository
-):
+    fragment_number: MuseumNumber,
+    cdli_number: str,
+    output_folder: str,
+    photo_repository: FileRepository,
+) -> Tuple[str, Tuple[int, ...]]:
     """
     Some images are in our database, some aren't so we have to retrieve them from CDLI.
-    Images from CDLI can have different shape than ours so if we can we try to retrieve
-    them from our db.
     """
     image_filename = f"{fragment_number}.jpg"
     if photo_repository.query_if_file_exists(image_filename):
@@ -115,7 +125,7 @@ def save_image(
     return file_name_id, image.size
 
 
-def create_directory(path):
+def create_directory(path: str) -> None:
     if os.path.exists(path):
         shutil.rmtree(path)
     os.makedirs(path)
@@ -150,10 +160,6 @@ if __name__ == "__main__":
     context = create_context()
     annotation_collection = retrieve_annotations(context)
     create_annotations(
-        annotation_collection,
-        args.output_annotations,
-        args.output_imgs,
-        context.fragment_repository,
-        context.photo_repository,
+        annotation_collection, args.output_annotations, args.output_imgs, context
     )
     print("Done")
