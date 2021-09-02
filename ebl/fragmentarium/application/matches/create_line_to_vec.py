70117e7e44393b29bd6f54f1affc8bed07d668cc
Jussi Laasonen <404469+jlaasonen@users.noreply.github.com>
2021-09-02 09:43:47 +0000

    Simplify _parse_text_line_line_number_range

--- ebl/fragmentarium/application/matches/create_line_to_vec.py
+++ ebl/fragmentarium/application/matches/create_line_to_vec.py
@@ -65,10 +65,8 @@ def _parse_text_line(_: LineNumber) -> LineToVecEncodings:
 def _parse_text_line_line_number_range(
     line_number: LineNumberRange,
 ) -> LineToVecEncodings:
-    return tuple(
-        LineToVecEncoding.TEXT_LINE
-        for _ in range(line_number.start.number, line_number.end.number + 1)
-    )
+    number_of_lines = line_number.end.number - line_number.start.number + 1
+    return (LineToVecEncoding.TEXT_LINE,) * number_of_lines
 
 
 @singledispatch
