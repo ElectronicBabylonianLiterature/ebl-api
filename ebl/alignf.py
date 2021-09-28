7d2b9c48b362a4b380156746c0c155a6a608fabe
Jussi Laasonen <404469+jlaasonen@users.noreply.github.com>
2021-09-28 12:11:41 +0000

    Remove workflows

---
+++ ebl/alignf.py
@@ -0,0 +1,73 @@
+import sys
+import time
+
+from alignment.sequence import Sequence
+from alignment.vocabulary import Vocabulary
+from alignment.sequencealigner import SimpleScoring, GlobalSequenceAligner
+
+from ebl.app import create_context
+from ebl.transliteration.domain.genre import Genre
+from ebl.corpus.domain.chapter import ChapterId, TextId, Stage
+
+sys.setrecursionlimit(10000)
+
+context = create_context()
+repository = context.text_repository
+chapter = repository.find_chapter(ChapterId(TextId(Genre.LITERATURE, 1, 2),Stage.STANDARD_BABYLONIAN, "I"))
+
+fragments = []
+for manuscript in chapter.manuscripts:
+    if manuscript.museum_number is not None:
+        try:
+            fragment = context.fragment_repository.query_by_museum_number(manuscript.museum_number)
+            fragments.append((fragment.number, fragment.signs))
+        except Exception:
+            pass
+
+t0 = time.time()
+v = Vocabulary()
+
+fsequences = [
+    (number, v.encodeSequence(Sequence(string.replace("\n", " # ").split(" "))))
+    for number, string in fragments
+]
+
+sequences = [
+    (chapter.manuscripts[index].siglum, v.encodeSequence(Sequence(string.replace("\n", " # ").split(" "))))
+    for index, string in enumerate(chapter.signs)
+]
+
+print(chapter.id_, end="\n\n")
+
+no_alignment = []
+x = 1
+for a in fsequences:
+    for b in sequences:
+        aEncoded = a[1]
+        bEncoded = b[1]
+
+        # Create a scoring and align the sequences using global aligner.
+        scoring = SimpleScoring(2, -1)
+        aligner = GlobalSequenceAligner(scoring, -2)
+        score, encodeds = aligner.align(aEncoded, bEncoded, backtrace=True)
+
+        if score > 0:
+            print(f"\n{x} == {a[0]} vs {b[0]} =============================================")
+            # Iterate over optimal alignments and print them.
+            for encoded in encodeds:
+                alignment = v.decodeSequenceAlignment(encoded)
+                print(alignment)
+                print("Alignment score:", alignment.score)
+                print("Percent identity:", alignment.percentIdentity())
+                print()
+        else:
+            no_alignment.append((a[0], b[0]))
+        x += 1
+
+t = time.time()
+
+print(f"\nNo aligment found ({len(no_alignment)}):")
+for a,b in no_alignment:
+    print(a, ", ", b)
+
+print(f"\n\nTime: {(t-t0)/60} min")
