5d8575cc357f50c6583643f65ae489eebed10c5f
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2022-04-26 13:41:44 +0000

    Refactor field population

--- ebl/corpus/web/display_schemas.py
+++ ebl/corpus/web/display_schemas.py
@@ -1,56 +1,45 @@
 from marshmallow import Schema, fields
 from ebl.transliteration.application.one_of_line_schema import OneOfLineSchema
-from ebl.corpus.application.schemas import (
-    labels,
-    OldSiglumSchema,
-)
+from ebl.corpus.application.schemas import labels
 from ebl.corpus.web.chapter_schemas import ApiOldSiglumSchema
 from ebl.bibliography.application.reference_schema import ApiReferenceSchema
+from operator import attrgetter
 
 
-def get_old_sigla(line, context):
-    m = context["manuscripts"][line.manuscript_id]
-    sigs = m.old_sigla
-
-    return OldSiglumSchema().dump(sigs, many=True)
+def get_manuscript_field(field_name):
+    return lambda line, context: attrgetter(field_name)(
+        context["manuscripts"][line.manuscript_id]
+    )
 
 
 class ManuscriptLineDisplaySchema(Schema):
     old_sigla = fields.Function(
         lambda line, context: ApiOldSiglumSchema().dump(
-            context["manuscripts"][line.manuscript_id].old_sigla, many=True
+            get_manuscript_field("old_sigla")(line, context), many=True
         ),
         data_key="oldSigla",
     )
     references = fields.Function(
         lambda line, context: ApiReferenceSchema().dump(
-            context["manuscripts"][line.manuscript_id].references, many=True
+            get_manuscript_field("references")(line, context), many=True
         ),
     )
     siglum_disambiguator = fields.Function(
-        lambda line, context: context["manuscripts"][
-            line.manuscript_id
-        ].siglum_disambiguator,
+        get_manuscript_field("siglum_disambiguator"),
         data_key="siglumDisambiguator",
     )
     period_modifier = fields.Function(
-        lambda line, context: context["manuscripts"][
-            line.manuscript_id
-        ].period_modifier.value,
+        get_manuscript_field("period_modifier.value"),
         data_key="periodModifier",
     )
     period = fields.Function(
-        lambda line, context: context["manuscripts"][
-            line.manuscript_id
-        ].period.long_name,
+        get_manuscript_field("period.long_name"),
     )
     provenance = fields.Function(
-        lambda line, context: context["manuscripts"][
-            line.manuscript_id
-        ].provenance.long_name,
+        get_manuscript_field("provenance.long_name"),
     )
     type = fields.Function(
-        lambda line, context: context["manuscripts"][line.manuscript_id].type.long_name,
+        get_manuscript_field("type.long_name"),
     )
     labels = labels()
     line = fields.Nested(OneOfLineSchema, required=True)
