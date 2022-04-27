7d9e2342c0a3b7ef1aeeb8f3865b995a349b79b3
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2022-04-27 13:52:58 +0000

    Refactoring

--- ebl/tests/corpus/test_chapter_schema.py
+++ ebl/tests/corpus/test_chapter_schema.py
@@ -87,7 +87,7 @@ def strip_documents(chapter: Chapter) -> Chapter:
 
 def get_museum_number(manuscript: Manuscript, include_documents: bool):
     if include_documents:
-        return str(getattr(manuscript, "museum_number", ""))
+        return str(manuscript.museum_number) if manuscript.museum_number else ""
 
     return manuscript.museum_number and MuseumNumberSchema().dump(
         manuscript.museum_number
