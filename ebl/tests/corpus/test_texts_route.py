8429387105a72f96ddf797bcaa4da835c3ee4ed8
yCobanoglu <yunus.cobanoglu@campus.lmu.de>
2022-07-14 15:31:15 +0200

    fix merge

--- ebl/tests/corpus/test_texts_route.py
+++ ebl/tests/corpus/test_texts_route.py
@@ -60,7 +60,7 @@ def test_searching_texts(client, bibliography, sign_repository, signs, text_repo
     allow_references(chapter, bibliography)
     text_repository.create_chapter(chapter)
 
-    get_result = client.simulate_get("/textsearch?transliteration=ku")
+    get_result = client.simulate_get("/textsearch?transliteration=ku&paginationIndex=0")
 
     assert get_result.status == falcon.HTTP_OK
     assert get_result.json == {
