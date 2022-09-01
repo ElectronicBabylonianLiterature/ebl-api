cd86a10d56d1234636746bb00a5492ca8dcdf1f1
yCobanoglu <yunus.cobanoglu@campus.lmu.de>
2022-09-01 15:19:33 +0200

    revert file change similar to master

--- ebl/tests/corpus/test_chapter_info.py
+++ ebl/tests/corpus/test_chapter_info.py
@@ -2,7 +2,6 @@ from typing import cast
 
 from ebl.corpus.domain.chapter import Chapter
 from ebl.corpus.domain.chapter_info import ChapterInfo
-from ebl.corpus.web.chapter_info_schema import ChapterInfoSchema
 from ebl.tests.factories.corpus import ChapterFactory
 from ebl.transliteration.domain.text_line import TextLine
 from ebl.transliteration.domain.transliteration_query import TransliterationQuery
@@ -21,11 +20,3 @@ def test_of() -> None:
             ]
         },
     )
-
-def test_chapter_info_schema():
-    chapter: Chapter = ChapterFactory.build()
-    query = TransliterationQuery([["KU"]])
-    chapter = ChapterInfo.of(chapter, query)
-    x = ChapterInfoSchema().dump(chapter)
-    assert ChapterInfoSchema().dump(chapter) == {}
-
