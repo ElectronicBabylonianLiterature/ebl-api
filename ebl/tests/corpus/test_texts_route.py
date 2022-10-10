72028f490f11c21ad45e350eeb1f287d84e92ffb
Ilya Khait <ilya.khait@lmu.de>
2022-10-10 14:02:01 +0200

    Transliteration search wildcards (patch) (#296)
    * Add ToDos
    
    * Extend transliteration query (WiP)
    
    * Extend and refactor `TransliterationQuery` (WiP)
    
    * Fix tests (WiP)
    
    * Fix tests (WiP)
    
    * Fix most tests (5 more to go)
    
    * Reformat
    
    * Fix tests
    
    * Format
    
    * 'Refactored by Sourcery' (#291)
    
    Co-authored-by: Sourcery AI <>
    
    * Refactor
    
    * Update and refactor
    
    * 'Refactored by Sourcery' (#294)
    
    Co-authored-by: Sourcery AI <>
    
    * Clean up
    
    * Update merged
    
    * Correct query & update tests
    
    * Refactor
    
    * Fix cyclic import warnings (WiP)
    
    * Switch to `visitor` & update
    
    * 'Refactored by Sourcery' (#295)
    
    Co-authored-by: Sourcery AI <>
    
    * Clean up
    
    * Patch & update tests
    
    * Format
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- ebl/tests/corpus/test_texts_route.py
+++ ebl/tests/corpus/test_texts_route.py
@@ -43,7 +43,7 @@ def test_invalid_index(client):
     assert result.status == falcon.HTTP_NOT_FOUND
 
 
-def test_listing_texts(client, bibliography, sign_repository, signs, text_repository):
+def test_listing_texts(client, bibliography, text_repository):
     first_text = TextFactory.build(chapters=tuple(), references=tuple())
     second_text = TextFactory.build(chapters=tuple(), references=tuple())
     text_repository.create(first_text)
