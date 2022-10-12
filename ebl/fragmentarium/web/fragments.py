f0271a3906ed6fd15f94f6aac3fae6b1aa016769
Ilya Khait <ilya.khait@lmu.de>
2022-10-12 16:36:14 +0000

    Add scope check (WiP)

--- ebl/fragmentarium/web/fragments.py
+++ ebl/fragmentarium/web/fragments.py
@@ -1,5 +1,6 @@
 import falcon
 from falcon import Request, Response
+from typing import Sequence
 
 from ebl.fragmentarium.application.fragment_finder import FragmentFinder
 from ebl.fragmentarium.web.dtos import create_response_dto, parse_museum_number
@@ -7,12 +8,27 @@ from ebl.users.domain.user import User
 from ebl.users.web.require_scope import require_scope
 
 
+def check_fragment_scope(user: User, scopes: Sequence[str]):
+    for scope_class in scopes:
+        scope = f"read:{scope_class}-fragments"
+        if not user.has_scope(scope):
+            raise falcon.HTTPForbidden()
+
+
+SCOPES = ["caic", "sipparlibrary", "uruklbu"]
+
+
 class FragmentsResource:
     def __init__(self, finder: FragmentFinder):
         self._finder = finder
 
     @falcon.before(require_scope, "read:fragments")
-    def on_get(self, req: Request, resp: Response, number: str):
+    def on_get(
+        self, req: Request, resp: Response, number: str
+    ):
+        scopes: Sequence[str] = []  # ToDo: retrieve scope from Fragment
+        if scopes:
+            check_fragment_scope(req.context.user, scopes)
         user: User = req.context.user
         fragment, has_photo = self._finder.find(parse_museum_number(number))
         resp.media = create_response_dto(fragment, user, has_photo)
