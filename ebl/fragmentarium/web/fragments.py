a67202d464eee796798ee74034f85c4c9533330d
Ilya Khait <ilya.khait@lmu.de>
2022-10-13 09:46:53 +0000

    Correct type

--- ebl/fragmentarium/web/fragments.py
+++ ebl/fragmentarium/web/fragments.py
@@ -6,9 +6,10 @@ from ebl.fragmentarium.application.fragment_finder import FragmentFinder
 from ebl.fragmentarium.web.dtos import create_response_dto, parse_museum_number
 from ebl.users.domain.user import User
 from ebl.users.web.require_scope import require_scope
+from ebl.fragmentarium.domain.fragment import Scope
 
 
-def check_fragment_scope(user: User, scopes: Sequence[str]):
+def check_fragment_scope(user: User, scopes: Sequence[Scope]):
     for scope_class in scopes:
         scope = f"read:{scope_class}-fragments"
         if not user.has_scope(scope):
