b73be0241ac0ce8a3a632fcac6fdb962bcfa249d
yCobanoglu <yunus.cobanoglu@campus.lmu.de>
2022-11-02 13:41:24 +0100

    format

--- ebl/fragmentarium/application/annotations_service.py
+++ ebl/fragmentarium/application/annotations_service.py
@@ -26,6 +26,7 @@ from ebl.users.domain.user import User
 
 Image.MAX_IMAGE_PIXELS = None  # pyre-ignore[9]
 
+
 @attr.attrs(auto_attribs=True, frozen=True)
 class AnnotationsService:
     _ebl_ai_client: EblAiClient
