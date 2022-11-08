8d8c264bb3631508e03ecd772607c0436de05f11
Fabian Simonjetz <fabian.simonjetz@rub.de>
2022-11-08 13:10:02 +0000

    don't show '@' if reference has no pages

--- ebl/transliteration/domain/markup.py
+++ ebl/transliteration/domain/markup.py
@@ -129,7 +129,8 @@ class BibliographyPart(MarkupPart):
     def value(self) -> str:
         id = escape(self.reference.id)
         pages = escape(self.reference.pages)
-        return f"@bib{{{id}@{pages}}}"
+        pages = '@'+pages if pages else pages
+        return f"@bib{{{id}{pages}}}"
 
     @staticmethod
     def of(id: BibliographyId, pages: str) -> "BibliographyPart":
