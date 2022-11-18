0f1187d192e6530894ce7e064796b72ece9dd6d5
Fabian Simonjetz <fabian.simonjetz@rub.de>
2022-11-18 16:06:15 +0000

    add update_many method

--- ebl/mongo_collection.py
+++ ebl/mongo_collection.py
@@ -73,6 +73,10 @@ class MongoCollection:
         else:
             return result
 
+    def update_many(self, query, update, **kwargs):
+        result = self.__get_collection().update_many(query, update, **kwargs)
+        return result
+
     def count_documents(self, query) -> int:
         return self.__get_collection().count_documents(query)
 
