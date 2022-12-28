2dd6e75c43a7ff36d5e5fb6753e670f10958e436
Ilya Khait <ilya.khait@lmu.de>
2022-12-28 18:31:02 +0000

    Update data & tests

--- ebl/tests/corpus/test_chapter_display_schema.py
+++ ebl/tests/corpus/test_chapter_display_schema.py
@@ -6,19 +6,33 @@ from ebl.tests.factories.corpus import (
     ChapterFactory,
     TextFactory,
 )
-from ebl.transliteration.application.line_number_schemas import OneOfLineNumberSchema
-from ebl.transliteration.application.line_schemas import NoteLineSchema
+from ebl.transliteration.application.line_number_schemas import (
+    OneOfLineNumberSchema,
+    OldLineNumberSchema,
+)
+
+from ebl.transliteration.application.line_schemas import (
+    NoteLineSchema,
+    TranslationLineSchema,
+)
 from ebl.transliteration.application.note_line_part_schemas import (
     OneOfNoteLinePartSchema,
 )
+from ebl.transliteration.application.one_of_line_schema import (
+    ParallelLineSchema,
+)
 from ebl.transliteration.application.token_schemas import OneOfTokenSchema
+from ebl.corpus.application.record_schemas import RecordSchema
+from ebl.corpus.application.schemas import ManuscriptSchema, ManuscriptLineSchema
 
 
 CHAPTER_DISPLAY = ChapterDisplay.of_chapter(TextFactory.build(), ChapterFactory.build())
 
 
-def to_dict(chapter: ChapterDisplay, missing_translation: bool = False) -> dict:
-    return {
+def to_dict(
+    chapter: ChapterDisplay, missing_translation: bool = False, for_loading=False
+) -> dict:
+    chapter_display = {
         "id": {
             "stage": chapter.id_.stage.value,
             "name": chapter.id_.name,
@@ -34,6 +48,9 @@ def to_dict(chapter: ChapterDisplay, missing_translation: bool = False) -> dict:
         "lines": [
             {
                 "number": OneOfLineNumberSchema().dump(line.number),
+                "oldLineNumbers": OldLineNumberSchema().dump(
+                    line.old_line_numbers, many=True
+                ),
                 "isSecondLineOfParallelism": line.is_second_line_of_parallelism,
                 "isBeginningOfSection": line.is_beginning_of_section,
                 "variants": [
@@ -44,17 +61,34 @@ def to_dict(chapter: ChapterDisplay, missing_translation: bool = False) -> dict:
                         "reconstruction": OneOfTokenSchema().dump(
                             variant.reconstruction, many=True
                         ),
-                        "note": variant.note and NoteLineSchema().dump(variant.none),
+                        "note": variant.note and NoteLineSchema().dump(variant.note),
+                        "manuscripts": ManuscriptLineSchema().dump(
+                            variant.manuscripts, many=True
+                        ),
+                        "parallelLines": ParallelLineSchema().dump(
+                            variant.parallel_lines, many=True
+                        ),
                     }
                     for variant in line.variants
                 ],
-                "translation": None
+                "translation": []
                 if missing_translation
-                else OneOfNoteLinePartSchema().dump(line.translation, many=True),
+                else TranslationLineSchema().dump(line.translation, many=True),
             }
             for line in chapter.lines
         ],
+        "record": RecordSchema().dump(chapter.record),
+        "manuscripts": ManuscriptSchema().dump(chapter.manuscripts, many=True),
+        "atf": chapter.atf,
     }
+    if for_loading:
+        del chapter_display["atf"]
+        for manuscript_index, manuscript in enumerate(CHAPTER_DISPLAY.manuscripts):
+            for reference_index, reference in enumerate(manuscript.references):
+                chapter_display["manuscripts"][manuscript_index]["references"][
+                    reference_index
+                ]["document"] = reference.document
+    return chapter_display
 
 
 def test_dump():
@@ -65,14 +99,22 @@ def test_dump():
 
 
 def test_load():
-    assert ChapterDisplaySchema().load(to_dict(CHAPTER_DISPLAY)) == CHAPTER_DISPLAY
+    assert (
+        ChapterDisplaySchema().load(to_dict(CHAPTER_DISPLAY, for_loading=True))
+        == CHAPTER_DISPLAY
+    )
 
 
-def test_load_missing_dat():
+def test_load_missing_data():
+    lines = list(CHAPTER_DISPLAY.lines)
+    lines[0] = attr.evolve(CHAPTER_DISPLAY.lines[0], translation=())
     chapter_display = attr.evolve(
         CHAPTER_DISPLAY,
-        lines=(attr.evolve(CHAPTER_DISPLAY.lines[0], translation=tuple())),
+        lines=tuple(lines),
     )
     assert (
-        ChapterDisplaySchema().load(to_dict(chapter_display, True)) == chapter_display
+        ChapterDisplaySchema().load(
+            to_dict(chapter_display, missing_translation=True, for_loading=True)
+        )
+        == chapter_display
     )
