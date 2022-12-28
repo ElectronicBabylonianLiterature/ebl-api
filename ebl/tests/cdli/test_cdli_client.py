bcdb7d6deb4b551301c9dbf0c50e3bdf32eb6643
Enrique Jiménez <39047733+ejimsan@users.noreply.github.com>
2022-12-28 10:46:05 +0000

    Update CDLI links to point to Development site (#355)
    * Update CDLI links to point to Development site
    
    * FIx format

--- ebl/tests/cdli/test_cdli_client.py
+++ ebl/tests/cdli/test_cdli_client.py
@@ -5,11 +5,17 @@ import ebl.cdli.infrastructure.cdli_client as client
 
 CDLI_NUMBER = "P397611"
 METHODS = [
-    (client.get_photo_url, f"https://cdli.ucla.edu/dl/photo/{CDLI_NUMBER}.jpg"),
-    (client.get_line_art_url, f"https://cdli.ucla.edu/dl/lineart/{CDLI_NUMBER}_l.jpg"),
+    (
+        client.get_photo_url,
+        f"https://cdli.mpiwg-berlin.mpg.de/dl/photo/{CDLI_NUMBER}.jpg",
+    ),
+    (
+        client.get_line_art_url,
+        f"https://cdli.mpiwg-berlin.mpg.de/dl/lineart/{CDLI_NUMBER}_l.jpg",
+    ),
     (
         client.get_detail_line_art_url,
-        f"https://cdli.ucla.edu/dl/lineart/{CDLI_NUMBER}_ld.jpg",
+        f"https://cdli.mpiwg-berlin.mpg.de/dl/lineart/{CDLI_NUMBER}_ld.jpg",
     ),
 ]
 STATUSES = {200: True, 404: False, 503: False}
