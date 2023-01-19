32fc1491da1d6c6e9974915c2eb63de400825656
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-01-19 12:56:37 +0000

    extract query parameter processing

---
+++ ebl/common/query/parameter_parser.py
@@ -0,0 +1,89 @@
+from typing import Sequence, Dict, Callable
+from ebl.common.query.query_result import LemmaQueryType
+from ebl.errors import DataError
+from ebl.transliteration.application.transliteration_query_factory import (
+    TransliterationQueryFactory,
+)
+
+
+def parse_integer_field(field: str) -> Callable[[Dict], Dict]:
+    def parse_integer(parameters: Dict) -> Dict:
+        if field not in parameters:
+            return parameters
+
+        value = parameters[field]
+
+        try:
+            return {**parameters, field: int(value)}
+        except ValueError as error:
+            raise DataError(
+                f"{field} must be integer, got {value!r} instead"
+            ) from error
+
+    return parse_integer
+
+
+def parse_lines(lines: Sequence[str]) -> Sequence[int]:
+    try:
+        return [int(line) for line in lines]
+    except ValueError as error:
+        raise DataError(
+            f"lines must be a list of integers, got {lines} instead"
+        ) from error
+
+
+def parse_transliteration(
+    transliteration_query_factory: TransliterationQueryFactory,
+) -> Callable[[Dict], Dict]:
+    def _parse(parameters: Dict) -> Dict:
+        if "transliteration" not in parameters:
+            return parameters
+
+        return {
+            **parameters,
+            "transliteration": [
+                transliteration_query_factory.create(line).regexp
+                for line in parameters["transliteration"].strip().split("\n")
+                if line
+            ],
+        }
+
+    return _parse
+
+
+def parse_lemmas(parameters: Dict) -> Dict:
+    if "lemmas" not in parameters:
+        return parameters
+
+    lemmas = parameters["lemmas"].split("+")
+
+    try:
+        lemma_operator = LemmaQueryType[
+            "AND"
+            if len(lemmas) == 1
+            else parameters.get("lemmaOperator", "and").upper()
+        ]
+    except KeyError as error:
+        raise DataError(
+            f"unexpected lemmaOperator {parameters['lemmaOperator']!r}"
+        ) from error
+
+    return {
+        **parameters,
+        "lemmas": lemmas,
+        "lemmaOperator": lemma_operator,
+    }
+
+
+def parse_pages(parameters: Dict) -> Dict:
+    if "pages" not in parameters:
+        return parameters
+
+    pages = parameters["pages"]
+
+    if "bibId" not in parameters:
+        raise DataError("Name, Year or Title required")
+    try:
+        return {**parameters, "pages": int(pages)}
+    except ValueError as error:
+        raise DataError(f"Pages {pages!r} not numeric.") from error
--- ebl/fragmentarium/web/fragments.py
+++ ebl/fragmentarium/web/fragments.py
@@ -1,33 +1,30 @@
 import falcon
 from falcon import Request, Response
-from typing import Sequence, Dict
+from typing import Sequence
 from ebl.common.query.query_schemas import QueryResultSchema
+from ebl.common.query.parameter_parser import (
+    parse_integer_field,
+    parse_lines,
+    parse_transliteration,
+    parse_lemmas,
+    parse_pages,
+)
 
 from ebl.fragmentarium.application.fragment_finder import FragmentFinder
 from ebl.fragmentarium.application.fragment_repository import FragmentRepository
-from ebl.fragmentarium.infrastructure.fragment_lemma_matcher import LemmaQueryType
 from ebl.fragmentarium.web.dtos import create_response_dto, parse_museum_number
 from ebl.users.domain.user import User
 from ebl.users.web.require_scope import require_scope
 from ebl.fragmentarium.domain.fragment import Scope
-from ebl.errors import DataError
 from ebl.transliteration.application.transliteration_query_factory import (
     TransliterationQueryFactory,
 )
+from pydash import flow
 
 
 def check_fragment_scope(user: User, scopes: Sequence[Scope]):
     if not user.can_read_fragment([scope_group.value for scope_group in scopes]):
         raise falcon.HTTPForbidden()
-
-
-def parse_lines(lines: Sequence[str]) -> Sequence[int]:
-    try:
-        return [int(line) for line in lines]
-    except ValueError as error:
-        raise DataError(
-            f"lines must be a list of integers, got {lines} instead"
-        ) from error
 
 
 class FragmentsResource:
@@ -57,46 +54,13 @@ class FragmentsQueryResource:
         self._repository = repository
         self._transliteration_query_factory = transliteration_query_factory
 
-    def _process_lemmas(self, parameters: Dict) -> Dict:
-        if "lemmas" not in parameters:
-            return {}
-
-        lemmas = parameters["lemmas"].split("+")
-        return {
-            "lemmas": lemmas,
-            "lemmaOperator": LemmaQueryType[
-                "AND"
-                if len(lemmas) == 1
-                else parameters.get("lemmaOperator", "and").upper()
-            ],
-        }
-
-    def _process_pages(self, parameters: Dict) -> Dict:
-        if "pages" not in parameters:
-            return {}
-
-        pages = parameters["pages"]
-
-        if "bibId" not in parameters:
-            raise DataError("Name, Year or Title required")
-        try:
-            return {"pages": int(pages)}
-        except ValueError as error:
-            raise DataError(f'Pages "{pages}" not numeric.') from error
-
     @falcon.before(require_scope, "read:fragments")
     def on_get(self, req: Request, resp: Response):
-        parameters = {**req.params}
-        parameters |= self._process_lemmas(parameters)
-        parameters.update(self._process_pages(parameters))
+        parse = flow(
+            parse_transliteration(self._transliteration_query_factory),
+            parse_lemmas,
+            parse_pages,
+            parse_integer_field("limit"),
+        )
 
-        if "transliteration" in parameters:
-            parameters["transliteration"] = [
-                self._transliteration_query_factory.create(line).regexp
-                for line in parameters["transliteration"].strip().split("\n")
-                if line
-            ]
-        if "limit" in parameters:
-            parameters["limit"] = int(parameters["limit"])
-
-        resp.media = QueryResultSchema().dump(self._repository.query(parameters))
+        resp.media = QueryResultSchema().dump(self._repository.query(parse(req.params)))
