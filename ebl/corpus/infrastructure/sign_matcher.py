e12c3e93d6aef0c7314011aec9f2a25440a19e1e
Fabian Simonjetz <fabian.simonjetz@rub.de>
2023-01-27 12:54:22 +0100

    add aggregation steps for sign matching

--- ebl/corpus/infrastructure/sign_matcher.py
+++ ebl/corpus/infrastructure/sign_matcher.py
@@ -7,40 +7,104 @@ class SignMatcher:
         self.pattern = pattern
 
     def _merge_manuscripts_and_signs(self) -> List[Dict]:
-        _zip = {
-            "$zip": {
-                "inputs": [
-                    "$manuscripts",
-                    {
-                        "$map": {
-                            "input": "$signs",
-                            "in": {
-                                "signs": {
-                                    "$split": [
-                                        "$$this",
-                                        "\n",
-                                    ]
-                                }
-                            },
-                        }
-                    },
-                ]
-            }
-        }
-
         return [
             {
                 "$project": {
-                    "lines": True,
-                    "manuscript": {
-                        "$map": {
-                            "input": _zip,
-                            "as": "tuple",
-                            "in": {"$mergeObjects": "$$tuple"},
-                        }
+                    "manuscriptsWithSigns": {
+                        "$zip": {"inputs": ["$manuscripts.id", "$signs"]}
                     },
+                    "textId": 1,
+                    "stage": 1,
+                    "name": 1,
+                    "lines.variants.manuscripts.manuscriptId": 1,
+                    "lines.variants.manuscripts.line.type": 1,
                 }
             },
+            {
+                "$project": {
+                    "manuscriptWithSigns": {
+                        "$map": {
+                            "input": "$manuscriptsWithSigns",
+                            "as": "m",
+                            "in": {
+                                "manuscriptId": {"$arrayElemAt": ["$$m", 0]},
+                                "signs": {"$arrayElemAt": ["$$m", 1]},
+                            },
+                        }
+                    },
+                    "textId": 1,
+                    "stage": 1,
+                    "name": 1,
+                    "manuscriptLines": "$lines.variants.manuscripts",
+                }
+            },
+        ]
+
+    def _restructure_signs(self) -> List[Dict]:
+        return [
+            {"$unwind": "$manuscriptWithSigns"},
+            {
+                "$project": {
+                    "signs": {"$split": ["$manuscriptWithSigns.signs", "\n"]},
+                    "manuscriptIdsToInclude": "$manuscriptWithSigns.manuscriptId",
+                    "textId": True,
+                    "stage": True,
+                    "name": True,
+                    "manuscriptLines": True,
+                }
+            },
+            {
+                "$unwind": {
+                    "path": "$signs",
+                    "includeArrayIndex": "manuscriptLinesToInclude",
+                }
+            },
+        ]
+
+    def _match(self) -> List[Dict]:
+        # TODO: Construct proper ngram matching
+        return [
+            {"$match": {"signs": {"$regex": r"^X.*X$"}}},
+            {
+                "$project": {"signs": 0},
+            },
+        ]
+
+    def _merge_manuscripts_to_include(self) -> List[Dict]:
+        return [
+            {
+                "$group": {
+                    "_id": "$_id",
+                    "stage": {"$first": "$stage"},
+                    "name": {"$first": "$name"},
+                    "textId": {"$first": "$textId"},
+                    "manuscriptIdsToInclude": {"$push": "$manuscriptIdsToInclude"},
+                    "manuscriptLinesToInclude": {"$push": "$manuscriptLinesToInclude"},
+                    "manuscriptId": {"$first": "$manuscriptLines"},
+                }
+            },
+            {
+                "$project": {
+                    "manuscriptsToInclude": {
+                        "$zip": {
+                            "inputs": [
+                                "$manuscriptIdsToInclude",
+                                "$manuscriptLinesToInclude",
+                            ]
+                        }
+                    },
+                    "stage": 1,
+                    "name": 1,
+                    "textId": 1,
+                    "manuscriptId": 1,
+                }
+            },
+        ]
+
+    def _flatten_variants(self) -> List[Dict]:
+        return [
+            {"$unwind": {"path": "$manuscriptId", "includeArrayIndex": "lineIndex"}},
+            {"$unwind": {"path": "$manuscriptId", "includeArrayIndex": "variantIndex"}},
         ]
 
     def _create_ngrams(self) -> List[Dict]:
@@ -54,6 +118,44 @@ class SignMatcher:
                 }
             },
             {"$unwind": "$ngram"},
+        ]
+
+    def _filter_textlines(self) -> List[Dict]:
+        return [
+            {
+                "$addFields": {
+                    "manuscriptId": {
+                        "$filter": {
+                            "input": "$manuscriptId",
+                            "as": "m",
+                            "cond": {"$eq": ["$$m.line.type", "TextLine"]},
+                        }
+                    }
+                }
+            },
+        ]
+
+    def _get_matching_variants(self) -> List[Dict]:
+        return [
+            {
+                "$unwind": {
+                    "path": "$manuscriptId",
+                    "includeArrayIndex": "manuscriptVariantLineIndex",
+                }
+            },
+            {
+                "$match": {
+                    "$expr": {
+                        "$in": [
+                            [
+                                "$manuscriptId.manuscriptId",
+                                "$manuscriptVariantLineIndex",
+                            ],
+                            "$manuscriptsToInclude",
+                        ]
+                    }
+                }
+            },
         ]
 
     def _collect_indexes(self) -> List[Dict]:
@@ -92,23 +194,28 @@ class SignMatcher:
             },
         ]
 
+    def _regroup_chapters(self) -> List[Dict]:
+        return [
+            {
+                "$group": {
+                    "_id": "$_id",
+                    "textId": {"$first": "$textId"},
+                    "name": {"$first": "$name"},
+                    "stage": {"$first": "$stage"},
+                    "lineIndex": {"$push": "$lineIndex"},
+                    "variantIndex": {"$push": "$variantIndex"},
+                }
+            }
+        ]
+
     def build_pipeline(self) -> List[Dict]:
         pipeline = [
-            {
-                "$project": {
-                    "manuscripts.id": True,
-                    "signs": True,
-                    "lines.variants.manuscripts.manuscriptId": True,
-                }
-            },
             *self._merge_manuscripts_and_signs(),
-            *self._create_ngrams(),
-            {
-                "$match": {
-                    "ngram.0": {"$regex": r"^X X X [^X]+ X X [^X]+ X "},
-                    "ngram.1": {"$regex": r"^."},
-                }
-            },
-            *self._collect_indexes(),
+            *self._restructure_signs(),
+            *self._match(),
+            *self._merge_manuscripts_to_include(),
+            *self._flatten_variants(),
+            *self._filter_textlines(),
+            *self._get_matching_variants(),
         ]
         return pipeline
