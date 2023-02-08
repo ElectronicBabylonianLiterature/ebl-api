78628c69f89f620b89a9425deffd876663c3e4df
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-02-08 16:49:40 +0000

    Add require_folio_scope

--- ebl/users/web/require_scope.py
+++ ebl/users/web/require_scope.py
@@ -1,12 +1,38 @@
 import falcon
 
 
+OPEN_FOLIO_SCOPES = [
+    "read:WGL-folios",
+    "read:FWG-folios",
+    "read:EL-folios",
+    "read:AKG-folios",
+    "read:CB-folios",
+    "read:JS-folios",
+    "read:RB-folios",
+    "read:ER-folios",
+    "read:GS-folios",
+]
+
+
+def is_guest_session(req: falcon.Request) -> bool:
+    return getattr(req.context, "user", None) is None
+
+
 def has_scope(req: falcon.Request, scope: str) -> bool:
-    if not hasattr(req.context, "user") or not req.context.user:
-        return "read:" in scope or scope in {"access:beta"}
+    if is_guest_session(req):
+        return "read:" in scope or scope == "access:beta"
     return req.context.user.has_scope(scope)
 
 
 def require_scope(req: falcon.Request, _resp, _resource, _params, scope: str):
     if not has_scope(req, scope):
         raise falcon.HTTPForbidden()
+
+
+def require_folio_scope(req: falcon.Request, _resp, _resource, params):
+    scope = f"read:{params['name']}-folios"
+
+    if scope not in OPEN_FOLIO_SCOPES:
+        if is_guest_session(req) or not has_scope(req, scope):
+            raise falcon.HTTPForbidden()
+
