7b90040ee47b0f9003f64e66a184a27bb4b8c8c0
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-02-15 13:04:53 +0000

    check for and exclude hidden folios in dump

--- ebl/fragmentarium/web/dtos.py
+++ ebl/fragmentarium/web/dtos.py
@@ -1,11 +1,12 @@
-from marshmallow import fields
+from marshmallow import fields, post_dump
 
 from ebl.bibliography.application.reference_schema import ApiReferenceSchema
 from ebl.errors import DataError
 from ebl.fragmentarium.application.fragment_schema import FragmentSchema
 from ebl.fragmentarium.domain.fragment import Fragment
 from ebl.transliteration.domain.museum_number import MuseumNumber
-from ebl.users.domain.user import User
+from ebl.users.domain.user import User, Guest
+from ebl.common.folios import HIDDEN_SCOPES
 
 
 class FragmentDtoSchema(FragmentSchema):
@@ -17,6 +18,18 @@ class FragmentDtoSchema(FragmentSchema):
 
     class Meta:
         exclude = ("authorized_scopes",)
+
+    @post_dump
+    def exclude_hidden_folios(self, data, **kwargs):
+        if "folios" in data:
+            user = self.context.get("user", Guest())
+            data["folios"] = [
+                folio
+                for folio in data["folios"]
+                if user.has_scope(f"read:{folio['name']}-folios")
+                or f"read:{folio['name']}-folios" not in HIDDEN_SCOPES
+            ]
+        return data
 
 
 def create_response_dto(fragment: Fragment, user: User, has_photo: bool):
