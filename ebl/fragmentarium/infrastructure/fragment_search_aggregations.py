a4c042099fd2044ae4f507f9fe396809ccc4a7ec
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-02-16 09:56:36 +0000

    Revert irrelevant change

--- ebl/fragmentarium/infrastructure/fragment_search_aggregations.py
+++ ebl/fragmentarium/infrastructure/fragment_search_aggregations.py
@@ -128,7 +128,7 @@ class PatternMatcher:
                         "$project": {
                             "_id": True,
                             "museumNumber": True,
-                            "matchingLines": [],
+                            "matchingLines": {"$range": [0, {"$size": "$text.lines"}]},
                             "matchCount": {"$literal": 0},
                         }
                     },
