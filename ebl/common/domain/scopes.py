2c0b35c4066c386b2f4fab33680ea5a01f7c9042
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-02-17 11:06:48 +0000

    Add Scope enum

--- ebl/common/domain/scopes.py
+++ ebl/common/domain/scopes.py
@@ -1,55 +1,90 @@
 from enum import Enum
 import re
 
-import attr
+OPEN = True
+RESTRICTED = False
 
 
-@attr.s(auto_attribs=True, frozen=True)
 class ScopeItem(Enum):
-    def __init__(self, scope_string: str):
-        match = re.match("([^:]+):(.+)(:?-(.+))?", scope_string)
+    def __init__(self, scope_string: str, is_open=True):
+        prefix, name, suffix = self.parse_scope_string(scope_string)
+        self.prefix = prefix
+        self.scope_name = name
+        self.suffix = suffix
+        self.is_open = is_open
+
+    @classmethod
+    def from_string(cls, scope_string: str):
+        prefix, name, suffix = cls.parse_scope_string(scope_string)
+        try:
+            return next(
+                enum
+                for enum in cls
+                if enum.prefix == prefix
+                and enum.scope_name == name
+                and suffix in [enum.suffix, ""]
+            )
+        except StopIteration:
+            raise ValueError(f"Unknown scope: {scope_string}")
+
+    @classmethod
+    def from_parts(cls, prefix: str, scope_name: str, suffix: str = ""):
+        return next(
+            enum
+            for enum in cls
+            if cls.prefix == prefix
+            and cls.scope_name == scope_name
+            and suffix in [cls.suffix, ""]
+        )
+
+    @staticmethod
+    def parse_scope_string(scope_string: str):
+        match = re.match("([^:]+):([^-]+)(?:-(.+))?", scope_string)
 
         if not match:
             raise ValueError(f"Unexepcted scope format: {scope_string!r}")
 
-        self.prefix = match[1]
-        self.name = match[2]
-        self.suffix = match[3] if len(match.groups()) > 3 else ""
+        return match[1], match[2], match[3] or ""
+
+    @property
+    def is_restricted(self) -> bool:
+        return not self.is_open
 
     def __str__(self):
         suffix = f"-{self.suffix}" if self.suffix else ""
-        return f"{self.prefix}:{self.name}{suffix}"
+        return f"{self.prefix}:{self.scope_name}{suffix}"
 
 
-@attr.s(auto_attribs=True, frozen=True)
 class Scope(ScopeItem):
-    READ_CAIC_FRAGMENTS = "read:CAIC-fragments"
-    READ_ITALIANNINEVEH_FRAGMENTS = "read:ITALIANNINEVEH-fragments"
-    READ_SIPPARLIBRARY_FRAGMENTS = "read:SIPPARLIBRARY-fragments"
-    READ_URUKLBU_FRAGMENTS = "read:URUKLBU-fragments"
-    READ_AKG_FOLIOS = "read:AKG-folios"
-    READ_ARG_FOLIOS = "read:ARG-folios"
-    READ_CB_FOLIOS = "read:CB-folios"
-    READ_EL_FOLIOS = "read:EL-folios"
-    READ_ER_FOLIOS = "read:ER-folios"
-    READ_EVW_FOLIOS = "read:EVW-folios"
-    READ_FWG_FOLIOS = "read:FWG-folios"
-    READ_GS_FOLIOS = "read:GS-folios"
-    READ_ILF_FOLIOS = "read:ILF-folios"
-    READ_JS_FOLIOS = "read:JS-folios"
-    READ_MJG_FOLIOS = "read:MJG-folios"
-    READ_RB_FOLIOS = "read:RB-folios"
-    READ_SJL_FOLIOS = "read:SJL-folios"
-    READ_SP_FOLIOS = "read:SP-folios"
-    READ_UG_FOLIOS = "read:UG-folios"
-    READ_USK_FOLIOS = "read:USK-folios"
-    READ_WGL_FOLIOS = "read:WGL-folios"
-    READ_WRM_FOLIOS = "read:WRM-folios"
-    READ_BIBLIOGRAPHY = "read:bibliography"
-    ANNOTATE_FRAGMENTS = "annotate:fragments"
-    CREATE_TEXTS = "create:texts"
-    LEMMATIZE_FRAGMENTS = "lemmatize:fragments"
-    TRANSLITERATE_FRAGMENTS = "transliterate:fragments"
-    WRITE_BIBLIOGRAPHY = "write:bibliography"
-    WRITE_TEXTS = "write:texts"
-    WRITE_WORDS = "write:words"
+    READ_CAIC_FRAGMENTS = ("read:CAIC-fragments", RESTRICTED)
+    READ_ITALIANNINEVEH_FRAGMENTS = ("read:ITALIANNINEVEH-fragments", RESTRICTED)
+    READ_SIPPARLIBRARY_FRAGMENTS = ("read:SIPPARLIBRARY-fragments", RESTRICTED)
+    READ_URUKLBU_FRAGMENTS = ("read:URUKLBU-fragments", RESTRICTED)
+    READ_ARG_FOLIOS = ("read:ARG-folios", RESTRICTED)
+    READ_EVW_FOLIOS = ("read:EVW-folios", RESTRICTED)
+    READ_ILF_FOLIOS = ("read:ILF-folios", RESTRICTED)
+    READ_MJG_FOLIOS = ("read:MJG-folios", RESTRICTED)
+    READ_SJL_FOLIOS = ("read:SJL-folios", RESTRICTED)
+    READ_SP_FOLIOS = ("read:SP-folios", RESTRICTED)
+    READ_UG_FOLIOS = ("read:UG-folios", RESTRICTED)
+    READ_USK_FOLIOS = ("read:USK-folios", RESTRICTED)
+    READ_WRM_FOLIOS = ("read:WRM-folios", RESTRICTED)
+    READ_AKG_FOLIOS = ("read:AKG-folios", OPEN)
+    READ_CB_FOLIOS = ("read:CB-folios", OPEN)
+    READ_EL_FOLIOS = ("read:EL-folios", OPEN)
+    READ_ER_FOLIOS = ("read:ER-folios", OPEN)
+    READ_FWG_FOLIOS = ("read:FWG-folios", OPEN)
+    READ_GS_FOLIOS = ("read:GS-folios", OPEN)
+    READ_JS_FOLIOS = ("read:JS-folios", OPEN)
+    READ_RB_FOLIOS = ("read:RB-folios", OPEN)
+    READ_WGL_FOLIOS = ("read:WGL-folios", OPEN)
+    READ_BIBLIOGRAPHY = ("read:bibliography", OPEN)
+    ANNOTATE_FRAGMENTS = ("annotate:fragments", RESTRICTED)
+    CREATE_TEXTS = ("create:texts", RESTRICTED)
+    LEMMATIZE_FRAGMENTS = ("lemmatize:fragments", RESTRICTED)
+    TRANSLITERATE_FRAGMENTS = ("transliterate:fragments", RESTRICTED)
+    WRITE_BIBLIOGRAPHY = ("write:bibliography", RESTRICTED)
+    WRITE_TEXTS = ("write:texts", RESTRICTED)
+    READ_WORDS = ("read:words", OPEN)
+    READ_TEXTS = ("read:texts", OPEN)
+    WRITE_WORDS = ("write:words", RESTRICTED)
