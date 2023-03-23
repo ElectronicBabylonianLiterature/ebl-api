ed3b93e105080062f637d7a397ae8330a4f588e4
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-03-23 11:36:06 +0000

    add introduction and notes update to update script

--- ebl/fragmentarium/update_fragments.py
+++ ebl/fragmentarium/update_fragments.py
@@ -30,6 +30,8 @@ def update_fragment(
     transliteration = transliteration_factory.create(fragment.text.atf)
     user = ApiUser("update_fragments.py")
     updater.update_transliteration(fragment.number, transliteration, user, True)
+    updater.update_introduction(fragment.number, fragment.introduction.text, user)
+    updater.update_notes(fragment.number, fragment.notes.text, user)
 
 
 def find_transliterated(fragment_repository: FragmentRepository) -> List[MuseumNumber]:
