ded373d71049a69c5fa906cdb8d7676e2f9f6c78
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-03-29 14:42:26 +0000

    Use existing _sortKey instead of re-sorting for each query

--- ebl/fragmentarium/infrastructure/fragment_search_aggregations.py
+++ ebl/fragmentarium/infrastructure/fragment_search_aggregations.py
@@ -91,7 +91,7 @@ class PatternMatcher:
 
     def _wrap_query_items_with_total(self) -> List[Dict]:
         return [
-            *sort_by_museum_number(pre_sort_keys={"script.sortKey": 1}),
+            {"$sort": {"script.sortKey": 1, "_sortKey": 1}},
             *self._limit_result(),
             {"$project": {"_id": False, "script": False}},
             {
