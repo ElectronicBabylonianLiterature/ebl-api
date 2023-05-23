abe5aa522d0541b346062cd1a9a5fb7443c37ce5
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-05-23 12:25:44 +0000

    add write_to_db tests

--- ebl/tests/io/test_fragment_importer.py
+++ ebl/tests/io/test_fragment_importer.py
@@ -1,3 +1,4 @@
+from functools import partial
 import json
 import re
 from marshmallow import ValidationError
@@ -7,17 +8,24 @@ from ebl.fragmentarium.application.fragment_schema import FragmentSchema
 from ebl.fragmentarium.infrastructure.mongo_fragment_repository import (
     MongoFragmentRepository,
 )
+from pymongo.errors import BulkWriteError
 from ebl.io.fragments.importer import (
     validate,
     load_data,
     validate_id,
     ensure_unique,
+    write_to_db,
 )
 
 from ebl.tests.factories.fragment import LemmatizedFragmentFactory
 
 
 FRAGMENT = LemmatizedFragmentFactory.build()
+MOCKFILE = "mock.json"
+
+validate = partial(validate, filename=MOCKFILE)
+validate_id = partial(validate_id, filename=MOCKFILE)
+ensure_unique = partial(ensure_unique, filename=MOCKFILE)
 
 
 @pytest.fixture
@@ -56,37 +64,51 @@ def test_missing_required_field(valid_fragment_data):
     with pytest.raises(
         ValidationError,
         match=re.escape(
-            "Invalid data in mock.json: {'museumNumber': ['Missing data for required field.']}"
+            f"Invalid data in {MOCKFILE}: "
+            "{'museumNumber': ['Missing data for required field.']}"
         ),
     ):
-        validate(valid_fragment_data, "mock.json")
+        validate(valid_fragment_data)
+
+
+def test_invalid_enum(valid_fragment_data):
+    unknown_period = "Neo-Foobarian"
+    valid_fragment_data["script"]["period"] = unknown_period
+
+    with pytest.raises(
+        ValidationError,
+        match=f"Invalid data in {MOCKFILE}: Unknown enum long_name: {unknown_period}",
+    ):
+        validate(valid_fragment_data)
 
 
 def test_invalid_input_type(valid_fragment_data):
     with pytest.raises(
         ValidationError,
         match=re.escape(
-            "Invalid data in mock.json: {'_schema': ['Invalid input type.']}"
+            f"Invalid data in {MOCKFILE}: {{'_schema': ['Invalid input type.']}}"
         ),
     ):
 
-        validate("invalid input", "mock.json")
+        validate("invalid input")
 
 
 def test_ensure_id(valid_fragment_data):
     valid_fragment_data["_id"] = "mock.id"
-    validate_id(valid_fragment_data, "mock.json")
+    validate_id(valid_fragment_data)
 
 
 def test_missing_id(valid_fragment_data):
-    with pytest.raises(ValidationError, match="Missing _id in mock.json"):
-        validate_id(valid_fragment_data, "mock.json")
+    with pytest.raises(ValidationError, match=f"Missing _id in {MOCKFILE}"):
+        validate_id(valid_fragment_data)
 
 
 def test_invalid_id(valid_fragment_data):
-    valid_fragment_data["_id"] = "invalid museum number"
+    invalid_number = "invalid museum number"
+    valid_fragment_data["_id"] = invalid_number
     with pytest.raises(
-        ValueError, match="'invalid museum number' is not a valid museum number"
+        ValidationError,
+        match=f"id {invalid_number!r} of {MOCKFILE} is not a valid museum number",
     ):
         validate_id(valid_fragment_data)
 
@@ -96,7 +118,7 @@ def test_ensure_unique(
 ):
     valid_fragment_data["_id"] = "mock.number"
     fragment_repository.create(FRAGMENT)
-    ensure_unique(valid_fragment_data, fragment_repository._fragments, "mock.json")
+    ensure_unique(valid_fragment_data, fragment_repository._fragments)
 
 
 def test_ensure_unique_duplicate(
@@ -106,9 +128,25 @@ def test_ensure_unique_duplicate(
     valid_fragment_data["_id"] = museum_number
     fragment_repository.create(FRAGMENT)
     with pytest.raises(
-        ValidationError, match=f"ID {museum_number} of file mock.json already exists"
+        ValidationError, match=f"ID {museum_number} of file {MOCKFILE} already exists"
     ):
-        ensure_unique(valid_fragment_data, fragment_repository._fragments, "mock.json")
+        ensure_unique(valid_fragment_data, fragment_repository._fragments)
 
 
-# TODO: test write to db
+def test_write_to_db(valid_fragment_data, fragment_repository: MongoFragmentRepository):
+    fragment_repository.create(FRAGMENT)
+    valid_fragment_data["_id"] = "mock.number"
+
+    assert write_to_db([valid_fragment_data], fragment_repository._fragments) == [
+        "mock.number"
+    ]
+
+
+def test_write_to_db_duplicate(
+    valid_fragment_data, fragment_repository: MongoFragmentRepository
+):
+    fragment_repository.create(FRAGMENT)
+    valid_fragment_data["_id"] = str(FRAGMENT.number)
+
+    with pytest.raises(BulkWriteError, match="E11000 duplicate key error"):
+        write_to_db([valid_fragment_data], fragment_repository._fragments)
