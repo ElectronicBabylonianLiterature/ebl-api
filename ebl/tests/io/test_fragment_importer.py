09c7e63bbb47c9430b29b43701c9a04c73af7ccb
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-05-23 15:28:03 +0000

    add test for validation script call

--- ebl/tests/io/test_fragment_importer.py
+++ ebl/tests/io/test_fragment_importer.py
@@ -4,9 +4,9 @@ import re
 import attr
 from marshmallow import ValidationError
 import pytest
-import os
 from ebl.fragmentarium.application.fragment_schema import FragmentSchema
 from pymongo.errors import BulkWriteError
+from ebl.fragmentarium.domain.fragment import Fragment
 from ebl.io.fragments.importer import (
     validate,
     load_data,
@@ -19,9 +19,9 @@ from ebl.mongo_collection import MongoCollection
 
 from ebl.tests.factories.fragment import LemmatizedFragmentFactory
 from ebl.transliteration.domain.museum_number import MuseumNumber
+import subprocess
 
 
-FRAGMENT = LemmatizedFragmentFactory.build()
 MOCKFILE = "mock.json"
 
 validate = partial(validate, filename=MOCKFILE)
@@ -30,8 +30,13 @@ ensure_unique = partial(ensure_unique, filename=MOCKFILE)
 
 
 @pytest.fixture
-def valid_fragment_data() -> dict:
-    return FragmentSchema().dump(FRAGMENT)
+def fragment() -> Fragment:
+    return LemmatizedFragmentFactory.build()
+
+
+@pytest.fixture
+def valid_fragment_data(fragment) -> dict:
+    return FragmentSchema().dump(fragment)
 
 
 @pytest.fixture
@@ -39,22 +44,22 @@ def fragments_collection(fragment_repository) -> MongoCollection:
     return fragment_repository._fragments
 
 
-def mock_json_file(contents, path) -> os.PathLike:
+def mock_json_file(contents, path) -> str:
     path = path / "test.json"
     with path.open("w") as tmp_file:
         tmp_file.write(contents)
-    return path
+    return str(path)
 
 
 def test_load_json(tmp_path):
     contents = {"test": "data"}
-    path = str(mock_json_file(json.dumps(contents), tmp_path))
+    path = mock_json_file(json.dumps(contents), tmp_path)
     assert load_data([path]) == {path: contents}
 
 
 def test_load_invalid_json(tmp_path):
     contents = "{'broken_file': None}"
-    path = str(mock_json_file(contents, tmp_path))
+    path = mock_json_file(contents, tmp_path)
 
     with pytest.raises(ValueError, match=f"Invalid JSON: {path}"):
         load_data([path])
@@ -121,22 +126,24 @@ def test_invalid_id(valid_fragment_data):
 
 def test_ensure_unique(
     valid_fragment_data,
+    fragment,
     fragment_repository,
     fragments_collection,
 ):
     valid_fragment_data["_id"] = "mock.number"
-    fragment_repository.create(FRAGMENT)
+    fragment_repository.create(fragment)
     ensure_unique(valid_fragment_data, fragments_collection)
 
 
 def test_ensure_unique_duplicate(
     valid_fragment_data,
+    fragment,
     fragment_repository,
     fragments_collection,
 ):
-    museum_number = str(FRAGMENT.number)
+    museum_number = str(fragment.number)
     valid_fragment_data["_id"] = museum_number
-    fragment_repository.create(FRAGMENT)
+    fragment_repository.create(fragment)
     with pytest.raises(
         ValidationError, match=f"ID {museum_number} of file {MOCKFILE} already exists"
     ):
@@ -146,9 +153,10 @@ def test_ensure_unique_duplicate(
 def test_write_to_db(
     valid_fragment_data,
     fragment_repository,
+    fragment,
     fragments_collection,
 ):
-    fragment_repository.create(FRAGMENT)
+    fragment_repository.create(fragment)
     valid_fragment_data["_id"] = "mock.number"
 
     assert write_to_db([valid_fragment_data], fragments_collection) == ["mock.number"]
@@ -158,20 +166,21 @@ def test_write_to_db(
 
 def test_write_to_db_duplicate(
     valid_fragment_data,
+    fragment,
     fragment_repository,
     fragments_collection,
 ):
-    fragment_repository.create(FRAGMENT)
-    valid_fragment_data["_id"] = str(FRAGMENT.number)
+    fragment_repository.create(fragment)
+    valid_fragment_data["_id"] = str(fragment.number)
 
     with pytest.raises(BulkWriteError, match="E11000 duplicate key error"):
         write_to_db([valid_fragment_data], fragments_collection)
 
 
-def test_create_sort_index(fragment_repository, fragments_collection):
+def test_create_sort_index(fragment, fragment_repository, fragments_collection):
     for i in [3, 2, 1]:
         fragment_repository.create(
-            attr.evolve(FRAGMENT, number=MuseumNumber.of(f"X.{i}"))
+            attr.evolve(fragment, number=MuseumNumber.of(f"X.{i}"))
         )
 
     assert not fragments_collection.exists({"_sortKey": {"$exists": True}})
@@ -182,3 +191,33 @@ def test_create_sort_index(fragment_repository, fragments_collection):
         fragments_collection.find_many({}, projection={"_id": True, "_sortKey": True}),
         key=lambda entry: entry["_sortKey"],
     ) == [{"_id": f"X.{i+1}", "_sortKey": i} for i in [0, 1, 2]]
+
+
+@pytest.mark.parametrize(
+    "updates,command,expected_error",
+    [
+        (
+            {},
+            "poetry run python -m ebl.io.fragments.importer --validate",
+            "ValidationError: Missing _id in {}",
+        ),
+        (
+            {"_id": "invalid museum number"},
+            "poetry run python -m ebl.io.fragments.importer --validate",
+            "ValidationError: id 'invalid museum number' of {} is not a valid museum number",
+        ),
+    ],
+)
+def test_main_validate_only(
+    valid_fragment_data, updates, tmp_path, command, expected_error
+):
+
+    valid_fragment_data.update(updates)
+    path = mock_json_file(json.dumps(valid_fragment_data), tmp_path)
+
+    result = subprocess.run(
+        command.split() + [path],
+        capture_output=True,
+    )
+    assert expected_error.format(path) in result.stderr.decode("utf-8")
+    assert "Writing to database..." not in result.stdout.decode("utf-8")
