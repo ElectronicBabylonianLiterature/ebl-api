d9ebe0e6b04425cbcb990aa9cb8c5c34ada38567
Yunus <yunus.cobanoglu@yahoo.de>
2023-08-02 21:07:59 +0200

    include has sign

--- ebl/fragmentarium/application/cropped_annotations_service.py
+++ ebl/fragmentarium/application/cropped_annotations_service.py
@@ -1,9 +1,12 @@
 from typing import Sequence
 
+import attr
+
 from ebl.fragmentarium.application.annotations_repository import AnnotationsRepository
 from ebl.fragmentarium.application.cropped_sign_images_repository import (
     CroppedSignImagesRepository,
 )
+from ebl.fragmentarium.domain.annotation import AnnotationValueType
 
 
 class CroppedAnnotationService:
@@ -19,6 +22,7 @@ class CroppedAnnotationService:
         annotations = self._annotations_repository.find_by_sign(sign)
         cropped_image_annotations = []
         for annotation in annotations:
+            annotation = attr.evolve(annotation, annotations=[x for x in annotation.annotations if x.data.type == AnnotationValueType.HAS_SIGN])
             for annotation_elem in annotation.annotations:
                 if cropped_sign := annotation_elem.cropped_sign:
                     cropped_sign_image = (
