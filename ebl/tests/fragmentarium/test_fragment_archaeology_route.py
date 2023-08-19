7a11beaea3c24f423d835598432acf0c1b37ca30
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-08-19 16:52:10 +0000

    add test for archaeology route

---
+++ ebl/tests/fragmentarium/test_fragment_archaeology_route.py
@@ -0,0 +1,49 @@
+import json
+import attr
+
+import falcon
+import pytest
+from ebl.fragmentarium.application.archaeology_schemas import ArchaeologySchema
+from ebl.fragmentarium.domain.archaeology import Archaeology
+from ebl.fragmentarium.domain.fragment import Fragment
+
+from ebl.fragmentarium.web.dtos import create_response_dto
+from ebl.tests.factories.archaeology import DateWithNotesFactory, FindspotFactory
+from ebl.tests.factories.fragment import FragmentFactory
+from ebl.transliteration.domain.museum_number import MuseumNumber
+from ebl.corpus.domain.provenance import Provenance as ExcavationSite
+
+ARCHAEOLOGY = Archaeology(MuseumNumber("F", "1"), ExcavationSite.KALHU)
+ARCHAEOLOGIES = [
+    ARCHAEOLOGY,
+    attr.evolve(ARCHAEOLOGY, site=ExcavationSite.NIPPUR),
+    attr.evolve(ARCHAEOLOGY, regular_excavation=False),
+    attr.evolve(ARCHAEOLOGY, excavation_date=(DateWithNotesFactory.build(),)),
+    attr.evolve(ARCHAEOLOGY, findspot=FindspotFactory.build()),
+]
+
+
+@pytest.mark.parametrize("old_archaeology", ARCHAEOLOGIES)
+@pytest.mark.parametrize("new_archaeology", ARCHAEOLOGIES)
+def test_update_archaeology(
+    client, fragmentarium, user, old_archaeology, new_archaeology
+):
+    fragment: Fragment = FragmentFactory.build(archaeology=old_archaeology)
+    fragment_number = fragmentarium.create(fragment)
+    update = {"archaeology": ArchaeologySchema().dump(new_archaeology)}
+    post_result = client.simulate_post(
+        f"/fragments/{fragment_number}/archaeology", body=json.dumps(update)
+    )
+    expected_json = {
+        **create_response_dto(
+            fragment.set_archaeology(new_archaeology),
+            user,
+            fragment.number == "K.1",
+        )
+    }
+
+    assert post_result.status == falcon.HTTP_OK
+    assert post_result.json == expected_json
+
+    get_result = client.simulate_get(f"/fragments/{fragment_number}")
+    assert get_result.json == expected_json
