ad5341c0864347da9a1509403671bcfbafa09eec
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-08-26 13:06:42 +0000

    add exc. number parser

--- ebl/fragmentarium/web/archaeology.py
+++ ebl/fragmentarium/web/archaeology.py
@@ -1,5 +1,11 @@
-from ebl.fragmentarium.application.archaeology_schemas import ArchaeologySchema
+from typing import Union
+from ebl.fragmentarium.application.archaeology_schemas import (
+    ArchaeologySchema,
+    ExcavationNumberSchema,
+)
 from ebl.fragmentarium.application.fragment_updater import FragmentUpdater
+
+# from ebl.errors import DataError
 import falcon
 from falcon import Request, Response
 from ebl.fragmentarium.web.dtos import create_response_dto, parse_museum_number
@@ -10,12 +16,23 @@ class ArchaeologyResource:
     def __init__(self, updater: FragmentUpdater):
         self._updater = updater
 
+    def _parse_excavation_number(
+        self, excavation_number: Union[str, None]
+    ) -> Union[dict, None]:
+        number = parse_museum_number(excavation_number)
+        return None if not excavation_number else ExcavationNumberSchema().dump(number)
+
     @falcon.before(require_scope, "transliterate:fragments")
     def on_post(self, req: Request, resp: Response, number: str) -> None:
         user = req.context.user
+        data = req.media["archaeology"]
+
+        data["excavationNumber"] = self._parse_excavation_number(
+            data.get("excavationNumber")
+        )
         updated_fragment, has_photo = self._updater.update_archaeology(
             parse_museum_number(number),
-            ArchaeologySchema().load(req.media["archaeology"]),
+            ArchaeologySchema().load(data),
             user,
         )
         resp.media = create_response_dto(updated_fragment, user, has_photo)
