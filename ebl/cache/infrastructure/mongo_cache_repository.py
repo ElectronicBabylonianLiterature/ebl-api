707e8d7918097fb3941ff8023180160dc730cffd
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-08-29 14:29:26 +0000

    use _id instead of cache_key

--- ebl/cache/infrastructure/mongo_cache_repository.py
+++ ebl/cache/infrastructure/mongo_cache_repository.py
@@ -13,21 +13,20 @@ class MongoCacheRepository(CacheRepository):
 
     def has(self, cache_key: str, regex=False) -> bool:
         return self._collection.exists(
-            {"cache_key": {"$regex": cache_key} if regex else cache_key}
+            {"_id": {"$regex": cache_key} if regex else cache_key}
         )
 
     def get(self, cache_key: str) -> dict:
-        return self._collection.find_one(
-            {"cache_key": cache_key}, projection={"cache_key": 0, "_id": 0}
-        )
+        return self._collection.find_one({"_id": cache_key}, projection={"_id": 0})
 
     def set(self, cache_key: str, item: dict) -> None:
-        self._collection.insert_one({"cache_key": cache_key, **item})
+        self.delete(cache_key)
+        self._collection.insert_one({"_id": cache_key, **item})
 
     def delete(self, cache_key: str) -> None:
         if self.has(cache_key):
-            self._collection.delete_one({"cache_key": cache_key})
+            self._collection.delete_one({"_id": cache_key})
 
     def delete_all(self, pattern: str) -> None:
         if self.has(pattern, regex=True):
-            self._collection.delete_many({"cache_key": {"$regex": pattern}})
+            self._collection.delete_many({"_id": {"$regex": pattern}})
