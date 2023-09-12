3497f08f9da1c182b2bbe4ef3f30caff59e4ba84
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-09-12 10:53:40 +0000

    add create_chapter_ngram_cache

---
+++ ebl/users/web/update_cache.py
@@ -0,0 +1,22 @@
+from marshmallow import EXCLUDE
+from ebl.corpus.application.id_schemas import ChapterIdSchema
+from ebl.corpus.infrastructure.corpus_ngram_repository import ChapterNGramRepository
+from ebl.fragmentarium.infrastructure.fragment_ngram_repository import (
+    FragmentNGramRepository,
+)
+
+
+NGRAM_LENGHTS = [1, 2, 3]
+
+
+def create_fragment_ngram_cache(_req, resp, resource):
+    museum_number_dto = resp.media["museumNumber"]
+
+    ngram_repository: FragmentNGramRepository = resource.ngram_repository
+    ngram_repository.update_ngrams(museum_number_dto, NGRAM_LENGHTS)
+
+
+def create_chapter_ngram_cache(_req, resp, resource):
+    ngram_repository: ChapterNGramRepository = resource.ngram_repository
+    chapter_id = ChapterIdSchema().load(resp.media, unknown=EXCLUDE)
+    ngram_repository.update_ngrams(chapter_id, NGRAM_LENGHTS)
