27cf1c9833c3d039ab92316f72e7671f3dc0efaf
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-09-21 11:58:49 +0000

    add compute_overlaps method

--- ebl/corpus/infrastructure/corpus_ngram_repository.py
+++ ebl/corpus/infrastructure/corpus_ngram_repository.py
@@ -1,3 +1,4 @@
+from ebl.corpus.application.id_schemas import ChapterIdSchema
 from ebl.corpus.domain.chapter import ChapterId
 from ebl.corpus.infrastructure.queries import chapter_id_query
 from ebl.errors import NotFoundError
@@ -6,7 +7,7 @@ from ebl.transliteration.infrastructure.collections import (
     CHAPTER_NGRAM_COLLECTION,
     CHAPTERS_COLLECTION,
 )
-from typing import Sequence, Set, Tuple
+from typing import List, Optional, Sequence, Set, Tuple
 
 from ebl.common.query.util import aggregate_all_ngrams, replace_all
 
@@ -22,7 +23,7 @@ class ChapterNGramRepository:
         self,
         chapter_id: ChapterId,
         N: Sequence[int],
-    ):
+    ) -> Sequence[dict]:
         return [
             {"$match": chapter_id_query(chapter_id)},
             {"$project": {"signs": 1, "textId": 1, "stage": 1, "name": 1}},
@@ -51,25 +52,83 @@ class ChapterNGramRepository:
             {"$project": {"_id": False}},
         ]
 
-    def update_ngrams(
+    def set_ngrams(
         self,
         chapter_id: ChapterId,
         N: Sequence[int],
-    ) -> None:
+    ) -> Set[Tuple[str]]:
         aggregation = self.aggregate_chapter_ngrams(chapter_id, N)
-        if data := next(
+        data = next(
             self._chapters.aggregate(aggregation, allowDiskUse=True),
-            None,
-        ):
-            try:
-                self._ngrams.update_one(
-                    chapter_id_query(chapter_id),
-                    {"$set": {NGRAM_FIELD: data[NGRAM_FIELD]}},
-                )
-            except NotFoundError:
-                self._ngrams.insert_one(data)
+            {NGRAM_FIELD: [], **ChapterIdSchema().dump(chapter_id)},
+        )
+        try:
+            self._ngrams.update_one(
+                chapter_id_query(chapter_id),
+                {"$set": {NGRAM_FIELD: data[NGRAM_FIELD]}},
+            )
+        except NotFoundError:
+            self._ngrams.insert_one(data)
+
+        return {tuple(ngram) for ngram in data[NGRAM_FIELD]}
 
     def get_ngrams(self, chapter_id: ChapterId) -> Set[Tuple[str]]:
         ngrams = self._ngrams.find_one(chapter_id_query(chapter_id))[NGRAM_FIELD]
 
         return {tuple(ngram) for ngram in ngrams}
+
+    def get_or_set_ngrams(
+        self, chapter_id: ChapterId, N: Sequence[int]
+    ) -> Set[Tuple[str]]:
+        try:
+            return self.get_ngrams(chapter_id)
+        except NotFoundError:
+            ngrams = self.set_ngrams(chapter_id, N)
+
+        return ngrams
+
+    def compute_overlaps(
+        self, ngrams: Set[Tuple[str]], limit: Optional[int] = None
+    ) -> Sequence[dict]:
+        ngram_list = list(ngrams)
+        pipeline: List[dict] = [
+            {"$match": {"textId.category": {"$ne": 99}}},
+            {
+                "$project": {
+                    "_id": 0,
+                    "textId": 1,
+                    "name": 1,
+                    "stage": 1,
+                    "overlap": {
+                        "$let": {
+                            "vars": {
+                                "intersection": {
+                                    "$size": {
+                                        "$setIntersection": ["$ngram", ngram_list]
+                                    }
+                                },
+                                "minLength": {
+                                    "$min": [
+                                        {"$size": "$ngram"},
+                                        {"$size": [ngram_list]},
+                                    ]
+                                },
+                            },
+                            "in": {
+                                "$cond": [
+                                    {"$eq": ["$$minLength", 0]},
+                                    0.0,
+                                    {"$divide": ["$$intersection", "$$minLength"]},
+                                ]
+                            },
+                        }
+                    },
+                }
+            },
+            {"$sort": {"overlap": -1}},
+        ]
+
+        if limit:
+            pipeline.append({"$limit": limit})
+
+        return list(self._ngrams.aggregate(pipeline, allowDiskUse=True))
