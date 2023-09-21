080059efdbd32d5433a52e545d16b15c2de625ac
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-09-21 10:03:26 +0000

    use global DEFAULT_N

--- ebl/users/web/update_cache.py
+++ ebl/users/web/update_cache.py
@@ -1,4 +1,5 @@
 from marshmallow import EXCLUDE
+from ebl.common.infrastructure.ngrams import DEFAULT_N
 from ebl.corpus.application.id_schemas import ChapterIdSchema
 from ebl.corpus.infrastructure.corpus_ngram_repository import ChapterNGramRepository
 from ebl.fragmentarium.infrastructure.fragment_ngram_repository import (
@@ -6,16 +7,13 @@ from ebl.fragmentarium.infrastructure.fragment_ngram_repository import (
 )
 
 
-NGRAM_LENGHTS = [1, 2, 3]
-
-
 def create_fragment_ngram_cache(_req, resp, resource):
     if museum_number_dto := resp.media.get("museumNumber"):
         ngram_repository: FragmentNGramRepository = resource.ngram_repository
-        ngram_repository.update_ngrams(museum_number_dto, NGRAM_LENGHTS)
+        ngram_repository.set_ngrams(museum_number_dto, DEFAULT_N)
 
 
 def create_chapter_ngram_cache(_req, resp, resource):
     ngram_repository: ChapterNGramRepository = resource.ngram_repository
     chapter_id = ChapterIdSchema().load(resp.media, unknown=EXCLUDE)
-    ngram_repository.update_ngrams(chapter_id, NGRAM_LENGHTS)
+    ngram_repository.set_ngrams(chapter_id, DEFAULT_N)
