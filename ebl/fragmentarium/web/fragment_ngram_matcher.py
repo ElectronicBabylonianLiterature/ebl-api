73c00b562bc867373e3ca2356d81e3c6ce74236f
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-09-25 13:40:43 +0000

    refactor

--- ebl/fragmentarium/web/fragment_ngram_matcher.py
+++ ebl/fragmentarium/web/fragment_ngram_matcher.py
@@ -1,4 +1,3 @@
-from ebl.common.application.ngram_matcher import aggregate_ngram_overlaps
 from ebl.corpus.application.text_repository import TextRepository
 from ebl.fragmentarium.application.fragment_repository import FragmentRepository
 from falcon import Request, Response
@@ -6,7 +5,7 @@ from falcon import Request, Response
 from ebl.transliteration.domain.museum_number import MuseumNumber
 
 
-class NgramMatchResource:
+class NgramMatcherResource:
     def __init__(
         self,
         fragment_repository: FragmentRepository,
@@ -17,6 +16,4 @@ class NgramMatchResource:
 
     def on_get(self, _req: Request, resp: Response, number: str) -> None:
         ngrams = self._fragment_repository.get_ngrams(MuseumNumber.of(number))
-        resp.media = self._text_repository._chapters.aggregate(
-            aggregate_ngram_overlaps(ngrams)
-        )
+        resp.media = self._text_repository.aggregate_ngram_overlaps(ngrams)
