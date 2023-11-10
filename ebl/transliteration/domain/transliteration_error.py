c3cd3e598b071c1373b12a4b8bb72cdffd20aed4
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-11-10 15:44:55 +0000

    add ErrorAnnotation, parameterize error definitions

--- ebl/transliteration/domain/transliteration_error.py
+++ ebl/transliteration/domain/transliteration_error.py
@@ -1,9 +1,26 @@
+class ErrorAnnotation:
+    def __init__(self, description: str, line_number: int):
+        self.description = description
+        self.line_number = line_number
+
+    def to_dict(self):
+        return {
+            "description": self.description,
+            "lineNumber": self.line_number,
+        }
+
+
 class TransliterationError(Exception):
+    label = "Invalid transliteration"
+
     def __init__(self, errors):
-        super().__init__("Invalid transliteration")
+        super().__init__(self.label)
         self.errors = errors
 
-class DuplicateLabelError(Exception):
-    def __init__(self, errors):
-        super().__init__("Duplicate labels")
-        self.errors = errors
+
+class DuplicateLabelError(TransliterationError):
+    label = "Duplicate labels"
+
+
+class ExtentLabelError(TransliterationError):
+    label = "Invalid extents"
