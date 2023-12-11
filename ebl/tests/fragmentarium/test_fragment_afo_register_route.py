e8de5ec336a67fc0c69b6ed4d83d080b91d4a338
Sourcery AI <>
2023-12-11 12:30:39 +0000

    'Refactored by Sourcery'

--- ebl/tests/fragmentarium/test_fragment_afo_register_route.py
+++ ebl/tests/fragmentarium/test_fragment_afo_register_route.py
@@ -8,7 +8,7 @@ from ebl.tests.factories.afo_register import AfoRegisterRecordFactory
 
 def test_query_fragmentarium_afo_register(fragment_repository, client):
     record = AfoRegisterRecordFactory.build()
-    record_id = record.text + " " + record.text_number
+    record_id = f"{record.text} {record.text_number}"
     fragment = FragmentFactory.build(traditional_references=[record_id])
     fragment_repository.create_many([fragment, FragmentFactory.build()])
 
--- ebl/tests/fragmentarium/test_fragment_repository_afo_register.py
+++ ebl/tests/fragmentarium/test_fragment_repository_afo_register.py
@@ -9,7 +9,7 @@ from ebl.tests.factories.afo_register import AfoRegisterRecordFactory
 
 def test_query_fragmentarium_afo_register(fragment_repository):
     record = AfoRegisterRecordFactory.build()
-    record_id = record.text + " " + record.text_number
+    record_id = f"{record.text} {record.text_number}"
     fragment = FragmentFactory.build(traditional_references=[record_id])
     fragment_repository.create_many([fragment, FragmentFactory.build()])
     result = fragment_repository.query_by_traditional_references([record_id])
