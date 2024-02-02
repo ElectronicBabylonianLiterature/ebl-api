c188a9caf08349eeb4623738b6686bce63dbc135
Yugay Vasiliy <31739813+Vas9ka@users.noreply.github.com>
2024-02-02 11:51:25 +0100

    Added isEnvelope property to class Join (#515)
    * Added isEnvelope property to class Join
    
    * Formatting
    
    * Reformatting
    
    * Removed duplicate JoinFactory  from corpus, updated in fragment, added isEnvelope prop to tests

--- ebl/tests/fragmentarium/test_joins.py
+++ ebl/tests/fragmentarium/test_joins.py
@@ -8,12 +8,15 @@ from hamcrest.core.core.isequal import equal_to
 def test_join() -> None:
     museum_number = MuseumNumber("X", "1")
     is_checked = True
+    is_envelope = True
     joined_by = "Test User"
     date = "today"
     note = "test join"
     legacy_data = "old stuff"
 
-    join = Join(museum_number, is_checked, joined_by, date, note, legacy_data)
+    join = Join(
+        museum_number, is_checked, is_envelope, joined_by, date, note, legacy_data
+    )
 
     assert_that(
         join,
@@ -21,6 +24,7 @@ def test_join() -> None:
             {
                 "museum_number": museum_number,
                 "is_checked": is_checked,
+                "is_envelope": is_envelope,
                 "joined_by": joined_by,
                 "date": date,
                 "note": note,
@@ -33,6 +37,7 @@ def test_join() -> None:
 def test_join_default() -> None:
     defaults = {
         "is_checked": False,
+        "is_envelope": False,
         "joined_by": "",
         "date": "",
         "note": "",
