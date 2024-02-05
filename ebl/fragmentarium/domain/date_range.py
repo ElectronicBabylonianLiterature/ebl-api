9cbb27543b69e331ca37bf7f2a17d6c7544ce9ae
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2024-02-05 15:59:13 +0000

    add is_post_canonical

--- ebl/fragmentarium/domain/date_range.py
+++ ebl/fragmentarium/domain/date_range.py
@@ -8,6 +8,7 @@ class PartialDate:
     month: Optional[int] = None
     day: Optional[int] = None
     notes: str = ""
+    is_post_canonical: Optional[bool] = False
 
 
 @attr.s(auto_attribs=True, frozen=True)
