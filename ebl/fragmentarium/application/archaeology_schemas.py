5a4d3a931c747efafdb6a901d62ab530427bf78e
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2024-02-05 10:33:44 +0000

    Revert "temporarily allow unknown fields in archaeology"
    This reverts commit 3db4ab0b81e565f53200ae013a122b76155275fe.

--- ebl/fragmentarium/application/archaeology_schemas.py
+++ ebl/fragmentarium/application/archaeology_schemas.py
@@ -14,7 +14,7 @@ from ebl.fragmentarium.domain.findspot import (
     ExcavationSite,
 )
 from ebl.schemas import NameEnumField
-from marshmallow import Schema, fields, post_load, EXCLUDE
+from marshmallow import Schema, fields, post_load
 
 
 class ExcavationNumberSchema(AbstractMuseumNumberSchema):
@@ -64,9 +64,6 @@ class FindspotSchema(Schema):
 
 
 class ArchaeologySchema(Schema):
-    class Meta:
-        unknown = EXCLUDE
-
     excavation_number = fields.Nested(
         ExcavationNumberSchema, data_key="excavationNumber", allow_none=True
     )
