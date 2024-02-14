45aa8e0a4a0f720d18a2958f42860dbc8c511b93
Enrique Jiménez <39047733+ejimsan@users.noreply.github.com>
2024-02-14 11:21:04 +0100

    Add HPM Alalakh to Fragment External Numbers (#521)
    * Bump cryptography from 41.0.6 to 42.0.0 (#518)
    
    Bumps [cryptography](https://github.com/pyca/cryptography) from 41.0.6 to 42.0.0.
    - [Changelog](https://github.com/pyca/cryptography/blob/main/CHANGELOG.rst)
    - [Commits](https://github.com/pyca/cryptography/compare/41.0.6...42.0.0)
    
    ---
    updated-dependencies:
    - dependency-name: cryptography
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump pillow from 10.0.1 to 10.2.0 (#509)
    
    Bumps [pillow](https://github.com/python-pillow/Pillow) from 10.0.1 to 10.2.0.
    - [Release notes](https://github.com/python-pillow/Pillow/releases)
    - [Changelog](https://github.com/python-pillow/Pillow/blob/main/CHANGES.rst)
    - [Commits](https://github.com/python-pillow/Pillow/compare/10.0.1...10.2.0)
    
    ---
    updated-dependencies:
    - dependency-name: pillow
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump pycryptodomex from 3.18.0 to 3.19.1 (#505)
    
    Bumps [pycryptodomex](https://github.com/Legrandin/pycryptodome) from 3.18.0 to 3.19.1.
    - [Release notes](https://github.com/Legrandin/pycryptodome/releases)
    - [Changelog](https://github.com/Legrandin/pycryptodome/blob/master/Changelog.rst)
    - [Commits](https://github.com/Legrandin/pycryptodome/compare/v3.18.0...v3.19.1)
    
    ---
    updated-dependencies:
    - dependency-name: pycryptodomex
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Date fixes (#510)
    
    * Implement date
    
    * Fix Seleucid issue
    
    * Date paleography
    
    * 'Refactored by Sourcery' (#444)
    
    Co-authored-by: Sourcery AI <>
    
    * Update test
    
    * Implement date deleting
    
    * Format
    
    * Implement dates in text
    
    * Add tests
    
    * Implement eponyms
    
    * Extend eponym
    
    * Add `broken` & `uncertain` to king and eponym
    
    * Update url
    
    ---------
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    * Update king schema (#520)
    
    * Update kings schema
    
    * Update tests
    
    * Remove unused
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Ilya Khait <ilya.khait@lmu.de>
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- ebl/tests/chronology/test_chronology.py
+++ ebl/tests/chronology/test_chronology.py
@@ -4,7 +4,7 @@ from ebl.chronology.chronology import KingSchema, ChronologySchema, Chronology, 
 
 def test_king_creation():
     king = King(
-        1, 2, "dyn1", "Dynasty 1", "1", "King A", "1999", "10 years", "Some notes"
+        1, 2, "dyn1", "Dynasty 1", "1", "King A", "1999", "10 years", "Some notes", None
     )
 
     assert king.order_global == 1
@@ -20,10 +20,10 @@ def test_king_creation():
 
 def test_chronology_creation():
     king_a = King(
-        1, 2, "dyn1", "Dynasty 1", "1", "King A", "1999", "10 years", "Some notes"
+        1, 2, "dyn1", "Dynasty 1", "1", "King A", "1999", "10 years", "Some notes", None
     )
     king_b = King(
-        2, 2, "dyn2", "Dynasty 2", "2", "King B", "2000", "5 years", "Other notes"
+        2, 2, "dyn2", "Dynasty 2", "2", "King B", "2000", "5 years", "Other notes", True
     )
     chronology = Chronology([king_a, king_b])
 
@@ -32,10 +32,10 @@ def test_chronology_creation():
 
 def test_find_king_by_name():
     king_a = King(
-        1, 2, "dyn1", "Dynasty 1", "1", "King A", "1999", "10 years", "Some notes"
+        1, 2, "dyn1", "Dynasty 1", "1", "King A", "1999", "10 years", "Some notes", None
     )
     king_b = King(
-        2, 2, "dyn2", "Dynasty 2", "2", "King B", "2000", "5 years", "Other notes"
+        2, 2, "dyn2", "Dynasty 2", "2", "King B", "2000", "5 years", "Other notes", True
     )
     chronology = Chronology([king_a, king_b])
 
@@ -73,8 +73,8 @@ def test_chronology_schema_deserialization():
     with open("ebl/chronology/brinkmanKings.json", "r", encoding="utf-8") as file:
         data = json.load(file)
         chronology = ChronologySchema().load({"kings": data})
-    assert len(chronology.kings) == 364
+    assert len(chronology.kings) == 365
     assert chronology.kings[0].name == "Sargon"
-    assert chronology.kings[363].name == "Aššur-uballiṭ II"
-    assert chronology.kings[50].order_global == 51
-    assert chronology.kings[363].order_global == 364
+    assert chronology.kings[363].name == "Sin-šar-iškun"
+    assert chronology.kings[50].order_global == 50
+    assert chronology.kings[363].order_global == 363
