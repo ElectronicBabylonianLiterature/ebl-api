61c652c16899bf95e79ed574fe3f8bcf37a29cb1
Yugay Vasiliy <31739813+Vas9ka@users.noreply.github.com>
2024-02-26 11:38:36 +0100

    Feature/provenance route (#526)
    * Added provenance property to Annotation class
    
    * Created "/provenances" route with list of provenances
    
    * added provenance route test
    
    * fixed output

---
+++ ebl/tests/fragmentarium/test_provenance_route.py
@@ -0,0 +1,16 @@
+import falcon
+
+from ebl.corpus.domain.provenance import Provenance
+
+
+def test_get_genre(client):
+    get_result = client.simulate_get("/provenances")
+    provenances_data = tuple(
+        (prov.long_name, prov.parent)
+        if prov.parent is None
+        else (prov.long_name, f"({prov.parent})")
+        for prov in Provenance
+        if prov.long_name != "Standard Text"
+    )
+    assert get_result.status == falcon.HTTP_OK
+    assert get_result.json == list(map(list, provenances_data))
