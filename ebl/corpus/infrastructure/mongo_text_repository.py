0738d0835a1f886c8c9859783cc104252fd8e25b
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2024-02-28 14:58:19 +0000

    add get_sign_data methods

--- ebl/corpus/infrastructure/mongo_text_repository.py
+++ ebl/corpus/infrastructure/mongo_text_repository.py
@@ -449,3 +449,16 @@ class MongoTextRepository(TextRepository):
             ]
         )
         return ManuscriptAttestationSchema().load(cursor, many=True)
+
+    def get_sign_data(self, id_: ChapterId) -> dict:
+        return self._chapters.find_one(
+            chapter_id_query(id_),
+            projection={
+                "_id": False,
+                "signs": True,
+                "manuscripts": True,
+                "textId": True,
+                "stage": True,
+                "name": True,
+            },
+        )
