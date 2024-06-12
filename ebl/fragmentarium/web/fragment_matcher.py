d2c5ddc375e02cc960c8699f2f604725a7f677db
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2024-06-12 10:29:17 +0000

    restrict /match endpoint to logged in users

--- ebl/fragmentarium/web/fragment_matcher.py
+++ ebl/fragmentarium/web/fragment_matcher.py
@@ -1,4 +1,7 @@
 from falcon import Request, Response
+import falcon
+from ebl.users.web.require_scope import require_scope
+
 
 from ebl.errors import DataError, NotFoundError
 from ebl.fragmentarium.application.fragment_matcher import FragmentMatcher
@@ -11,6 +14,7 @@ class FragmentMatcherResource:
     def __init__(self, fragment_matcher: FragmentMatcher):
         self.fragment_matcher = fragment_matcher
 
+    @falcon.before(require_scope, "transliterate:fragments")
     def on_get(self, req: Request, resp: Response, number) -> None:
         try:
             resp.media = LineToVecRankingSchema().dump(
