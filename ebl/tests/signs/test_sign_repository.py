60d222881d3af588eb10bd29705858d1c5d9c03b
Yugay Vasiliy <vasyayugay99@gmail.com>
2024-06-17 08:09:24 +0000

    added test for checking get_unicode_from_atf

--- ebl/tests/signs/test_sign_repository.py
+++ ebl/tests/signs/test_sign_repository.py
@@ -262,6 +262,15 @@ def test_find_signs_by_order(database, sign_repository, mongo_sign_igi, mongo_si
     ]
 
 
+def test_get_unicode_from_atf(database, sign_repository):
+    transliteration_line = "gar ki"
+    assert sign_repository.get_unicode_from_atf(transliteration_line) == [
+        {"unicode": [73979]},
+        {"unicode": [9999]},
+        {"unicode": [74144]},
+    ]
+
+
 def test_find_signs_by_order_not_found(sign_repository):
     assert sign_repository.find_signs_by_order("SI", "not_existing_era") == []
 
