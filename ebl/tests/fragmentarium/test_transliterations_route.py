c6fb895a2d9cb060b6e7991117dd5e9d63b8794f
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2025-02-18 13:05:01 +0000

    add introduction tests

--- ebl/tests/fragmentarium/test_transliterations_route.py
+++ ebl/tests/fragmentarium/test_transliterations_route.py
@@ -6,7 +6,7 @@ import pytest
 from freezegun import freeze_time
 
 from ebl.transliteration.domain.museum_number import MuseumNumber
-from ebl.fragmentarium.domain.fragment import Notes, Fragment
+from ebl.fragmentarium.domain.fragment import Notes, Introduction, Fragment
 from ebl.fragmentarium.domain.transliteration_update import TransliterationUpdate
 from ebl.fragmentarium.web.dtos import create_response_dto
 from ebl.tests.factories.fragment import FragmentFactory, LemmatizedFragmentFactory
@@ -218,3 +218,69 @@ def test_update_notes(client, fragmentarium, user, database, old_notes, new_note
             "user_profile.name": user.profile["name"],
         }
     )
+
+
+@pytest.mark.parametrize(
+    "old_introduction,new_introduction",
+    [
+        [
+            Introduction(),
+            Introduction(
+                "A new introduction",
+                (StringPart("A new introduction"),),
+            ),
+        ],
+        [
+            Introduction(
+                "An old introduction",
+                (StringPart("An old introduction"),),
+            ),
+            Introduction(),
+        ],
+        [
+            Introduction(),
+            Introduction(),
+        ],
+    ],
+)
+def test_update_introduction(
+    client, fragmentarium, user, database, old_introduction, new_introduction
+):
+    fragment: Fragment = FragmentFactory.build(introduction=old_introduction)
+    fragment_number = fragmentarium.create(fragment)
+    update = {"introduction": new_introduction.text}
+    post_result = client.simulate_post(
+        f"/fragments/{fragment_number}/edition", body=json.dumps(update)
+    )
+    expected_json = {
+        **create_response_dto(
+            fragment.set_introduction(new_introduction.text),
+            user,
+            fragment.number == "K.1",
+        )
+    }
+
+    assert post_result.status == falcon.HTTP_OK
+    assert post_result.json == expected_json
+
+    get_result = client.simulate_get(f"/fragments/{fragment_number}")
+    assert get_result.json == expected_json
+
+    assert database["changelog"].find_one(
+        {
+            "resource_id": fragment_number,
+            "resource_type": "fragments",
+            "user_profile.name": user.profile["name"],
+        }
+    )
+
+
+def test_update_invalid_introduction(client, fragmentarium, user, database):
+    fragment: Fragment = FragmentFactory.build()
+    fragment_number = fragmentarium.create(fragment)
+    update = {"introduction": "@i{syntax error"}
+    post_result = client.simulate_post(
+        f"/fragments/{fragment_number}/edition", body=json.dumps(update)
+    )
+
+    assert post_result.status == falcon.HTTP_UNPROCESSABLE_ENTITY
