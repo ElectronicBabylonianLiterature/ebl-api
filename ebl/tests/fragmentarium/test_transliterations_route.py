c27d5344b2b4a6735dad67ae0aa3696209da43df
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2025-02-18 13:03:18 +0000

    add notes update tests

--- ebl/tests/fragmentarium/test_transliterations_route.py
+++ ebl/tests/fragmentarium/test_transliterations_route.py
@@ -6,11 +6,13 @@ import pytest
 from freezegun import freeze_time
 
 from ebl.transliteration.domain.museum_number import MuseumNumber
+from ebl.fragmentarium.domain.fragment import Notes, Fragment
 from ebl.fragmentarium.domain.transliteration_update import TransliterationUpdate
 from ebl.fragmentarium.web.dtos import create_response_dto
 from ebl.tests.factories.fragment import FragmentFactory, LemmatizedFragmentFactory
 from ebl.transliteration.domain.lark_parser import parse_atf_lark
 from ebl.fragmentarium.domain.joins import Join
+from ebl.transliteration.domain.markup import StringPart, EmphasisPart
 
 
 @freeze_time("2018-09-07 15:41:24.032")
@@ -171,3 +173,48 @@ def test_update_transliteration_invalid_entity(client, fragmentarium, body):
     post_result = client.simulate_post(url, body=body)
 
     assert post_result.status == falcon.HTTP_BAD_REQUEST
+
+
+@pytest.mark.parametrize(
+    "old_notes,new_notes",
+    [
+        [Notes(), Notes("Some notes", (StringPart("Some notes"),))],
+        [Notes(), Notes()],
+        [Notes("Different notes"), Notes()],
+        [
+            Notes("Different notes"),
+            Notes(
+                "Different notes @i{with emphasis}",
+                (StringPart("Different notes "), EmphasisPart("with emphasis")),
+            ),
+        ],
+    ],
+)
+def test_update_notes(client, fragmentarium, user, database, old_notes, new_notes):
+    fragment: Fragment = FragmentFactory.build(notes=old_notes)
+    fragment_number = fragmentarium.create(fragment)
+    update = {"notes": new_notes.text}
+    post_result = client.simulate_post(
+        f"/fragments/{fragment_number}/edition", body=json.dumps(update)
+    )
+    expected_json = {
+        **create_response_dto(
+            fragment.set_notes(new_notes.text),
+            user,
+            fragment.number == "K.1",
+        )
+    }
+
+    assert post_result.status == falcon.HTTP_OK
+    assert post_result.json == expected_json
+
+    get_result = client.simulate_get(f"/fragments/{fragment_number}")
+    assert get_result.json == expected_json
+
+    assert database["changelog"].find_one(
+        {
+            "resource_id": fragment_number,
+            "resource_type": "fragments",
+            "user_profile.name": user.profile["name"],
+        }
+    )
