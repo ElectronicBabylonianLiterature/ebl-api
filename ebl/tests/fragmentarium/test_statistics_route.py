9075134163258aae1776c7298e4089e8b2a78e05
Enrique Jiménez <enrique.jimenez@lmu.de>
2025-03-17 13:33:27 +0000

    Fix tests

--- ebl/tests/fragmentarium/test_statistics_route.py
+++ ebl/tests/fragmentarium/test_statistics_route.py
@@ -9,7 +9,11 @@ PATH = "/statistics"
 def test_get_statistics(guest_client):
     result = guest_client.simulate_get(PATH)
 
-    assert result.json == {"transliteratedFragments": 0, "lines": 0}
+    assert result.json == {
+        "transliteratedFragments": 0,
+        "lines": 0,
+        "totalFragments": 0
+    }
     assert result.status == falcon.HTTP_OK
     assert result.headers["Cache-Control"] == "public, max-age=600"
 
