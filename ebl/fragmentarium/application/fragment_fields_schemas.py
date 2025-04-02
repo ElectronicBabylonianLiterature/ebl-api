1b335f9d644b8e2c687c7073e886f7aec42dde94
Zsombor Földi <zsombor.foldi@gmail.com>
2025-04-02 21:43:09 +0000

    add Ashmolean number

--- ebl/fragmentarium/application/fragment_fields_schemas.py
+++ ebl/fragmentarium/application/fragment_fields_schemas.py
@@ -63,6 +63,7 @@ class ExternalNumbersSchema(Schema):
     louvre_number = fields.String(load_default="", data_key="louvreNumber")
     dublin_tcd_number = fields.String(load_default="", data_key="dublinTcdNumber")
     cambridge_maa_number = fields.String(load_default="", data_key="cambridgeMaaNumber")
+    ashmolean_number = fields.String(load_default="", data_key="ashmoleanNumber")
     alalah_hpm_number = fields.String(load_default="", data_key="alalahHpmNumber")
     philadelphia_number = fields.String(load_default="", data_key="philadelphiaNumber")
     australianinstituteofarchaeology_number = fields.String(
