35589d474a719de93ac8e526f7844e28b900a712
fsimonjetz <fabian.simonjetz@rub.de>
2025-05-19 15:34:31 +0200

    Restructure Lark Grammars (#616)
    * remove legacy oracc grammar
    
    * move lark.py
    
    * move lark_parser.py
    
    * update lark_parser.py
    
    * move ebl_atf.lark
    
    * update ebl_atf.lark
    
    * add abbreviations
    
    * add at-line artifacts
    
    * add chapter artifacts
    
    * add common artifacts
    
    * add control line artifacts
    
    * add dollar line artifacts
    
    * add empty line artifacts
    
    * add manuscript line artifact
    
    * add note line artifact
    
    * add parallel line artifact
    
    * add text line artifact
    
    * add translation line artifact
    
    * add legacy atf artifact
    
    * add oracc lem line artifact
    
    * move parser errors file
    
    * move reconstructed_text_parser.py
    
    * update oracc_atf_lem_line.lark
    
    * update reconstructed text parser
    
    * reformat
    
    * remove deprecated lark grammar artifacts

--- ebl/transliteration/domain/atf_parsers/lark_parser.py
+++ ebl/transliteration/domain/atf_parsers/lark_parser.py
@@ -4,7 +4,7 @@ import re
 
 import pydash
 from lark.exceptions import ParseError, UnexpectedInput, VisitError
-from lark.lark import Lark
+from lark.lark import Lark, Tree
 
 from ebl.transliteration.domain import atf
 from ebl.transliteration.domain.enclosure_error import EnclosureError
@@ -28,32 +28,39 @@ from ebl.transliteration.domain.transliteration_error import (
     ErrorAnnotation,
 )
 from ebl.transliteration.domain.word_tokens import Word
-from ebl.transliteration.domain.lark_parser_errors import PARSE_ERRORS
+from ebl.transliteration.domain.atf_parsers.lark_parser_errors import PARSE_ERRORS
 from ebl.transliteration.domain.line_transformer import LineTransformer
+from ebl.transliteration.domain.label_transformer import LabelTransformer
+from ebl.transliteration.domain.labels import Label
 from functools import singledispatch
 
-WORD_PARSER = Lark.open(
-    "ebl_atf.lark", maybe_placeholders=True, rel_to=__file__, start="any_word"
-)
-NOTE_LINE_PARSER = Lark.open(
-    "ebl_atf.lark", maybe_placeholders=True, rel_to=__file__, start="note_line"
-)
-MARKUP_PARSER = Lark.open(
-    "ebl_atf.lark", maybe_placeholders=True, rel_to=__file__, start="markup"
-)
-PARALLEL_LINE_PARSER = Lark.open(
-    "ebl_atf.lark", maybe_placeholders=True, rel_to=__file__, start="parallel_line"
-)
+ATF_GRAMMAR_PATH = "lark_parser/ebl_atf.lark"
+ATF_COMMON_PATH = "lark_parser/ebl_atf_common.lark"
+kwargs_lark = {"maybe_placeholders": True, "rel_to": __file__}
+
+WORD_PARSER = Lark.open(ATF_GRAMMAR_PATH, **kwargs_lark, start="any_word")
+NOTE_LINE_PARSER = Lark.open(ATF_GRAMMAR_PATH, **kwargs_lark, start="note_line")
+MARKUP_PARSER = Lark.open(ATF_GRAMMAR_PATH, **kwargs_lark, start="markup")
+PARALLEL_LINE_PARSER = Lark.open(ATF_GRAMMAR_PATH, **kwargs_lark, start="parallel_line")
 TRANSLATION_LINE_PARSER = Lark.open(
-    "ebl_atf.lark", maybe_placeholders=True, rel_to=__file__, start="translation_line"
+    ATF_GRAMMAR_PATH, **kwargs_lark, start="translation_line"
 )
-PARATEXT_PARSER = Lark.open(
-    "ebl_atf.lark", maybe_placeholders=True, rel_to=__file__, start="paratext"
-)
+PARATEXT_PARSER = Lark.open(ATF_GRAMMAR_PATH, **kwargs_lark, start="paratext")
 CHAPTER_PARSER = Lark.open(
-    "ebl_atf.lark", maybe_placeholders=True, rel_to=__file__, start="chapter"
+    "lark_parser/ebl_atf_chapter.lark", **kwargs_lark, start="chapter"
 )
-LINE_PARSER = Lark.open("ebl_atf.lark", maybe_placeholders=True, rel_to=__file__)
+MANUSCRIPT_PARSER = Lark.open(
+    "lark_parser/ebl_atf_manuscript_line.lark", **kwargs_lark, start="manuscript_line"
+)
+LINE_PARSER = Lark.open(ATF_GRAMMAR_PATH, **kwargs_lark)
+LINE_NUMBER_PARSER = Lark.open(ATF_COMMON_PATH, **kwargs_lark, start="line_number")
+
+LABEL_PARSER = Lark.open(
+    "lark_parser/ebl_atf.lark",
+    maybe_placeholders=True,
+    rel_to=__file__,
+    start="labels",
+)
 
 
 def parse_word(atf: str) -> Word:
@@ -125,17 +132,27 @@ def parse_translation_line(atf: str) -> TranslationLine:
     return LineTransformer().transform(tree)
 
 
+def parse_labels(label: str) -> Sequence[Label]:
+    if label:
+        tree = LABEL_PARSER.parse(label)
+        return LabelTransformer().transform(tree)
+    else:
+        return ()
+
+
 def parse_text_line(atf: str) -> TextLine:
     tree = LINE_PARSER.parse(atf, start="text_line")
     return LineTransformer().transform(tree)
 
 
 def parse_line_number(atf: str) -> AbstractLineNumber:
-    tree = LINE_PARSER.parse(atf, start="ebl_atf_text_line__line_number")
+    tree = LINE_NUMBER_PARSER.parse(atf)
     return LineTransformer().transform(tree)
 
 
 def validate_line(line: Line) -> None:
+    if isinstance(line, Tree):
+        raise ParseError
     visitor = EnclosureValidator()
     line.accept(visitor)
     visitor.done()
@@ -150,7 +167,7 @@ def clean_line(line: str):
     return line
 
 
-def parse_atf_lark(atf_):
+def parse_atf_lark(atf_: str) -> Text:
     def parse_line_(line: str, line_number: int):
         try:
             line = clean_line(line)
@@ -177,7 +194,9 @@ def parse_atf_lark(atf_):
     if duplicates := pydash.duplicates(text.labels):
         raise DuplicateLabelError(
             [
-                ErrorAnnotation("Duplicate label", label.line_index + 1)
+                ErrorAnnotation(
+                    "Duplicate label", label.line_index + 1 if label.line_index else 1
+                )
                 for label in text.labels
                 if label in duplicates
             ]
