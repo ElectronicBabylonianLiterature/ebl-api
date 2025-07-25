8dac541465bd2e1fcf3a1dbd8a4e53f5c9f505eb
Fabian Simonjetz <fabian.simonjetz@rub.de>
2025-07-25 21:02:57 +0200

    add named entity schema tests

---
+++ ebl/tests/fragmentarium/test_named_entity_schemas.py
@@ -0,0 +1,41 @@
+from ebl.fragmentarium.application.named_entity_schema import (
+    EntityAnnotationSpanSchema,
+    NamedEntitySchema,
+)
+import pytest
+
+from ebl.fragmentarium.domain.named_entity import (
+    EntityAnnotationSpan,
+    NamedEntity,
+    NamedEntityType,
+)
+
+
+@pytest.mark.parametrize(
+    "entity,serialized",
+    [
+        (
+            NamedEntity("Test-1", NamedEntityType.LOCATION),
+            {"id": "Test-1", "type": NamedEntityType.LOCATION.long_name},
+        ),
+        (
+            NamedEntity("Test-2", NamedEntityType.PERSON),
+            {"id": "Test-2", "type": NamedEntityType.PERSON.long_name},
+        ),
+    ],
+)
+def test_named_entity_schema(entity, serialized):
+    assert NamedEntitySchema().load(serialized) == entity
+    assert NamedEntitySchema().dump(entity) == serialized
+
+
+@pytest.mark.parametrize(
+    "span,serialized",
+    [
+        (EntityAnnotationSpan("Test-1", NamedEntityType.YEAR, ["Word-1", "Word-2"])),
+        (EntityAnnotationSpan("Test-2", NamedEntityType.PERSON, ["Word-3", "Word-4"])),
+    ],
+)
+def test_entity_annotation_span_schema(span, serialized):
+    assert EntityAnnotationSpanSchema().load(serialized) == span
+    assert EntityAnnotationSpanSchema().dump(span) == serialized
