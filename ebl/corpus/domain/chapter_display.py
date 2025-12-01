e86f76e223eade857050f6cc9f92755ab0845d02
Enrique Jiménez <39047733+ejimsan@users.noreply.github.com>
2025-12-01 09:54:03 +0000

    Remove old code

--- ebl/corpus/domain/chapter_display.py
+++ ebl/corpus/domain/chapter_display.py
@@ -49,7 +49,6 @@ class LineDisplay:
         line_atf_blocks = []
         for index, variant in enumerate(self.variants):
             reconstruction = f"{self.number.atf} {variant.reconstruction_atf}"
-            reconstruction_note = variant.note.atf if variant.note else ""
             translation = (
                 self.translation[index].atf if len(self.translation) > index else ""
             )
