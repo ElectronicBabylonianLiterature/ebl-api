e514d5f7455e2dc2b397a073509914032fa382ec
Asim <114213130+asimniaz-ai@users.noreply.github.com>
2026-04-12 13:14:20 +0000

    Apply ruff formatting

--- ebl/signs/web/bootstrap.py
+++ ebl/signs/web/bootstrap.py
@@ -39,9 +39,7 @@ def create_signs_routes(api: falcon.App, context: Context):
     api.add_route("/signs/{sign_name}/images", signs_images)
 
     # NEW ROUTE
-    api.add_route(
-        "/signs/{sign_name}/images/cluster/{cluster_id}", cluster_images
-    )
+    api.add_route("/signs/{sign_name}/images/cluster/{cluster_id}", cluster_images)
 
     api.add_route("/signs/all", signs_all)
     api.add_route("/signs/{sign_name}/{sort_era}", ordered_signs)
