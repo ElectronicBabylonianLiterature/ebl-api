610a5335c03b5263483d5c25c0d6712a781b7922
Asim <114213130+asimniaz-ai@users.noreply.github.com>
2026-04-12 12:20:54 +0000

    Add PCA clustering filters for cropped annotations

--- ebl/fragmentarium/application/cropped_annotations_service.py
+++ ebl/fragmentarium/application/cropped_annotations_service.py
@@ -1,14 +1,15 @@
-from typing import Sequence
+from typing import Optional, Sequence
 
 import attr
 
 from ebl.fragmentarium.application.annotations_repository import AnnotationsRepository
+from ebl.fragmentarium.application.annotations_schema import PcaClusteringSchema
 from ebl.fragmentarium.application.cropped_sign_images_repository import (
     CroppedSignImagesRepository,
 )
 from ebl.fragmentarium.application.fragment_repository import FragmentRepository
-from ebl.fragmentarium.domain.date import DateSchema
 from ebl.fragmentarium.domain.annotation import AnnotationValueType
+from ebl.fragmentarium.domain.date import DateSchema
 
 
 class CroppedAnnotationService:
@@ -22,9 +23,18 @@ class CroppedAnnotationService:
         self._fragment_repository = fragment_repository
         self._cropped_sign_image_repository = cropped_sign_images_repository
 
-    def find_annotations_by_sign(self, sign: str) -> Sequence[dict]:
-        annotations = self._annotations_repository.find_by_sign(sign)
+    def find_annotations_by_sign(
+        self,
+        sign: str,
+        centroids_only: bool = False,
+        cluster_id: Optional[str] = None,
+        script_filter: Optional[str] = None,
+    ) -> Sequence[dict]:
+        annotations = self._annotations_repository.find_by_sign(
+            sign, centroids_only, cluster_id, script_filter
+        )
         cropped_image_annotations = []
+
         for annotation in annotations:
             date = DateSchema().dump(
                 self._fragment_repository.fetch_date(annotation.fragment_number)
@@ -37,6 +47,7 @@ class CroppedAnnotationService:
                     if x.data.type == AnnotationValueType.HAS_SIGN
                 ],
             )
+
             for annotation_elem in annotation.annotations:
                 if cropped_sign := annotation_elem.cropped_sign:
                     try:
@@ -45,16 +56,24 @@ class CroppedAnnotationService:
                                 cropped_sign.image_id
                             )
                         )
-                        cropped_image_annotations.append(
-                            {
-                                "fragmentNumber": str(annotation.fragment_number),
-                                "image": cropped_sign_image.image,
-                                "script": str(annotation.script),
-                                "label": cropped_sign.label,
-                                "date": date,
-                                "provenance": annotation.provenance,
-                            }
-                        )
+
+                        response = {
+                            "fragmentNumber": str(annotation.fragment_number),
+                            "image": cropped_sign_image.image,
+                            "script": str(annotation.script),
+                            "label": cropped_sign.label,
+                            "date": date,
+                            "provenance": annotation.provenance,
+                            "annotationId": annotation_elem.data.id,
+                        }
+
+                        if annotation_elem.pca_clustering:
+                            response["pcaClustering"] = PcaClusteringSchema().dump(
+                                annotation_elem.pca_clustering
+                            )
+
+                        cropped_image_annotations.append(response)
                     except Exception:
                         pass
+
         return cropped_image_annotations
--- ebl/signs/web/cropped_annotations.py
+++ ebl/signs/web/cropped_annotations.py
@@ -1,3 +1,5 @@
+from falcon import Request, Response
+
 from ebl.fragmentarium.application.cropped_annotations_service import (
     CroppedAnnotationService,
 )
@@ -7,8 +9,34 @@ class CroppedAnnotationsResource:
     def __init__(self, cropped_annotations_service: CroppedAnnotationService):
         self._cropped_annotations_service = cropped_annotations_service
 
-    def on_get(self, _req, resp, sign_name):
+    def on_get(self, req: Request, resp: Response, sign_name: str):
+        centroids_only = req.get_param_as_bool("centroids_only", default=False)
+
         cropped_signs = self._cropped_annotations_service.find_annotations_by_sign(
-            sign_name
+            sign_name,
+            centroids_only=centroids_only,
+        )
+        resp.media = cropped_signs
+
+
+class ClusterCroppedAnnotationsResource:
+    def __init__(self, cropped_annotations_service: CroppedAnnotationService):
+        self._cropped_annotations_service = cropped_annotations_service
+
+    def on_get(self, req: Request, resp: Response, sign_name: str, cluster_id: str):
+        script_filter = req.get_param("script")
+
+        if not script_filter:
+            resp.status = "400 Bad Request"
+            resp.media = {
+                "error": "Query parameter 'script' is required for cluster queries"
+            }
+            return
+
+        cropped_signs = self._cropped_annotations_service.find_annotations_by_sign(
+            sign_name,
+            centroids_only=False,
+            cluster_id=cluster_id,
+            script_filter=script_filter,
         )
         resp.media = cropped_signs
