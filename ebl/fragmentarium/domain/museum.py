9bc96c49a43dc10dc583f690105c93b4a4fa89ad
Enrique Jiménez <39047733+ejimsan@users.noreply.github.com>
2026-07-31 10:09:50 +0200

    Add various museums in Turkey (#745)

--- ebl/fragmentarium/domain/museum.py
+++ ebl/fragmentarium/domain/museum.py
@@ -33,6 +33,12 @@ class Museum(Enum):
         "AU",
         "https://www.aiarch.org.au/",
     )
+    BATMAN_HASANKEYF_MUZESI = (
+        "Batman Hasankeyf Müzesi",
+        "Batman",
+        "TR",
+        "https://kvmgm.ktb.gov.tr/TR-281298/batman-muze-mudurlugu.html",
+    )
     BANQUE_NATIONALE_DE_BELGIQUE = (
         "Banque Nationale de Belgique",
         "Brussels",
@@ -160,6 +166,12 @@ class Museum(Enum):
         "https://www.khm.at/",
     )
     LOUVRE = ("Louvre", "Paris", "FR", "https://www.louvre.fr/")
+    MARDIN_MUZESI = (
+        "Mardin Müzesi",
+        "Mardin",
+        "TR",
+        "https://muze.gov.tr/muze-detay?sectionId=MRD01&distId=MRK",
+    )
     MOSUL_MUSEUM = (
         "Mosul Museum",
         "Mosul",
@@ -258,6 +270,11 @@ class Museum(Enum):
         "Private collection of W. Lamplough",
         "",
         "GB",
+    )
+    PRIVATE_COLLECTION_OF_Z_YILDIZ = (
+        "Private collection of Zeynel Yıldız",
+        "Elazığ",
+        "TR",
     )
     MCGILL_UNIVERSITY = (
         "McGill University Ethnological Collections",
